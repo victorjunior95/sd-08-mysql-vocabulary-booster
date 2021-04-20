7ff0f4b730c56540b0b321decb0b6b257732c841
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 18:31:04 -0300

    fix: ERRATA

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT hrj.JOB_TITLE AS 'Cargo', 
 (hrj.MAX_SALARY - hrj.MIN_SALARY) AS 'Variação Salarial',
-ROUND(hrj.MIN_SALARY / 12, 0) AS 'Média mínima mensal',
-ROUND(hrj.MAX_SALARY / 12, 0) AS 'Média mínima mensal'
+ROUND(hrj.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+ROUND(hrj.MAX_SALARY / 12, 2) AS 'Média mínima mensal'
 FROM hr.jobs AS hrj;
