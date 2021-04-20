3117926a5a7758b61d45fbaaba847d1de54e9f90
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-20 19:36:16 -0300

    Requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -4,4 +4,4 @@ JOB_TITLE AS 'Cargo',
 (ROUND(MIN_SALARY/12, 2)) AS 'Média mínima mensal',
 (ROUND(MAX_SALARY/12, 2)) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY `Variação Salarial`;
+ORDER BY `Variação Salarial`, `Cargo`;
