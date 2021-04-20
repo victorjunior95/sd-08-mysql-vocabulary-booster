5183f3b9d133ccf314d43f321aab7ae42bdddd38
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-04-20 15:03:25 -0300

    faz desadio 3 e identa desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,5 @@
-SELECT country_name AS `País`, IF(region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão`
+SELECT 
+  country_name AS `País`, 
+  IF(region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries
 ORDER BY country_name;
