91cea014e8ef9073755587c9537d3a6f0a5826e8
victorjunior95 <victorjr.muchacho@gmail.com>
2021-04-20 16:33:55 -0300

    Requisito 1 completed

---
+++ desafio1.sql
@@ -0,0 +1,12 @@
+-- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
+-- 	Seu relatório deve apresentar 02 colunas:
+-- 		a) A primeira deve possuir o alias "País" e exibir o nome do país.
+-- 		b) A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão
+-- 		está incluso em "Europe", ou "não incluído", caso contrário.
+-- 	Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
+SELECT
+	COUNTRY_NAME AS `País`,
+	IF (REGION_ID = 1, 'incluido', 'não incluido') AS `Status Inclusão`
+FROM
+	hr.countries
+ORDER BY COUNTRY_NAME;
