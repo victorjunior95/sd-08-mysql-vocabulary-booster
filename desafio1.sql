addf59208dad954c6f2af7737a623785ccede11b
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-20 16:33:41 -0300

    [FIX] - Corrige Req 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,3 @@
 SELECT country_name AS `País`, 
-IF(region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão` FROM hr.countries; 
+IF(region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão` FROM hr.countries
+ORDER BY `País`;
