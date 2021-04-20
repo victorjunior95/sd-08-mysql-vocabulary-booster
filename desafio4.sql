91fc0c77a6c8d18a10c2ee15bec1d986f85c6457
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 19:28:41 -0300

    cumprindo desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+select j.job_title as Cargo, truncate(avg(e.salary), 2) as `Média salarial`,
+case 
+	when e.salary between 1999 and 5800 then 'Júnior'
+    when e.salary between 5801 and 7500 then 'Pleno'
+    when e.salary between 7501 and 10500 then 'Sênior'
+    else 'CEO'
+end as `Senioridade` 
+from hr.jobs j
+inner join hr.employees e
+on j.job_id = e.job_id
+group by j.job_title
+order by e.salary, senioridade;
