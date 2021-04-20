c2c673f2bebe2f4dfce4aabce5c18eb2cf741569
Luiz Reina <luizreeina@gmail.com>
2021-04-20 16:25:56 -0300

    Desafio 2 codeclimate

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 SELECT job_title AS 'Cargo', max_salary,
 CASE
-	  WHEN max_salary > 5000 AND max_salary <= 10000 THEN ('Baixo') 
-    WHEN max_salary > 10001 AND max_salary <= 20000 THEN ('Médio')
-    WHEN max_salary > 20001 AND max_salary <= 30000 THEN ('Alto')
-    WHEN max_salary > 30000 THEN ('Altíssimo')
+WHEN max_salary > 5000 AND max_salary <= 10000 THEN ('Baixo') 
+WHEN max_salary > 10001 AND max_salary <= 20000 THEN ('Médio')
+WHEN max_salary > 20001 AND max_salary <= 30000 THEN ('Alto')
+WHEN max_salary > 30000 THEN ('Altíssimo')
 END AS 'Nível'
 FROM hr.jobs;
