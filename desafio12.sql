cb12554370ef1733275564350f9101b2fa75ddd2
emassaki <massakihirayama@gmail.com>
2021-04-20 15:03:24 -0300

    all reqs

--- desafio12.sql
+++ desafio12.sql
@@ -10,4 +10,3 @@ FROM
 WHERE E1.JOB_ID = E2.JOB_ID
         AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
-
--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial' FROM hr.employees AS E
+SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial' FROM hr.employees AS E
     INNER JOIN hr.jobs AS J ON E.JOB_ID = J.JOB_ID AND J.JOB_TITLE = cargo
     GROUP BY E.JOB_ID;
 END $$
