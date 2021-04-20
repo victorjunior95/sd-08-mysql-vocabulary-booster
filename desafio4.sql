4aac8f15a2087ee90800198dc7833d8aa27af6f9
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-20 19:00:29 -0300

    Requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,15 @@
 SELECT
 job.JOB_TITLE AS 'Cargo',
-ROUND(AVG(emp.SALARY), 2) AS 'Média Salarial',
+ROUND(AVG(emp.SALARY), 2) AS `Média Salarial`,
 CASE
 WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
 WHEN ROUND(AVG(emp.SALARY), 2) > 10500 THEN 'CEO'
-ELSE 'Não categorizado'
+-- ELSE 'CEO' 
 END AS 'Senioridade'
 FROM hr.employees AS emp
 INNER JOIN
 hr.jobs AS job ON emp.JOB_ID = job.JOB_ID
-GROUP BY emp.DEPARTMENT_ID
-ORDER BY ROUND(AVG(emp.SALARY), 2), Cargo;
+GROUP BY emp.JOB_ID
+ORDER BY `Média Salarial`, job.JOB_TITLE;
