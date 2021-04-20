bf90750ab4c04ef578a7902d5541632262006c95
emassaki <massakihirayama@gmail.com>
2021-04-20 11:50:29 -0300

    Added req 18

---
+++ desafio18.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+    DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM
+    hr.employees E
+        INNER JOIN
+    hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
