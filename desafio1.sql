6c20793b053ba904da63d0d186696ab52fbef83d
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 17:42:32 -0300

    fix : tab removed

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,3 @@
-SELECT 
-    locale.COUNTRY_NAME AS 'País',
-    IF(locale.REGION_ID = 1,
-        'incluído',
-        'não incluído') AS 'Status Inclusão'
-FROM
-    hr.countries AS locale ORDER BY locale.COUNTRY_NAME;
+SELECT locale.COUNTRY_NAME AS 'País', 
+IF(locale.REGION_ID = 1,'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS locale ORDER BY locale.COUNTRY_NAME;
--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,2 @@
-SELECT 
-  JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
-FROM
-  hr.jobs 
-ORDER BY MAX_SALARY, JOB_TITLE;
+SELECT JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo' 
+FROM hr.jobs ORDER BY MAX_SALARY, JOB_TITLE;
