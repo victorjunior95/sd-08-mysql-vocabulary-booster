be3b8318facd17971a6dda10a37ddcf41380ce07
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-20 18:22:08 -0300

    traido pelo ctrl + c ctrl + v, envia req4 novamente

--- desafio4.sql
+++ desafio4.sql
@@ -2,8 +2,8 @@ SET @media_salarial = ROUND(MAX_SALARY - MIN_SALARY, 2);
 SELECT JOB_TITLE AS 'Cargo', @media_salarial AS 'Média Salarial',
   CASE
     WHEN @media_salarial >= 2000 AND @media_salarial <= 5800 THEN 'Júnior'
-    WHEN @media_salarial >= 2000 AND @media_salarial <= 5800 THEN 'Pleno'
-    WHEN @media_salarial >= 2000 AND @media_salarial <= 5800 THEN 'Sênior'
+    WHEN @media_salarial >= 5801 AND @media_salarial <= 7500 THEN 'Pleno'
+    WHEN @media_salarial >= 7501 AND @media_salarial <= 10500 THEN 'Sênior'
     ELSE 'CEO'
   END AS 'Senioridade'
-FROM hr.jobs ORDER BY ASC @media_salarial, JOB_TITLE;
+FROM hr.jobs ORDER BY @media_salarial, JOB_TITLE;
