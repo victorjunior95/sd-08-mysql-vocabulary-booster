02e762555f8efd2f0bac0c579530971fd5a21a13
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 17:43:57 -0300

    cumprindo desafio 2

---
+++ desafio02.sql
@@ -0,0 +1,9 @@
+select job_title as Cargo,
+	case
+		when max_salary between 5000 and 10000  then 'Baixo'
+		when max_salary between 10001 and 20000  then 'Médio'
+		when max_salary between 20001 and 30000  then 'Alto'
+		else 'Altíssimo'
+	end as Nível
+from jobs
+order by job_title;
