d396e4dfea4c16a9f49dcc5af66da8fd51cc2e43
Leonardo <lecastro16@gmail.com>
2021-04-20 15:48:39 -0300

    requisito 1v2

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 SELECT c.COUNTRY_NAME AS `País`,
 CASE 
 WHEN r.REGION_NAME = 'EUROPE' THEN 'incluído'
-ELSE 'não inculuído'
+ELSE 'não incluído'
 END AS `Status Inclusão`
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r
