251feae492a84aabc08b8de70b84aa2f07a9cf15
emassaki <massakihirayama@gmail.com>
2021-04-20 14:17:22 -0300

    Added req 20

---
+++ desafio20.sql
@@ -0,0 +1,14 @@
+DELIMITER $$ 
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50))
+BEGIN 
+SELECT 
+CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+D.DEPARTMENT_NAME AS Departamento,
+J.JOB_TITLE AS Cargo FROM hr.employees AS E
+INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+INNER JOIN hr.departments AS D ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
+INNER JOIN hr.jobs AS J ON JH.JOB_ID = J.JOB_ID 
+WHERE E.EMAIL = 'NKOCHHAR'
+ORDER BY Cargo;
+END $$
+DELIMITER ;
