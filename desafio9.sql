7aec99c0a8d68a425fee582c2718443f578649ba
emassaki <massakihirayama@gmail.com>
2021-04-19 23:26:50 -0300

    Added req 10

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,9 @@
-SELECT CONCAT(FirstName, ' ', LastName) AS 'Nome completo', COUNT(*) AS 'Total de pedidos' FROM w3schools.employees AS E
-INNER JOIN w3schools.orders AS C ON E.EmployeeID = C.EmployeeID
+SELECT 
+    CONCAT(FirstName, ' ', LastName) AS 'Nome completo',
+    COUNT(*) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS E
+        INNER JOIN
+    w3schools.orders AS C ON E.EmployeeID = C.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
