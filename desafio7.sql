0ac2857c15fd537615563840a0534838ef13e5e9
Mateus Coury <mateuscoury@gmail.com>
2021-04-20 19:38:30 -0300

    6 e 7

---
+++ desafio7.sql
@@ -0,0 +1,1 @@
+SELECT UCASE(CONCAT(E.first_name,' ',E.last_name)) AS `Nome completo`,JH.start_date AS `Data de início`,E.salary AS 'Salário' FROM hr.employees AS E INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID= JH.EMPLOYEE_ID ORDER BY `Nome completo`,`Data de início`; 
