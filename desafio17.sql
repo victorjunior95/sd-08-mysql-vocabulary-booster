6db817f59107472865fb891ce32b5ef5eb577283
Luiz Reina <luizreeina@gmail.com>
2021-04-20 19:51:00 -0300

    arquivo 17 preenchido com o conteudo do desafio 10, agora corrigido

--- desafio17.sql
+++
@@ -1,8 +0,0 @@
-SELECT prod.ProductName AS 'Produto', MIN(orderDet.Quantity) AS 'Mínima',
-MAX(orderDet.Quantity) AS 'Máxima', ROUND(AVG(orderDet.Quantity), 2) AS 'Média'
-FROM w3schools.order_details orderDet
-INNER JOIN w3schools.products prod
-ON orderDet.ProductID = prod.ProductID
-GROUP BY prod.ProductName
-HAVING AVG(orderDet.Quantity) > 20.00
-ORDER BY AVG(orderDet.Quantity) , prod.ProductName;
