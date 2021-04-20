2beab0ed73537a20486cf7081c24cd834b5b16c3
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-20 17:10:49 -0300

    req 1 order by

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-select COUNTRY_NAME as País, IF(region_id = 1, 'incluído', 'não incluído' ) as `Status Inclusão` from hr.countries;
+select COUNTRY_NAME as País, IF(region_id = 1, 'incluído', 'não incluído' ) as `Status Inclusão` from hr.countries order by País;
