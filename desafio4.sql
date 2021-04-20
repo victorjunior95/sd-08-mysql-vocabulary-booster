0e89795948485a69a3a2d87a68a1e0b4bd156253
Bruno Martins <bruno.martins217@gmail.com>
2021-04-20 19:09:23 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT job.JOB_TITLE AS 'Cargo', ROUND(AVG(emp.SALARY), 2) AS `Média Salarial`,
+CASE
+WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 2000 and 5800 THEN 'Júnior'
+WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 5801 and 7500 THEN 'Pleno'
+WHEN ROUND(AVG(emp.SALARY), 2)  BETWEEN 7501 and 10500 THEN 'Sênior'
+WHEN ROUND(AVG(emp.SALARY), 2) > 10500 THEN 'CEO'
+END AS 'Senioridade'
+FROM hr.employees as emp
+INNER JOIN hr.jobs as job
+ON emp.JOB_ID = job.JOB_ID
+GROUP BY Cargo ORDER BY ROUND(AVG(emp.SALARY), 2), Cargo;
