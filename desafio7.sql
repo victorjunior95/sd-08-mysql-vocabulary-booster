8f395ab1dcafdc23c68231fb3cbc0fa96f32a1a5
albertassi88 <albertassi88@gmail.com>
2021-04-20 18:25:20 -0300

    Criacao do desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+jo.start_date AS 'Data de início do cargo',
+e.salary AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN hr.departments AS d
+ON e.department_id = d.department_id
+INNER JOIN hr.job_history AS jo
+ON e.employee_id = jo.employee_id
+WHERE MONTH(jo.start_date) IN (1,2,3)
+ORDER BY CONCAT(e.first_name, ' ', e.last_name), jo.start_date;
