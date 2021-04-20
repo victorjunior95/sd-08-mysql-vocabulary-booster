f010f69ad5a4cbeba191df1f77843d13a6e095d6
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-20 19:19:17 -0300

    req 4 ^

--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ round(avg(employees.salary), 2) as `Média salarial`,
 case 
 when avg(employees.salary) > 1999 and avg(employees.salary) < 5801 then 'Júnior'
 when avg(employees.salary) > 5800 and avg(employees.salary) < 7501 then 'Pleno'
-when avg(employees.salary) > 7500 and avg(employees.salary) < 10501 then 'Senior'
+when avg(employees.salary) > 7500 and avg(employees.salary) < 10501 then 'Sênior'
 else 'CEO'
 end as Senioridade
 from hr.jobs jobs inner join hr.employees employees
