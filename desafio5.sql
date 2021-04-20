4addadbda20c2fb80f61c45864c86523d5fd508b
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-04-20 15:21:20 -0300

    Mysql - VocBooster - Req05

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT j.JOB_TITLE AS Cargo,
+j.MAX_SALARY - j.MIN_SALARY AS 'Variação Salarial',
+ROUND((j.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+ROUND((j.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+FROM hr.jobs AS j, hr.employees AS e
+WHERE j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_ID
+ORDER BY `Variação Salarial`, Cargo;
