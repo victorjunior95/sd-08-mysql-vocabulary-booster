d33473c954b4a79c8041bf616eab61b9d359ae40
patriciavsilva <silva.patricia.v@gmail.com>
2021-04-20 20:05:37 -0300

    code climate

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
 SELECT JOB_TITLE AS 'Cargo' ,
- MAX_SALARY - MIN_SALARY  AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs
- ORDER BY JOB_TITLE;
+MAX_SALARY - MIN_SALARY  AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs
+ORDER BY JOB_TITLE;
  
