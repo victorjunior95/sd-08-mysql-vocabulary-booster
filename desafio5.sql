15c82328445e232300148c193c8e9178ebcfb9e2
Thiago Ferreira <thiagocristhian@gmail.com>
2021-04-20 19:42:23 -0300

    Requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT
 JOB_TITLE AS 'Cargo',
-(ROUND((MAX_SALARY - MIN_SALARY)/12, 2)) AS 'Variação Salarial',
+(ROUND((MAX_SALARY - MIN_SALARY)/12)) AS 'Variação Salarial',
 (ROUND(MIN_SALARY/12, 2)) AS 'Média mínima mensal',
 (ROUND(MAX_SALARY/12, 2)) AS 'Média máxima mensal'
 FROM hr.jobs
