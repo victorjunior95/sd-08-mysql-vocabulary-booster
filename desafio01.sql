eb7cf0c4152a6ea6675e7dcef3d33dfce66ddad9
rodolfo_oliveira <rod.vgo@gmail.com>
2021-04-20 17:13:26 -0300

    desafio 1

--- desafio01.sql
+++ desafio01.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS País, IF(region_id = 1, 'incluído', 'não incluído' ) AS 'Status Inclusão' FROM hr.countries;
+SELECT COUNTRY_NAME AS País, IF(region_id = 1, 'incluído', 'não incluído' ) AS 'Status Inclusão' FROM hr.countries ORDER BY 'País';
