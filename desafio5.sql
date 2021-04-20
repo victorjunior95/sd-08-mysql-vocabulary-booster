09087e1826e53538159dd60c308d0281d942c914
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 18:44:56 -0300

    fix: remove alias

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
-SELECT hrj.JOB_TITLE AS 'Cargo', 
-(hrj.MAX_SALARY - hrj.MIN_SALARY) AS 'Variação Salarial',
-ROUND(hrj.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
-ROUND(hrj.MAX_SALARY / 12, 2) AS 'Média mínima mensal'
-FROM hr.jobs AS hrj
-ORDER BY `Variação Salarial`;
+SELECT JOB_TITLE AS 'Cargo',
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
+ORDER BY `Variação Salarial`, JOB_TITLE;
