59bfbccbbccd8e8a00c95bae26ba77f65800c180
emassaki <massakihirayama@gmail.com>
2021-04-20 11:55:35 -0300

    Added req 19

---
+++ desafio19.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE quantidadeContratada INT;
+    SELECT COUNT(*) FROM hr.employees
+    WHERE YEAR(HIRE_DATE) = ano AND MONTH(HIRE_DATE) = mes
+    INTO quantidadeContratada;
+    RETURN quantidadeContratada;
+END $$
+DELIMITER ;
