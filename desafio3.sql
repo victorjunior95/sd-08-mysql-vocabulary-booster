21724bc1704438c4ce6c96546593610a62bfb481
devFelipeFranca <felipefrancafj@gmail.com>
2021-04-20 20:03:45 -0300

    UPDATE: desafioN-3-4-5-6.sql implemented by Felipe França

--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,5 @@
-SELECT * FROM hr.countries;
+SELECT 
+  JOB_TITLE AS 'Cargo',
+  MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE;
