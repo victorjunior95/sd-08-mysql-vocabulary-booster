5e29462dd176bec372e988ae5e06ece86df761bd
Leandro Lachini <leandrolachini@gmail.com>
2021-04-20 18:31:46 -0300

    task 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS `Cargo`,
-e.HIRE_DATE AS `Data de início do cargo`,
+jh.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID
+INNER JOIN hr.job_history as jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID 
 INNER JOIN hr.departments AS d
 ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
