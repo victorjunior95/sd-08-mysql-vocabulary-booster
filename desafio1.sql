44698dca45927fd09cf2924b808d2c4afbe638c3
patriciavsilva <silva.patricia.v@gmail.com>
2021-04-20 17:41:22 -0300

    code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
-SELECT COUNTRY_NAME AS 'País', 
-      CASE 
-	      WHEN REGION_ID = 1 THEN 'incluído'
-          ELSE 'não incluído'
-	  END AS 'Status Inclusão'
+SELECT COUNTRY_NAME AS 'País',
+      CASE
+      WHEN REGION_ID = 1 THEN 'incluído'
+      ELSE 'não incluído'
+      END AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY COUNTRY_NAME; 
     
