59b69c4938669e32d689d6fc2c4607447efbd9dd
emassaki <massakihirayama@gmail.com>
2021-04-19 21:54:21 -0300

    Added req 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    J.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN AVG(E.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(E.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(E.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN AVG(E.SALARY) > 10500 THEN 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.jobs AS J ON E.JOB_ID = J.JOB_ID
+GROUP BY E.JOB_ID
+ORDER BY `Média salarial` , Cargo;
