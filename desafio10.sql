3e8109a396af0258587ae8489922a8781d4316c4
Seu nome <luiza.dilly@gmail.com>
2021-04-20 18:28:03 -0300

    req 9 e 10, acho, num sei, to perdida na vida =(

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT ProductName AS `Produto`,
+MIN(od.Quantity) AS `Mínima`,
+MAX(od.Quantity) AS `Máxima`,
+ROUND(AVG(od.Quantity), 2) AS `Média`
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+GROUP BY `Produto`
+HAVING ROUND(AVG(od.Quantity), 2) > 20.00
+ORDER BY `Média`, `Produto` ASC;
