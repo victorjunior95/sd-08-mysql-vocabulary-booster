ee852a237370c7f5e525ceadf0cd82eefb8bb652
Bruno Wedley <brunowedley27@gmail.com>
2021-04-20 16:39:49 -0300

    correção de escrita nos desafios 6 e 8

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FIRST_NAME,' ', e.LAST_NAME) AS `Nome Completo`,
+SELECT CONCAT(e.FIRST_NAME,' ', e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS `Cargo`,
 jhis.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS `Departamento`
@@ -6,4 +6,4 @@ FROM hr.employees AS e
 INNER JOIN hr.job_history AS jhis ON e.EMPLOYEE_ID = jhis.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j ON jhis.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d ON jhis.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, `Cargo`; 
+ORDER BY `Nome completo` DESC, `Cargo`; 
