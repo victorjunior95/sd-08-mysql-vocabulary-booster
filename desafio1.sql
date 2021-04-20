eef497921add74a447799c7f29ae6dca48c659a1
marcos <marcosd3wow@gmail.com>
2021-04-20 15:42:01 -0300

    req1b

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,1 @@
-SELECT country_name AS "País", 
-	IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY 'País';
+SELECT country_name AS "País", IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY 'País';
