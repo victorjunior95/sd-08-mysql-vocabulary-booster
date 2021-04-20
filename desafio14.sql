7369214c0cca8ad79027b1ee31979bdd8621fe6e
Vitor Nunes <vitornunes200@gmail.com>
2021-04-20 20:26:41 -0300

    Fixed Code Climate

--- desafio14.sql
+++ desafio14.sql
@@ -1,8 +1,8 @@
 SELECT Country as 'País'
 FROM
 (
-	SELECT DISTINCT Country FROM w3schools.customers
-	UNION
-	SELECT DISTINCT Country FROM w3schools.suppliers
+SELECT DISTINCT Country FROM w3schools.customers
+UNION
+SELECT DISTINCT Country FROM w3schools.suppliers
 ) AS union_table
 ORDER BY Country LIMIT 5;
--- desafio5.sql
+++ desafio5.sql
@@ -2,5 +2,5 @@ SELECT j.JOB_TITLE AS 'Cargo',
 (j.MAX_SALARY - j.MIN_SALARY) AS 'Variação Salarial',
 ROUND((j.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
 ROUND((j.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
- FROM hr.jobs AS j
- ORDER BY (j.MAX_SALARY - j.MIN_SALARY), j.JOB_TITLE;
+FROM hr.jobs AS j
+ORDER BY (j.MAX_SALARY - j.MIN_SALARY), j.JOB_TITLE;
