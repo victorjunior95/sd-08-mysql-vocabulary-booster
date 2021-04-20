59671be2db428ff3d43e5e3e6e2f2f3e1e0c3ba1
Bruno Martins <bruno.martins217@gmail.com>
2021-04-20 17:27:55 -0300

    Desafio 3 - fix

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,2 @@
-SELECT JOB_TITLE AS 'Cago', MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo` FROM hr.jobs
+SELECT JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo` FROM hr.jobs
 ORDER BY `Diferença entre salários máximo e mínimo`;
