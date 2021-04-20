007095ff27fe0fac7bfb9f5dbcf9093e49655fd0
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-20 20:02:11 -0300

    Desafios 6-10

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT (SELECT c.ContactName FROM w3schools.customers AS c WHERE c.CustomerID = o.CustomerID) AS 'Nome de contato', (SELECT s.ShipperName FROM w3schools.shippers as s WHERE s.ShipperID = o.ShipperID) AS 'Empresa que fez o envio', o.OrderDate FROM w3schools.orders AS o WHERE (SELECT s.ShipperName FROM w3schools.shippers as s WHERE s.ShipperID = o.ShipperID) IN ('Speedy Express', 'United Package') ORDER BY (SELECT c.ContactName FROM w3schools.customers AS c WHERE c.CustomerID = o.CustomerID), (SELECT s.ShipperName FROM w3schools.shippers as s WHERE s.ShipperID = o.ShipperID), o.OrderDate;
+
+-- SELECT c.ContactName, s.ShipperName, o.OrderDate 
+-- FROM w3schools.orders AS o
+-- INNER JOIN w3schools.customers AS c
+-- INNER JOIN w3schools.shippers as s
+-- ON c.CustomerID = o.CustomerID AND s.ShipperID = o.ShipperID
+-- WHERE s.ShipperName IN ('Speedy Express', 'United Package')
+-- ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
---
+++ desafio9.sql
@@ -0,0 +1,1 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo', (SELECT COUNT(*) FROM w3schools.orders AS o WHERE o.EmployeeID = e.EmployeeID GROUP BY o.EmployeeID) AS 'Total de pedidos' FROM w3schools.employees AS e ORDER BY (SELECT COUNT(*) FROM w3schools.orders AS o WHERE o.EmployeeID = e.EmployeeID GROUP BY o.EmployeeID);
