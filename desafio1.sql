b531f1a59d61d8dbd39d1bc5c3aed503906e04e3
Paulo Simões <paulohbsimoes@gmail.com>
2021-04-20 14:36:26 -0300

    Desafio 1 concluído

---
+++ desafio1.sql
@@ -0,0 +1,11 @@
+SELECT
+  C.COUNTRY_NAME AS "País",
+  IF(
+    R.REGION_NAME = 'Europe',
+    "incluído",
+    "não incluído"
+  ) AS "Status Inclusão"
+FROM hr.countries AS C
+INNER JOIN hr.regions AS R
+ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME;
