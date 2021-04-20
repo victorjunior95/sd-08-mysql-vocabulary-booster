d248e4b6f69a73612d8a721bb9a154c6de44af7b
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-20 17:07:31 -0300

    Challenge 6 done

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+  JH.START_DATE AS `Data de início do cargo`,
+  D.DEPARTMENT_NAME
+FROM hr.employees AS E
+INNER JOIN hr.job_history AS JH
+  ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+INNER JOIN hr.departments AS D
+  ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
+ORDER BY 1 DESC;
