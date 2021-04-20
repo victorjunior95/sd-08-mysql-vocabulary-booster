23768b1f314851dfd4caaa74f2963bb561a6c7bf
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-20 16:52:27 -0300

    ESLINT

--- desafio1.sql
+++ desafio1.sql
@@ -1,10 +1,10 @@
 SELECT 
-	country.COUNTRY_NAME AS "País",
-    IF (
-		region.REGION_NAME = 'Europe',
-        "incluído",
-        "não incluído"
-        ) AS "Status Inclusão"
+country.COUNTRY_NAME AS "País",
+  IF (
+  region.REGION_NAME = 'Europe',
+      "incluído",
+      "não incluído"
+      ) AS "Status Inclusão"
 FROM hr.countries AS country
 INNER JOIN hr.regions AS region
 ON country.REGION_ID = region.REGION_ID
