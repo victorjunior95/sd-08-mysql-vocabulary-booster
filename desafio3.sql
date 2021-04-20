9b1cbf6cc70d7e3c0158b1afae189f31c7b9dacb
Uilton Rocha <itoclick@gmail.com>
2021-04-20 17:07:38 -0300

    REQ3 do projeto Vocabulary Booster

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,2 @@
-SELECT JOB_TITLE AS 'Cargo',(MAX_SALARY - MIN_SALARY)
-AS `Diferença entre salários máximo e mínimo` FROM hr.jobs 
-ORDER BY `Diferença entre salários máximo e mínimo` , `cargo`;
- 
+SELECT JOB_TITLE AS 'Cargo',(MAX_SALARY - MIN_SALARY)AS `Diferença entre salários máximo e mínimo` FROM hr.jobs ORDER BY `Diferença entre salários máximo e mínimo` , `cargo`;
+
