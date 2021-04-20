734caa5beaf9455bc2844c2eb105735118895355
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-04-20 16:32:00 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+  CONCAT(UCASE(e.FIRST_NAME), ' ', UCASE(e.LAST_NAME)) AS `Nome completo`,
+  h.START_DATE AS `Data de início`,
+  e.SALARY AS "Salário"
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS h
+ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo`;
