6cdfdf50ad5976a9df7a7390f1339ad09df62048
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-20 19:48:02 -0300

    ESLINT eita ponto e virgula

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,4 @@ SELECT
 JOB_TITLE AS "Cargo",
 MIN_SALARY - MAX_SALARY AS `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
-ORDER BY `Diferença entre salários máximo e mínimo`, Cargo
+ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
