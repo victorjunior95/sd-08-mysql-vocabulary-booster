2b0d6175385efbb55fa100cceb66a629a4389b06
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-20 17:28:36 -0300

    Challenge 7 done

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT
+  UPPER(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS `Nome completo`,
+  JH.START_DATE AS `Data de início`,
+  E.SALARY AS Salário
+FROM hr.employees AS E
+INNER JOIN hr.job_history AS JH
+  ON (E.EMPLOYEE_ID = JH.EMPLOYEE_ID)
+ORDER BY 1, 2;
