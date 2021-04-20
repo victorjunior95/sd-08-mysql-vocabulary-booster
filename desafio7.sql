66ec3f94a2a6ab3ae9a0fd4b042b34a0a06c1e4c
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-20 16:40:29 -0300

    adicionando desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT 
+    UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS `Nome completo`,
+    hist.START_DATE AS `Data de início`,
+    emp.SALARY AS `Salário`
+FROM
+    hr.employees AS emp
+        INNER JOIN
+    hr.job_history AS hist ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
+WHERE
+    MONTH(hist.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo`;
