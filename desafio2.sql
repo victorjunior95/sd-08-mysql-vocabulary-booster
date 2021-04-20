dec7d727772808dd1d38ec7dde50e13fdffc7582
Cleberton <clebertonfgc@gmail.com>
2021-04-20 16:46:07 -0300

    Ajustando lint 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
     JOB_TITLE AS 'Cargo',
     CASE
         WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
@@ -9,4 +9,3 @@ SELECT
 FROM
     hr.jobs
 ORDER BY JOB_TITLE ASC;
- 
