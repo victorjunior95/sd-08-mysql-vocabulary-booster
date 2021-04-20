db4a760be6fdfb879408f3131f7e1a28b30facbe
Leonardo <lecastro16@gmail.com>
2021-04-20 15:40:10 -0300

    requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,9 @@
-SELECT * FROM hr.countries;
+SELECT c.COUNTRY_NAME AS `País`,
+CASE 
+	WHEN r.REGION_NAME = 'EUROPE' THEN 'incluído'
+    ELSE 'não inculuído'
+END AS `Status Inclusão`
+FROM hr.countries AS c
+INNER JOIN hr.regions AS r
+ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME;
