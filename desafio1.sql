09fc248ee1c8fb111a8a84611d7c3002230285c9
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-20 16:11:58 -0300

    Challenge 1 done

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT
+  COUNTRY_NAME AS País,
+  IF(REGION_ID = (
+    SELECT REGION_ID
+    FROM regions 
+    WHERE REGION_NAME = "Europe"), "incluído", "não incluído"
+  ) AS "Status Inclusão"
+FROM hr.countries
+ORDER BY País;
