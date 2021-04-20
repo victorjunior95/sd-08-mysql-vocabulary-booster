8f590b685a3d8c3ab6236266da16709c1e71748a
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-20 15:46:05 -0300

    adicionando desafio 4 e 5

--- desafio4.sql
+++ desafio4.sql
@@ -1,25 +1,28 @@
 SELECT 
-    JOB_TITLE AS Cargo,
-    ROUND(((MAX_SALARY + MIN_SALARY) / 2), 2) AS `Média salarial`,
+    job.JOB_TITLE AS Cargo,
+    ROUND(avg(emp.SALARY),2) AS `Média salarial`,
     CASE
         WHEN
-            ((MAX_SALARY + MIN_SALARY) / 2) >= 2000
-                AND ((MAX_SALARY + MIN_SALARY) / 2) <= 5800
+            (avg(emp.SALARY)) >= 2000
+                AND (avg(emp.SALARY)) <= 5800
         THEN
             'Júnior'
         WHEN
-            ((MAX_SALARY + MIN_SALARY) / 2) >= 5801
-                AND ((MAX_SALARY + MIN_SALARY) / 2) <= 7500
+            (avg(emp.SALARY)) >= 5801
+                AND (avg(emp.SALARY)) <= 7500
         THEN
             'Pleno'
         WHEN
-            ((MAX_SALARY + MIN_SALARY) / 2) >= 7501
-                AND ((MAX_SALARY + MIN_SALARY) / 2) <= 10500
+            (avg(emp.SALARY)) >= 7501
+                AND (avg(emp.SALARY)) <= 10500
         THEN
             'Sênior'
-        WHEN ((MAX_SALARY + MIN_SALARY) / 2) > 10500 THEN 'CEO'
+        WHEN (avg(emp.SALARY)) > 10500 THEN 'CEO'
         ELSE 'não classificado'
     END AS 'Senioridade'
 FROM
-    hr.jobs
+    hr.jobs AS job
+INNER JOIN hr.employees AS emp
+ON job.JOB_ID = emp.JOB_ID
+GROUP BY (job.JOB_ID)
 ORDER BY `Média salarial` , Cargo;
