584736369a280660f1f2a1a092f986dad70e0d1f
Bruno Martins <bruno.martins217@gmail.com>
2021-04-20 16:37:04 -0300

    Desafio 1 - fix acentuacao

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluido', 'não incluido') AS `Status Inclusão` from hr.countries
+SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão` from hr.countries
 ORDER BY COUNTRY_NAME;
