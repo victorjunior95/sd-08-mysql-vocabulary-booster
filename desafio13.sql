5f603ecd6b0d68afdf6a438ea9fa7eeac9454940
Igor <igorln96@gmail.com>
2021-04-20 19:54:26 -0300

    req 12 - 14

---
+++ desafio13.sql
@@ -0,0 +1,13 @@
+SELECT (
+	SELECT p.ProductName
+	FROM w3schools.products p
+	WHERE p.ProductID = o.ProductID
+) AS Produto,
+(
+	SELECT p.Price
+	FROM w3schools.products p
+	WHERE p.ProductID = o.ProductID
+) AS Preço
+FROM w3schools.order_details o
+WHERE o.Quantity > 80
+ORDER BY Produto;
