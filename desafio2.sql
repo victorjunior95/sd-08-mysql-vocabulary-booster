00e9d1fe38b5d8b989da31c067af95a2620bfb3a
Leandro Lachini <leandrolachini@gmail.com>
2021-04-20 16:21:36 -0300

    task 1 and 2

--- desafio2.sql
+++ desafio2.sql
@@ -4,7 +4,7 @@ WHEN ( j.MAX_SALARY > 5000 AND j.MAX_SALARY <= 10000) THEN 'Baixo'
 WHEN ( j.MAX_SALARY > 10001 AND j.MAX_SALARY <= 20000) THEN 'Médio'
 WHEN ( j.MAX_SALARY > 20001 AND j.MAX_SALARY <= 30000) THEN 'Alto'
 WHEN ( j.MAX_SALARY > 30000) THEN 'Médio'
-ELSE 'Nível de saláio não associado'
+ELSE 'Nível de salário não associado'
 END AS `Nível`
 FROM hr.jobs AS j
 ORDER BY `Cargo`;
