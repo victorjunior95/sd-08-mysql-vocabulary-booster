74af40a699062731eddb627098111c25d5d7e715
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-20 20:19:34 -0300

    req 5 agora vai

--- desafio5.sql
+++ desafio5.sql
@@ -2,4 +2,4 @@ select JOB_TITLE as Cargo,
 (MAX_SALARY - MIN_SALARY) as `Variação Salarial`,
 round(MIN_SALARY / 12, 2) as `Média mínima mensal`,
 round(MAX_SALARY / 12, 2) as `Média máxima mensal`
-from hr.jobs group by Cargo order by `Variação Salarial`, Cargo;
+from hr.jobs order by `Variação Salarial`, Cargo;
