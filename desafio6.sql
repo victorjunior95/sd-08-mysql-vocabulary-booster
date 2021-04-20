27757347b2f46de871e816ac4be7e759bc83b518
victor Farias <victorfarias.tv@gmail.com>
2021-04-20 15:44:05 -0300

    Desafio 6 Correçao

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ J.JOB_TITLE AS `Cargo`,
 JH.START_DATE AS `Data de início do cargo`,
 D.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS E
-INNER JOIN hr.jobs AS J ON J.JOB_ID = E.JOB_ID
 INNER JOIN hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J ON J.JOB_ID = JH.JOB_ID
 INNER JOIN hr.departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
