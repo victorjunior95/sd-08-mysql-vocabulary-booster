8c0f65b37f2574f04cfe946a5a1ca36b9f22494c
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 17:36:02 -0300

    fix: requeriment 03

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 SELECT 
-    hrj.JOB_TITLE AS 'Cargo', CONCAT(hrj.MAX_SALARY - hrj.MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+  JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
 FROM
-    hr.jobs AS hrj
-ORDER BY hrj.MAX_SALARY, hrj.JOB_TITLE;
+  hr.jobs 
+ORDER BY MAX_SALARY, JOB_TITLE;
