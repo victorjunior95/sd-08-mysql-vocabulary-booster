8a374d6cfb04a8c8e2193eab5da7501ea86dd0c3
victorjunior95 <victorjr.muchacho@gmail.com>
2021-04-20 19:22:07 -0300

    Ultima tentativa

--- desafio4.sql
+++ desafio4.sql
@@ -19,7 +19,7 @@ WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN "Pleno"
 WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN "Sênior"
 ELSE "CEO"
 END AS `Senioridade`
-FROM hr.jobs AS j
-INNER JOIN hr.employees AS e ON j.JOB_ID = e.JOB_ID
-GROUP BY j.JOB_ID
+FROM hr.employees AS e INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID
 ORDER BY `Média Salarial`, `Cargo`;
