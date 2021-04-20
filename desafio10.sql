43e52d23f2847d0ade0b3b78977ad449fc571428
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-04-20 18:16:03 -0300

    conclui desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT P.`ProductName` AS `Produto`,
+MIN(OD.Quantity) AS `Mínima`,
+MAX(OD.Quantity) AS `Máxima`,
+ROUND(AVG(OD.Quantity), 2) AS `Média`
+FROM w3schools.order_details AS OD
+INNER JOIN w3schools.products AS P ON P.ProductID = OD.ProductID
+GROUP BY OD.ProductID
+HAVING ROUND(AVG(OD.Quantity), 2) > 20.00
+ORDER BY `Média`, `Produto`;
