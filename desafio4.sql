9d58eaebe08308645746438255f3ef75486be53c
Luiz Reina <luizreeina@gmail.com>
2021-04-20 16:59:41 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT jobs.job_title AS `Cargo`, ROUND(AVG(employ.salary), 2) AS `Média salarial`,
+CASE
+WHEN ROUND(AVG(employ.salary), 2) >= 2000 AND ROUND(AVG(employ.salary), 2) <= 5800 THEN ('Junior')
+WHEN ROUND(AVG(employ.salary), 2) > 5800 AND ROUND(AVG(employ.salary), 2) <= 7501 THEN ('Pleno')
+WHEN ROUND(AVG(employ.salary), 2) > 7501 AND ROUND(AVG(employ.salary), 2) <= 10500 THEN ('Senior')
+WHEN ROUND(AVG(employ.salary), 2) > 10500 THEN ('CEO')
+END AS `Senioridade`
+FROM hr.jobs as jobs
+INNER JOIN hr.employees as employ
+ON employ.job_id = jobs.job_id
+GROUP BY jobs.job_title
+ORDER BY `Média salarial`, Cargo;
