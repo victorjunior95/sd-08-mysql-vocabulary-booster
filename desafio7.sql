e09043f349be019ce6c98406e23a7440aca462c6
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-04-20 17:36:45 -0300

    conclui desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+jh.START_DATE AS `Data de início`,
+e.SALARY AS `Salário`
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN (1) AND (3)
+ORDER BY `Nome completo`, `Data de início`;
