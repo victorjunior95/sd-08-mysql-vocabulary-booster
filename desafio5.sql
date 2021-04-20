0e68252f5d022247acb04d6bcb9b3465490904b3
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 18:40:49 -0300

    fix: added order by

--- desafio5.sql
+++ desafio5.sql
@@ -2,4 +2,5 @@ SELECT hrj.JOB_TITLE AS 'Cargo',
 (hrj.MAX_SALARY - hrj.MIN_SALARY) AS 'Variação Salarial',
 ROUND(hrj.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
 ROUND(hrj.MAX_SALARY / 12, 2) AS 'Média mínima mensal'
-FROM hr.jobs AS hrj;
+FROM hr.jobs AS hrj
+ORDER BY `Variação Salarial`;
