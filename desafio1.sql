d7871df90c59a4f46a66160a08f8fbd49f373239
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 18:06:26 -0300

    corrigindo desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 select country_name as País,
-if(region_id = 1, 'incluido', 'não incluído') as `Status Inclusão`
+if(region_id = 1, 'incluído', 'não incluído') as `Status Inclusão`
 from hr.countries
 order by País;
