c1068939c6302de57b878515021000d691b9b74c
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-04-20 18:37:12 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+  t1.ContactName AS `Nome`,
+  t1.Country AS `País`,
+  COUNT(t2.CustomerName) AS `Número de compatriotas`
+FROM w3schools.customers AS t1, w3schools.customers AS t2
+WHERE t1.Country = t2.Country
+AND t1.CustomerID <> t2.CustomerID
+GROUP BY t1.ContactName, t2.Country
+ORDER BY `Nome`;
