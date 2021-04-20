d72a77786a55ba13d3a8f09058bc2cae28193b63
Luiz Reina <luizreeina@gmail.com>
2021-04-20 16:36:45 -0300

    substituido o padrao dos ALIAS para utilizacao sempre com crase ao inves de single quote, adicionada ordenacao por cargo no desafio 2 e desafio 3 ok

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
-SELECT country_name AS 'País', IF (regions.region_name = 'EUROPE', 'incluído', 'não incluído') AS `Status Inclusão`
+SELECT country_name AS `País`, IF (regions.region_name = 'EUROPE', 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries AS countries
 INNER JOIN hr.regions AS regions
 ON countries.region_id = regions.region_id
-ORDER BY 'País';
+ORDER BY `País`;
