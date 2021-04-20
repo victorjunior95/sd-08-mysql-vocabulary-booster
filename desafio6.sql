10a10bc137bdced081fe1b8cf932782d312b85bf
Leonardo <lecastro16@gmail.com>
2021-04-20 18:34:58 -0300

    requisito 6v1

--- desafio6.sql
+++ desafio6.sql
@@ -5,7 +5,9 @@ FROM hr.employees AS e
 RIGHT JOIN hr.job_history AS h
 ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
+ON h.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d
 ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
+
+SELECT * FROM hr.job_history;
