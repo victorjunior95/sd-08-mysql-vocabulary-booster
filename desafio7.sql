0e337166590dea8c07eabd0a83d5612a588657c1
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 21:38:40 -0300

    fix: added month

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ hre.SALARY AS 'Salário'
 FROM hr.employees AS hre
 INNER JOIN hr.job_history AS hrjh
 ON hre.EMPLOYEE_ID = hrjh.EMPLOYEE_ID
+WHERE MONTH(hrjh.START_DATE) IN(1, 2, 3) 
 ORDER BY `Nome completo`, `Data de início`;
