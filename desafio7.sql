8d05ec154a4c1a28a1ac6994fe2288aec4a86392
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-20 23:03:47 -0300

    Desafio 7 correçao5 - nao e possivel que um c me lascou

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
 -- SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome Completo` FROM hr.employees;-- UPPER no lugar do ucase
 SELECT
-UCASE(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS `Nome Completo`,
+UCASE(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS `Nome completo`,
 jhistory.START_DATE AS `Data de início`,
 employee.SALARY AS "Salário"
 FROM hr.employees AS employee
 INNER JOIN hr.job_history AS jhistory
 ON employee.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
-WHERE MONTH(jhistory.START_DATE) IN (01, 02, 03) -- Porque não between 01 e 12?
-ORDER BY `Nome Completo`, `Data de início`;
+WHERE MONTH(jhistory.START_DATE) IN (01, 02, 03)
+ORDER BY `Nome completo`, `Data de início`;
