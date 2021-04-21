53f8cca548c0617eb87b70773ae70789ce4c2b54
Digo-Castro <dygo.castro@gmail.com>
2021-04-20 21:33:11 -0300

    [Desafio 11] - OK

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT c.ContactName AS 'Nome',
+c.Country AS 'País',
+(sub.Qnt - 1) AS 'Número de compatriotas'
+FROM (
+SELECT Country, COUNT(*) AS 'Qnt'
+FROM w3schools.customers
+GROUP BY (Country)) AS sub
+INNER JOIN w3schools.customers AS c
+ON c.Country = sub.Country
+WHERE (sub.Qnt -1) > 0
+ORDER BY 1;
