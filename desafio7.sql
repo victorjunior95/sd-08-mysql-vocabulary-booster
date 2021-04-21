a81449f1a60281e738228431ea2408d58aa72b4a
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-20 22:58:05 -0300

    Desafio 7 correçao4

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
--- SELECT UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome Completo` FROM hr.employees;
+-- SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome Completo` FROM hr.employees;-- UPPER no lugar do ucase
 SELECT
-UPPER(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS `Nome Completo`,
+UCASE(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS `Nome Completo`,
 jhistory.START_DATE AS `Data de início`,
 employee.SALARY AS "Salário"
 FROM hr.employees AS employee
 INNER JOIN hr.job_history AS jhistory
 ON employee.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
-WHERE MONTH(jhistory.START_DATE) IN (01, 02, 03)
+WHERE MONTH(jhistory.START_DATE) IN (01, 02, 03) -- Porque não between 01 e 12?
 ORDER BY `Nome Completo`, `Data de início`;
