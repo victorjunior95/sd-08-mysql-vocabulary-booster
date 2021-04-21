9b4a9a96ffc52c870a3e2e110f5ce1788562972b
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 21:22:57 -0300

    feat: requeriment 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT wp.ProductName AS 'Produto', 
+MIN(wod.Quantity) AS 'Mínima', 
+MAX(wod.Quantity) AS'Máxima',
+ROUND(AVG(wod.Quantity), 2) AS 'Média'
+FROM w3schools.products AS wp
+INNER JOIN w3schools.order_details AS wod
+ON wp.ProductID = wod.ProductID
+GROUP BY `Produto`
+ORDER BY `Média`, `Produto`;
