c5912d26b7e817e4020f7c0510b8e08058a37da9
Erick-Marinho <erickgaves@gmail.com>
2021-04-21 00:41:50 -0300

    corrigindo desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
 select concat(ucase(e.first_name),' ', ucase(e.last_name)) as `Nome completo`,
-month(start_date) between 1 and 3 as `Data de início`,
+jh.start_date as `Data de início`,
 salary as `Salário`
-from employees e
-inner join job_history jh
+from hr.employees e
+inner join hr.job_history jh
 on e.job_id = jh.job_id
+where month(jh.start_date) in (01,02,03)
 order by `Nome Completo`, start_date;
