4760be7b85f05ce6392ee1a49ffcabe69ed71e57
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-20 22:43:43 -0300

    Desafio 7 correçao2

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
 -- SELECT UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome Completo` FROM hr.employees;
 SELECT
 UPPER(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS `Nome Completo`,
-jhistory.START_DATE AS `Data de início do cargo`,
+jhistory.START_DATE AS `Data de início`,
 employee.SALARY AS "Salário"
 FROM hr.employees AS employee
 INNER JOIN hr.job_history AS jhistory
 ON employee.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
 WHERE MONTH(jhistory.START_DATE) BETWEEN 01 AND 12
-ORDER BY `Nome Completo`;
+ORDER BY `Nome Completo`, `Data de início`;
