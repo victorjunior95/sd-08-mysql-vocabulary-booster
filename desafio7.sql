03951580b14642bb51c47f90fd39863c1529f274
caiomzrocha <caiomzrocha@gmail.com>
2021-04-21 18:28:01 -0300

    [REQ-07][FIX] Corrige a data de início do cargo.

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,9 @@
 SELECT
-  UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
-  HIRE_DATE AS `Data de início`,
-  SALARY AS `Salário`
-FROM hr.employees
-WHERE MONTH(HIRE_DATE) IN (1, 2, 3)
+  UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`,
+  job_history.START_DATE AS `Data de início`,
+  employees.SALARY AS `Salário`
+FROM hr.employees AS employees
+INNER JOIN hr.job_history AS job_history
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+WHERE MONTH(START_DATE) IN (1, 2, 3)
 ORDER BY `Nome completo`, DATE(`Data de início`);
