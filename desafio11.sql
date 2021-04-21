bc0e61d5eef29cbc0ecf04539e5ed9c10db5da7a
Aline Debastiani <debastianialine@gmail.com>
2021-04-21 14:52:56 -0300

    cumpre desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+c1.ContactName AS 'Nome',
+c1.Country AS 'País',
+COUNT(c2.ContactName) AS 'Número de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+AND c1.ContactName <> c2.ContactName
+GROUP BY c1.ContactName, c1.Country
+ORDER BY `Nome`;
