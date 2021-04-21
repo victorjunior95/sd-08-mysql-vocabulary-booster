5b7bacf8e33b8e848691d78400d1fac9d5521147
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-04-21 11:59:24 -0300

    corrige desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,7 +1,6 @@
 SELECT
   p.ProductName AS `Produto`,
-  p.Price AS `Preço`,
-  o.Quantity
+  p.Price AS `Preço`
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o
 ON p.ProductID = o.ProductID
