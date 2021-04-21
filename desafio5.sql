e7895766219b3bcd6497954edbc72402f38f84b6
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 21:53:08 -0300

    corrigindo desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 select job_title as 'Cargo', 
 max_salary - min_salary as `Variação Salarial`,
-truncate(avg(min_salary),2) as `Média mínima mensal`,
-truncate(avg(max_salary),2) as `Média máxima mensal`
+truncate(avg(min_salary / 12),2) as `Média mínima mensal`,
+truncate(avg(max_salary / 12),2) as `Média máxima mensal`
 from jobs
 group by Cargo
 order by `Variação Salarial`, Cargo;
