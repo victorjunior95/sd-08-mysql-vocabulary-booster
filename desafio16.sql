f154653b7a750cc92e73fd910080f44fff520474
Fernanda PC <nandateixmarques@gmail.com>
2021-04-21 15:17:41 -0300

    adicionando requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_funcionario VARCHAR(200))
+RETURNS VARCHAR(200) READS SQL DATA
+BEGIN
+    DECLARE quantidadeDeEmpregos INT;
+    SELECT COUNT(JH.EMPLOYEE_ID)
+	FROM hr.job_history AS JH
+	INNER JOIN hr.employees AS E
+	ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+	WHERE E.EMAIL = email_funcionario INTO quantidadeDeEmpregos;
+    RETURN quantidadeDeEmpregos;
+END $$
+
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
