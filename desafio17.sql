e1cf6e1085c06d40b0a328717b524523b994feaf
Clênio Galvão <clenio.cgm@gmail.com>
2021-04-21 13:33:11 -0300

    Resolve desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE TRIGGER trigger_order_insert
+    BEFORE INSERT ON w3schools.orders
+    FOR EACH ROW
+BEGIN
+    SET NEW.OrderDate = NOW();
+END $$
+DELIMITER ;
+
+INSERT INTO w3schools.orders(CustomerID, EmployeeID, ShipperID)
+VALUES(4, 2, 2);
+
+SELECT * FROM w3schools.orders
+WHERE CustomerID = 4;
