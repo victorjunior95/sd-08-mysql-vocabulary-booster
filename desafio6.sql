793c7f0b59c1ccc6d96d43eee4255bbe7600133f
rodolfo_oliveira <rod.vgo@gmail.com>
2021-04-20 23:10:23 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ j.job_title AS `Cargo`,
 jbh.start_date AS `Data de início do cargo`,
 d.department_name AS `Departamento`
 FROM hr.employees AS e 
-INNER JOIN hr.job_history AS jbh ON e.employee_id = jbh.employee_id
 INNER JOIN hr.jobs AS j ON jbh.job_id = j.job_id 
 INNER JOIN hr.departments AS d ON jbh.department_id = d.department_id 
+INNER JOIN hr.job_history AS jbh ON e.employee_id = jbh.employee_id
 ORDER BY `Nome completo` DESC, `Cargo`;
