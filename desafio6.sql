0bdda7785b85055a35e970accad73e9d450b79f5
albertassi88 <albertassi88@gmail.com>
2021-04-21 16:43:48 -0300

    Alteracoes no desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,10 @@ j.job_title AS 'Cargo',
 jo.start_date AS 'Data de início do cargo',
 d.department_name AS 'Departamento'
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON e.job_id = j.job_id
-INNER JOIN hr.departments AS d
-ON e.department_id = d.department_id
 INNER JOIN hr.job_history AS jo
-ON e.employee_id = jo.employee_id
-ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.job_title;
+ON jo.employee_id = e.employee_id
+INNER JOIN hr.jobs AS j
+ON j.job_id = jo.job_id
+INNER JOIN hr.departments AS d
+ON d.department_id = jo.department_id
+ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.job_title; 
