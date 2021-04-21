2c6daa2f4606dc7dbe6a6fbb3f84577f790b6d4f
albertassi88 <albertassi88@gmail.com>
2021-04-21 16:21:56 -0300

    Criacao do desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER ordersInsert
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
+BEGIN
+SET NEW.OrderDate = NOW(),
+NEW.CustomerID = 'INSERT',
+NEW.EmployeeID = 'INSERT',
+NEW.ShipperID = 'INSERT';
+END $$
+DELIMITER ;
