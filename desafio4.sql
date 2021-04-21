48ad8ebccb12a5924c9e0aa8502b0f13a12fb523
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 21:04:09 -0300

    corrigindo desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
 select j.job_title as 'Cargo', round(avg(e.salary), 2) as `Média salarial`,
 case 
-when e.salary between 2000 and 5800 then 'Júnior'
-when e.salary between 5801 and 7500 then 'Pleno'
-when e.salary between 7501 and 10500 then 'Sênior'
+when avg(e.salary) between 2000 and 5800 then 'Júnior'
+when avg(e.salary) between 5801 and 7500 then 'Pleno'
+when avg(e.salary) between 7501 and 10500 then 'Sênior'
 else 'CEO'
 end as Senioridade 
 from hr.jobs j
 inner join hr.employees e
 on j.job_id = e.job_id
-group by e.salary
-order by (e.salary), Cargo;
+group by Cargo
+order by `Mádia Salarial`, Cargo;
