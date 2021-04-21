d428e54cb368f2aa58d33c5300a529b452a1b7fe
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-21 18:03:49 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,1 @@
+SELECT c1.ContactName AS 'Nome', c1.Country AS 'País', (COUNT(c2.ContactName) - 1) AS 'Número de compatriotas' FROM w3schools.customers AS c1, w3schools.customers AS c2 WHERE c1.Country = c2.Country GROUP BY c1.ContactName HAVING (COUNT(c2.ContactName) - 1) > 0 ORDER BY c1.ContactName;
