0750b78bc1a45541b16d2ac6d31cec60b7a15655
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-04-20 22:35:03 -0300

    FIX: DESAFIO 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT
   UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-  e.HIRE_DATE AS 'Data de início',
+  j.START_DATE AS 'Data de início',
   e.SALARY AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS j
