440cb910ec060d8ab39cc659be9effc697dd2cbe
Clênio Galvão <clenio.cgm@gmail.com>
2021-04-21 09:08:08 -0300

    Corrige desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT
 t1.ContactName AS `Nome`,
-t1.Country AS `País`,
-COUNT(*) AS `Número de compatriotas`
+t2.Country AS `País`,
+(COUNT(*) - 1) AS `Número de compatriotas`
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
-GROUP BY t1.ContactName
+GROUP BY t1.ContactName, t1.Country HAVING (COUNT(*) - 1) > 0
 ORDER BY 1;
