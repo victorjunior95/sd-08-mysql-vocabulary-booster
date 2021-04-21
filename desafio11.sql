2d64cc7dd86122fc2fd6158668ee0f510d10903b
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-04-21 08:28:53 -0300

    fix2: desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT
   c2.ContactName AS 'Nome',
-  c1.Country AS 'País',
+  c2.Country AS 'País',
   COUNT(*) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country
-GROUP BY c2.ContactName
+GROUP BY c2.CustomerID
 ORDER BY `Nome`;
