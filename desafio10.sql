36cea5628c1fbbe760c8263670b344481f961c1a
devFelipeFranca <felipefrancafj@gmail.com>
2021-04-21 08:34:44 -0300

    UPDATE: desafioN-8-9-10.sql implemented by Felipe França

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,11 @@
-SELECT * FROM hr.countries;
+SELECT
+  P.ProductName AS 'Produto',
+  MIN(OD.Quantity) AS 'Mínima',
+  MAX(OD.Quantity) AS 'Máxima',
+  ROUND(AVG(OD.Quantity), 2) AS 'Média'
+FROM w3schools.order_details AS OD
+INNER JOIN w3schools.products AS P
+ON OD.ProductID = P.ProductID
+GROUP BY OD.ProductID
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git status
 git add --all
-git commit -am "UPDATE: desafio7.sql implemented by Felipe França"
+git commit -am "UPDATE: desafioN-8-9-10.sql implemented by Felipe França"
 git push
