78f1f58b3b09c2abefab43493c9edacd9be01824
Fernanda PC <nandateixmarques@gmail.com>
2021-04-20 23:00:56 -0300

    corrigindo requisito 6 - agora vai

--- desafio6.sql
+++ desafio6.sql
@@ -4,10 +4,10 @@ J.JOB_TITLE AS `Cargo`,
 JH.START_DATE AS `Data de início do cargo`,
 D.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS E
-INNER JOIN hr.jobs AS J
-ON E.JOB_ID = J.JOB_ID
 INNER JOIN job_history AS JH
 ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J
+ON JH.JOB_ID = J.JOB_ID
 INNER JOIN hr.departments AS D
 ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Nome completo` ASC;
