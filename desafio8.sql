8d7f2b9f2f701ec0bcc22b5bb1f827aaee766366
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 14:48:08 -0300

    Corrigindo erro de logica

--- desafio8.sql
+++ desafio8.sql
@@ -4,4 +4,5 @@ O.OrderDate AS 'Data do pedido'
 FROM w3schools.orders AS O
 INNER JOIN w3schools.customers AS C ON O.CustomerID = C.CustomerID
 INNER JOIN w3schools.shippers AS S ON O.ShipperID = S.ShipperID
+WHERE S.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY C.ContactName ASC, S.ShipperName ASC, O.OrderDate ASC;
