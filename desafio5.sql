cfa319dcce0ccaacb93fa994e33ead255d1683c8
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-04-20 22:01:03 -0300

    fix: desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,7 @@
 SELECT
-  j.JOB_TITLE AS 'Cargo',
-  (MAX(e.SALARY) - MIN(e.SALARY)) AS 'Variação Salarial',
-  ROUND(MIN(e.SALARY) / 12, 2) AS 'Média mínima mensal',
-  ROUND(MAX(e.SALARY) / 12, 2) AS 'Média máxima mensal'
-FROM hr.jobs AS j
-INNER JOIN hr.employees AS e
-ON j.JOB_ID = e.JOB_ID
-GROUP BY e.JOB_ID
+  JOB_TITLE AS 'Cargo',
+  (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+  ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+  ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
 ORDER BY `Variação Salarial`, `Cargo`;
