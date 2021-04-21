2797ffc39f4cfc9bbeb7c567894593d6fc31f576
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 14:42:55 -0300

    Enviando desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT C.ContactName AS 'Nome de contato',
+S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS O
+INNER JOIN w3schools.customers AS C ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers AS S ON O.ShipperID = S.ShipperID
+ORDER BY C.ContactName ASC, S.ShipperName ASC, O.OrderDate ASC;
