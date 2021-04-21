2fb37d170e55a0ad7b22013deb8e4938790f3493
devFelipeFranca <felipefrancafj@gmail.com>
2021-04-21 09:18:41 -0300

    UPDATE: desafioN-11-12-13-14.sql implemented by Felipe França

--- desafio14.sql
+++ desafio14.sql
@@ -1,1 +1,7 @@
-SELECT * FROM hr.countries;
+SELECT Country as 'País' FROM (
+  SELECT Country FROM w3schools.customers
+  UNION
+  SELECT Country FROM w3schools.suppliers
+) AS Países
+ORDER BY `País`
+LIMIT 5;
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git status
 git add --all
-git commit -am "UPDATE: desafioN-8-9-10.sql implemented by Felipe França"
+git commit -am "UPDATE: desafioN-11-12-13-14.sql implemented by Felipe França"
 git push
