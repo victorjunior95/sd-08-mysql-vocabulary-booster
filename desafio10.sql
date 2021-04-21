3047e1b3223b47f4850b0116e34e73a201abad0a
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 17:02:23 -0300

    Corrigindo erro de logica

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,7 @@
-SELECT P.ProductName, M.Mínima, M.Máxima, ROUND(`Média`, 2) AS `Média` FROM (
+SELECT P.ProductName AS 'Produto' , M.Mínima, M.Máxima, ROUND(`Média`, 2) AS `Média` FROM (
 SELECT ProductID, MIN(Quantity) AS 'Mínima', MAX(Quantity) AS 'Máxima',
 AVG(Quantity) AS `Média` FROM w3schools.order_details GROUP BY ProductID
 ) AS M
 INNER JOIN w3schools.products AS P ON P.ProductID = M.ProductID
 WHERE `Média` > 20 ORDER BY `Média`, P.ProductName;
+
