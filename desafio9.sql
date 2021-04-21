d6f5d34270ca0980d902427e61ea69e86defc8fe
bidfca <bruninhakjurugat11@gmail.com>
2021-04-21 18:17:32 -0300

    Desafio 9 Fix

--- desafio9.sql
+++ desafio9.sql
@@ -3,5 +3,9 @@ COUNT(*) AS 'Total de Pedidos'
 FROM w3schools.employees employee
 INNER JOIN w3schools.orders `order`
 ON employee.EmployeeID = `order`.EmployeeID
+WHERE EXISTS (
+SELECT * FROM w3schools.employees
+WHERE `order`.EmployeeID = employee.EmployeeID
+)
 GROUP BY `Nome completo`
 ORDER BY COUNT(*);
