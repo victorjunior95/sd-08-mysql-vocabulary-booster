5aee743a0300a5f6e6d0231faa2033aab249ea54
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 22:10:29 -0300

    corrigindo desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,4 @@ select job_title as 'Cargo',
 round(min_salary / 12,2) as `Média mínima mensal`,
 round(max_salary / 12,2) as `Média máxima mensal`
 from hr.jobs
-group by Cargo
 order by `Variação Salarial`, Cargo;
