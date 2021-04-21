e5f67028fd8c3f964149b6d58488665b4bf318e1
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-04-21 17:25:05 -0300

    c5 - fix lint

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,8 @@
-SELECT
-	JOB_TITLE AS 'Cargo',
-	MAX_SALARY,
+SELECT JOB_TITLE AS 'Cargo', MAX_SALARY,
     CASE
-		WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
-        WHEN MAX_SALARY > 20000 THEN 'Alto'
-        WHEN MAX_SALARY > 10000 THEN 'Médio'
-        ELSE 'Baixo'
+	WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+    WHEN MAX_SALARY > 20000 THEN 'Alto'
+    WHEN MAX_SALARY > 10000 THEN 'Médio'
+    ELSE 'Baixo'
 	END AS 'Nível'
 FROM hr.jobs;
