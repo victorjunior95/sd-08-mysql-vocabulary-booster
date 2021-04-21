e7b67235f8e8fc0d2b31392f042e87c7387d3322
Fernanda PC <nandateixmarques@gmail.com>
2021-04-21 16:26:37 -0300

    adicionando requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+	SELECT ROUND(AVG(E.SALARY), 2) AS `Média salarial`
+	FROM hr.jobs AS J
+	INNER JOIN hr.employees AS E
+	ON J.JOB_ID = E.JOB_ID
+	WHERE JOB_TITLE = cargo
+    GROUP BY J.JOB_TITLE;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
