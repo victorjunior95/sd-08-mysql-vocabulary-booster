940e360d0d9d0706f5971d1e8f5702a016d117c1
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 16:52:44 -0300

    Corrigindo espacamento

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT P.ProductName, M.Mínima, M.Máxima, ROUND(`Média`, 2) AS `Média` FROM (
 SELECT ProductID, MIN(Quantity) AS 'Mínima', MAX(Quantity) AS 'Máxima',
- AVG(Quantity) AS `Média` FROM w3schools.order_details GROUP BY ProductID
+AVG(Quantity) AS `Média` FROM w3schools.order_details GROUP BY ProductID
 ) AS M
 INNER JOIN w3schools.products AS P ON P.ProductID = M.ProductID
 WHERE `Média` > 20 ORDER BY `Média`, P.ProductName;
