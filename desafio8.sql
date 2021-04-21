1c22d6b3523691343fa1d39974bcf0d8cc48cb80
Cleberton <clebertonfgc@gmail.com>
2021-04-21 17:45:39 -0300

    Requisito 08

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+    tb_cust.ContactName AS 'Nome de contato',
+    tb_shp.ShipperName,
+    tb_ord.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS tb_cust
+        INNER JOIN
+    w3schools.orders AS tb_ord ON tb_cust.CustomerID = tb_ord.CustomerID
+        INNER JOIN
+    w3schools.shippers AS tb_shp ON tb_ord.ShipperID = tb_shp.ShipperID
+WHERE
+    tb_shp.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY tb_cust.ContactName ASC , tb_shp.ShipperName ASC , tb_ord.OrderDate ASC;
