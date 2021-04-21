b90a97c8c4dc48634280ad4d1a2e27de9032fe5f
bidfca <bruninhakjurugat11@gmail.com>
2021-04-20 21:06:38 -0300

    Eureka

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,12 @@ SELECT CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS 'Nome completo',
 job.JOB_TITLE AS 'Cargo',
 jhistory.START_DATE AS 'Data de início do cargo',
 department.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees employee
+FROM hr.job_history jhistory 
 INNER JOIN hr.jobs job
-ON employee.JOB_ID = job.JOB_ID
-INNER JOIN hr.job_history jhistory
-ON employee.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
+ON jhistory.JOB_ID = job.JOB_ID
+INNER JOIN hr.employees employee
+ON jhistory.EMPLOYEE_ID = employee.EMPLOYEE_ID
 INNER JOIN hr.departments department
-ON employee.DEPARTMENT_ID = department.DEPARTMENT_ID
+ON jhistory.DEPARTMENT_ID = department.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, job.JOB_TITLE;
+
