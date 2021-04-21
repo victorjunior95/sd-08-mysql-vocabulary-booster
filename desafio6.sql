168b4df86daf9dd979a3d6a972e663d1f0280fc9
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-04-21 11:09:03 -0300

    adiciona alias correto ao departamento desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS `Cargo`,
 jh.START_DATE AS `Data de início do cargo`,
-d.DEPARTMENT_NAME
+d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
