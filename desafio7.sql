6db9c932b7e34f7a6a02a0f1314be21e2fe2d3d7
bidfca <bruninhakjurugat11@gmail.com>
2021-04-20 21:18:13 -0300

    Do Not Copy And Paste

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 SELECT UCASE(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS 'Nome completo',
-jhistory.START_DATE AS 'Data de início do cargo',
+jhistory.START_DATE AS 'Data de início',
 employee.SALARY AS 'Salário'
 FROM hr.job_history jhistory 
 INNER JOIN hr.employees employee
 ON jhistory.EMPLOYEE_ID = employee.EMPLOYEE_ID
 WHERE MONTH(jhistory.START_DATE) IN (01,02,03)
-ORDER BY `Nome completo` DESC, jhistory.START_DATE;
+ORDER BY `Nome completo`, jhistory.START_DATE;
