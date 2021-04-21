592835b2dc2c92898bcc65c693a7c308b6765c28
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-21 11:02:55 -0300

    adicionando desafio 12

---
+++ desafio12.sql
@@ -0,0 +1,13 @@
+SELECT 
+    CONCAT(emp1.FIRST_NAME, ' ', emp1.LAST_NAME) AS `Nome completo funcionário 1`,
+    emp1.SALARY AS `Salário funcionário 1`,
+    emp1.PHONE_NUMBER AS `Telefone funcionário 1`,
+    CONCAT(emp2.FIRST_NAME, ' ', emp2.LAST_NAME) AS `Nome completo funcionário 2`,
+    emp2.SALARY AS `Salário funcionário 2`,
+    emp2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM
+    hr.employees AS emp1
+        INNER JOIN
+    hr.employees AS emp2 ON emp1.JOB_ID = emp2.JOB_ID
+        AND emp1.EMPLOYEE_ID <> emp2.EMPLOYEE_ID
+ORDER BY `Nome completo funcionário 1` , `Nome completo funcionário 2`;
