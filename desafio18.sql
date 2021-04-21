c3aea4de93adb8bdb0e77da90bae88799fba3e00
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-04-21 00:31:21 -0300

    conclui desafio 18

---
+++ desafio18.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT CONCAT( E.`FIRST_NAME`, ' ', E.`LAST_NAME` ) AS `Nome completo`,
+DATE_FORMAT(JH.`START_DATE`, '%d%/%m%/%Y') AS `Data de início`,
+DATE_FORMAT(JH.`END_DATE`, '%d%/%m%/%Y')  AS `Data de rescisão`,
+(ROUND((DATEDIFF(JH.`END_DATE`, JH.`START_DATE`) / 365), 2)) AS `Anos trabalhados`
+FROM hr.`employees` AS E
+INNER JOIN hr.`job_history` AS JH ON JH.`EMPLOYEE_ID` = E.`EMPLOYEE_ID`
+ORDER BY `Nome completo`, `Anos trabalhados`;
