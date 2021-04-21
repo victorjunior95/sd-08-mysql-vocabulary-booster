dc16a12f5e1fb7107986b3c686fa1fab9c91589e
Leonardo <lecastro16@gmail.com>
2021-04-21 14:01:24 -0300

    requisito 17 v1

--- desafio17.sql
+++ desafio17.sql
@@ -2,10 +2,10 @@ USE w3schools;
 
 DELIMITER $$
 CREATE TRIGGER update_date
-	BEFORE INSERT ON orders
-	FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END
 
 $$ DELIMITER ;
