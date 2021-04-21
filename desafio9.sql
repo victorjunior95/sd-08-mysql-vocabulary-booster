930a66549563c9ede1e102be07f53612e1381e3f
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-04-20 23:24:51 -0300

    fix: desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,8 +1,8 @@
 SELECT
-  CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
-  COUNT(*) AS 'Total de Pedidos'
+  CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+  COUNT(*) AS 'Total de pedidos'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS e
 ON o.EmployeeID = e.EmployeeID
 GROUP BY e.EmployeeID
-ORDER BY `Total de Pedidos`;
+ORDER BY `Total de pedidos`;
