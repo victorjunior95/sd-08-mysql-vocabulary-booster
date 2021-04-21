c00d34ad14693cae0a5d468cac1160e7b81c0440
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-20 22:51:58 -0300

    req 6 cargo cargo concat

--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,4 @@ departments.DEPARTMENT_NAME as Departamento from hr.employees employees
 inner join hr.jobs jobs on jobs.JOB_ID = employees.JOB_ID 
 inner join hr.job_history as `history` on jobs.JOB_ID = history.JOB_ID
 inner join hr.departments departments on history.DEPARTMENT_ID = departments.DEPARTMENT_ID
-order by `Nome completo` desc, Cargo asc;
+order by concat(employees.FIRST_NAME, ' ', employees.LAST_NAME) desc, Cargo asc;
