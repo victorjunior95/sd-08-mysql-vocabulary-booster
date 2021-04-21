219213de43b5618e63d910edf462dcc271949a28
Bruno Martins <bruno.martins217@gmail.com>
2021-04-20 23:08:56 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+cus.ContactName AS `Nome`,
+cus.Country AS `País`,
+COUNT(cus2.Country) - 1 AS `Número de compatriotas`
+FROM
+w3schools.customers AS cus,
+w3schools.customers AS cus2
+WHERE
+cus.Country = cus2.Country
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;
