07e12384bfc115d55ed50d5459efb9f2d4562657
victor718 <victorfelipeoliveira1@gmail.com>
2021-04-21 17:41:34 -0300

    req 10, 11, 12

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT C.ContactName AS "Nome",
+C.Country AS "País",
+(SELECT COUNT(C3.Country) - 1
+FROM w3schools.customers AS C3
+WHERE C3.Country = C.Country
+GROUP BY C3.Country) AS Número de compatriotas"
+FROM w3schools.customers AS C
+WHERE (SELECT COUNT(C3.Country)
+FROM w3schools.customers AS C3
+WHERE C3.Country = C.Country
+GROUP BY C3.Country) >= 2
+ORDER BY C.ContactName;
