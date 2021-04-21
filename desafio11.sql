17a3d74c3784bb98bd6472969440efada0c65537
victor718 <victorfelipeoliveira1@gmail.com>
2021-04-21 17:51:53 -0300

    fix req 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,7 +3,7 @@ C.Country AS "País",
 (SELECT COUNT(C3.Country) - 1
 FROM w3schools.customers AS C3
 WHERE C3.Country = C.Country
-GROUP BY C3.Country) AS Número de compatriotas"
+GROUP BY C3.Country) AS "Número de compatriotas"
 FROM w3schools.customers AS C
 WHERE (SELECT COUNT(C3.Country)
 FROM w3schools.customers AS C3
