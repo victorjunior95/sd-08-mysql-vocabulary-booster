d75246da2434b7f1b9f50985a24f03e6bd845f13
Lucas-PC <lucasmvnascimento@gmail.com>
2021-04-21 15:01:09 -0300

    Reqs 15 - 17

---
+++ desafio15.sql
@@ -0,0 +1,13 @@
+USE hr;
+DELIMITER $$
+  CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+  BEGIN
+    SELECT 
+	ROUND(AVG(e.SALARY),2) AS `Média Salarial`
+	FROM hr.employees AS e
+	INNER JOIN hr.jobs AS j
+	ON e.JOB_ID = j.JOB_ID
+	GROUP BY j.JOB_TITLE
+	HAVING j.JOB_TITLE = cargo;
+  END $$
+DELIMITER ;
---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+  CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+    RETURNS INT READS SQL DATA
+    BEGIN
+      DECLARE quantidade_empregos INT;
+      SELECT 
+	  COUNT(e.EMPLOYEE_ID)
+      FROM employees AS e
+      RIGHT JOIN job_history AS jh
+      ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+      GROUP BY e.EMAIL
+      HAVING e.EMAIL = email
+      INTO quantidade_empregos;
+      RETURN quantidade_empregos;
+    END
+$$ DELIMITER ;
