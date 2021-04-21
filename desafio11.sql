25fd5ba4f74cb9f44707672697d727ec64e62e9e
Igor <igorln96@gmail.com>
2021-04-21 16:27:11 -0300

    fixing issues

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
     c1.ContactName AS Nome,
     c1.Country AS País,
-    co.Total - 1 AS `Número de compatriotas`
+    IF(co.Total - 1 = 0, 0, co.Total - 1) AS `Número de compatriotas`
 FROM
     w3schools.customers AS c1
         LEFT JOIN
--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,5 @@
-(SELECT DISTINCT
-    (Country)
+(SELECT Country AS País
 FROM
-    w3schools.customers) UNION (SELECT DISTINCT
-    (Country)
+    w3schools.customers) UNION (SELECT Country AS País
 FROM
-    w3schools.suppliers) ORDER BY Country LIMIT 5;
+    w3schools.suppliers) ORDER BY País LIMIT 5;
