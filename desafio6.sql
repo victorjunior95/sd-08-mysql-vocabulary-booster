dd2f46c3664f816f5560218e4c40b4c4e93361a5
caiomzrocha <caiomzrocha@gmail.com>
2021-04-21 18:19:54 -0300

    [REQ-06][FIX] Corrige a data de início do cargo.

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,13 @@
 SELECT
   CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
   jobs.JOB_TITLE AS `Cargo`,
-  employees.HIRE_DATE AS `Data de início do cargo`,
+  job_history.START_DATE AS `Data de início do cargo`,
   departments.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS employees
+INNER JOIN hr.job_history AS job_history
+ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
 INNER JOIN hr.jobs AS `jobs`
-ON employees.JOB_ID = jobs.JOB_ID
+ON job_history.JOB_ID = jobs.JOB_ID
 INNER JOIN hr.departments AS departments
-ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
+ON job_history.DEPARTMENT_ID = departments.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
