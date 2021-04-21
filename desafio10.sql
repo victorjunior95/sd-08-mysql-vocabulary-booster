13cdc2a19b24cfb45b0e17a78e27978ac46dd982
Fernanda PC <nandateixmarques@gmail.com>
2021-04-21 11:37:27 -0300

    adicionando requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT 
+P.ProductName AS `Produto`,
+MIN(OD.Quantity) as `Mínima`,
+MAX(OD.Quantity) as `Máxima`,
+ROUND(AVG(OD.Quantity)) as `Média`
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS OD
+ON OD.ProductID = P.ProductID
+GROUP BY P.ProductName
+ORDER BY `Média`, `Produto`;
