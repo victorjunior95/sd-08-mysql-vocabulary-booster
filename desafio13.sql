a20d92b17a56e1e563017e6ea478d78cc6103e12
Leonardo <lecastro16@gmail.com>
2021-04-21 11:32:01 -0300

    requisito 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT p.ProductName AS `Produto`, p.Price AS `Preço`
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+WHERE od.Quantity > 80
+GROUP BY od.ProductID
+ORDER BY `Produto`;
