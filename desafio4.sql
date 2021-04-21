5f94483acd2318a01e59defac3c95da170dde424
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-04-21 17:13:59 -0300

    c4 - req4 ok

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT 
+	j.JOB_TITLE AS 'Cargo', 
+    ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
+    CASE
+		WHEN ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(e.SALARY), 2) <= 10500 THEN 'Sênior'
+        ELSE 'CEO'
+	END AS 'Senioridade'
+    FROM hr.employees AS e
+    INNER JOIN hr.jobs AS j
+    ON e.JOB_ID = j.JOB_ID
+    GROUP BY Cargo
+    ORDER BY ROUND(AVG(e.salary), 2), Cargo;
