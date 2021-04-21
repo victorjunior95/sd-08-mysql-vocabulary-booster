d14908d1bbc29ab90c858462b79469102c7b84f2
albertassi88 <albertassi88@gmail.com>
2021-04-21 14:16:53 -0300

    Criacao do desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT p.ProductName AS 'Produto',
+p.Price AS 'Preço'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS o
+ON p.ProductID = o.ProductID
+WHERE o.Quantity > 80
+ORDER BY p.ProductName;
