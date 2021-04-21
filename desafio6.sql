0a9154125914f18bbf9facc5245cfae320c14008
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-20 23:43:12 -0300

    req 6 employee_id

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 select concat(employees.FIRST_NAME, ' ', employees.LAST_NAME) as `Nome completo`,
 jobs.JOB_TITLE as Cargo, history.START_DATE as `Data de início do cargo`,
 departments.DEPARTMENT_NAME as Departamento from hr.employees employees 
-inner join hr.jobs jobs on jobs.JOB_ID = employees.JOB_ID 
-inner join hr.job_history as `history` on jobs.JOB_ID = history.JOB_ID
+inner join hr.job_history as `history` on employees.EMPLOYEE_ID = history.EMPLOYEE_ID
+inner join hr.jobs jobs on jobs.JOB_ID = history.JOB_ID 
 inner join hr.departments departments on history.DEPARTMENT_ID = departments.DEPARTMENT_ID
 order by concat(employees.FIRST_NAME, ' ', employees.LAST_NAME) desc, Cargo asc;
