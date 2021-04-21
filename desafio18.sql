2f2c19e93d9128c4e1387ab7578e66d13c809ef3
Leonardo <lecastro16@gmail.com>
2021-04-21 14:41:57 -0300

    requisito 18 v2

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME,' ', e.LAST_NAME) AS `Nome completo`, 
-CONCAT(DAY(h.START_DATE),'/',LPAD(MONTH(h.START_DATE),2,0),'/',YEAR(h.START_DATE)) AS `Data de início`,
-CONCAT(DAY(h.END_DATE),'/',LPAD(MONTH(h.END_DATE),2,0),'/',YEAR(h.END_DATE)) AS `Data de rescisão`,
+CONCAT(LPAD(DAY(h.START_DATE),2,0),'/',LPAD(MONTH(h.START_DATE),2,0),'/',YEAR(h.START_DATE)) AS `Data de início`,
+CONCAT(LPAD(DAY(h.END_DATE),2,0),'/',LPAD(MONTH(h.END_DATE),2,0),'/',YEAR(h.END_DATE)) AS `Data de rescisão`,
 ROUND((DATEDIFF(h.END_DATE, h.START_DATE)/365),2) AS `Anos trabalhados`
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e
