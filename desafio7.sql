b09fc8df822ba0ef79f0ac341369858b642a66ab
Bruno Martins <bruno.martins217@gmail.com>
2021-04-20 21:26:35 -0300

    Desafio 7-fix-

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UPPER(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS `Nome completo`, 
-MONTH(jhis.START_DATE) AS `Data de início`,
+jhis.START_DATE AS `Data de início`,
 emp.SALARY AS `Salário`
 FROM
 hr.employees AS emp,
