00aa0abaf224162a2d349564d60d0323de788ec3
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 14:11:29 -0300

    Verificando codeClimate

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
- SELECT CONCAT(UCASE(E.first_name), ' ', UCASE(E.last_name)) AS `Nome completo`,
- JH.start_date AS `Data de início`,
- E.salary AS 'Salário'
- FROM hr.job_history AS JH 
- INNER JOIN hr.employees AS E ON JH.employee_id = E.employee_id
- ORDER BY `Nome completo`, `Data de início` ASC; 
+SELECT CONCAT( UCASE( E.first_name ), ' ', UCASE( E.last_name )) AS `Nome completo`,
+JH.start_date AS `Data de início`,
+E.salary AS 'Salário'
+FROM hr.job_history AS JH 
+INNER JOIN hr.employees AS E ON JH.employee_id = E.employee_id
+ORDER BY `Nome completo`, `Data de início` ASC; 
