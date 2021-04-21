a718646e8dea2858555197b53cb37116c0784050
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 18:45:41 -0300

    Enviando desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees
+WHERE JOB_ID IN(SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = cargo);
+END $$
+DELIMITER ;
