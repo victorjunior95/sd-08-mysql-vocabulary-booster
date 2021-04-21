40cb239e320657cce0f0367cbbf315fec6d9ee3a
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 14:19:19 -0300

    Corrigindo erro de Logica

--- desafio7.sql
+++ desafio7.sql
@@ -3,4 +3,5 @@ JH.start_date AS `Data de início`,
 E.salary AS 'Salário'
 FROM hr.job_history AS JH 
 INNER JOIN hr.employees AS E ON JH.employee_id = E.employee_id
+WHERE MONTH(JH.start_date) IN (01, 02, 03)
 ORDER BY `Nome completo`, `Data de início` ASC; 
