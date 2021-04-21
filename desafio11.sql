944112d2544594cf8c39a976e5b217e389614274
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-20 23:13:45 -0300

    fix: added order by customer

--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,5 @@ wc.Country AS 'País',
 COUNT(wc2.Country) AS 'Número de compatriotas' 
 FROM w3schools.customers AS wc, w3schools.customers AS wc2
 WHERE wc.ContactName <> wc2.ContactName AND wc.Country = wc2.Country
-GROUP BY `Nome`
+GROUP BY wc.CustomerID
 ORDER BY `Nome`;
