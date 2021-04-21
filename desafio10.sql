94b0406163a7310ebae6c03761559011ab3bc57c
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-04-21 03:18:36 -0300

    C2 - 6th-10th req sent. FIX - 4th req fixed

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS 'Produto',
+MIN(d.Quantity) AS 'Mínima',
+MAX(d.Quantity) AS 'Máxima',
+ROUND(AVG(d.Quantity), 2) AS 'Média'
+FROM w3schools.order_details AS d
+INNER JOIN w3schools.products AS p ON d.ProductID = p.ProductID
+GROUP BY d.ProductID
+HAVING ROUND(AVG(d.Quantity), 2) > 20
+ORDER BY ROUND(AVG(d.Quantity), 2), p.ProductName;
---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+h.START_DATE AS 'Data de início',
+e.SALARY AS 'Salário'
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) IN (1, 2, 3)
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), h.START_DATE;
