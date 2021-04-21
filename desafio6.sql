8465dfa61d496ea51ecd19af2345cfecc7bb9152
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 22:45:45 -0300

    corrigindo desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -6,4 +6,5 @@ from employees e
 inner join jobs j
 on e.job_id = j.job_id
 inner join departments dpt
-on dpt.department_id = e.department_id; 
+on dpt.department_id = e.department_id
+order by `Nome completo`, `Cargo`; 
