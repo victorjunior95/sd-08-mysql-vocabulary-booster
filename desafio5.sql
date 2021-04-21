1bd5adc70e6fd48ae0f212dcd962525949775c2e
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-04-21 18:33:08 -0300

    Corrigindo Desafio 5 Retirando Alias

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
-SELECT J.job_title AS `Cargo`, 
-ROUND((J.max_salary - J.min_salary), 2)
-AS `Variação Salarial`,
-ROUND(J.min_salary/12, 2) AS `Média mínima mensal`,
-ROUND(J.max_salary/12, 2) AS `Média máxima mensal`
-FROM hr.jobs AS J
+SELECT
+JOB_TITLE AS `Cargo`,
+MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs
 ORDER BY `Variação Salarial`, `Cargo`;
