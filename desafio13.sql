7e132795f7fdf269cac9e90a7b92f1c2b17fa8f4
Digo-Castro <dygo.castro@gmail.com>
2021-04-21 00:53:06 -0300

    [Desafio 13] - OK

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS 'Produto',
+p.Price AS 'Preço'
+FROM (
+SELECT ProductID
+FROM w3schools.order_details
+WHERE Quantity > 80) AS sub
+INNER JOIN w3schools.products AS p
+ON p.ProductID = sub.ProductID
+ORDER BY 1;
