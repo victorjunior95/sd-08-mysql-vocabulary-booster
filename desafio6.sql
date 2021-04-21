98c0a3bfdcd6f3f21bd3111ba7c39ade879d389e
Erick-Marinho <erickgaves@gmail.com>
2021-04-20 22:37:51 -0300

    corrigindo desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 select concat(e.first_name, ' ', e.last_name) as `Nome completo`,
 j.job_title as `Cargo`,
-hire_date as `Data de início do cargo`,
-department_name as `Departamento`
+e.hire_date as `Data de início do cargo`,
+dpt.department_name as `Departamento`
 from employees e
 inner join jobs j
 on e.job_id = j.job_id
