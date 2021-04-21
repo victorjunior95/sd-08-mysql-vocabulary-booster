5a0e5ea3efb3a8fc3d665c4e451430468ae42bf1
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-20 22:36:27 -0300

    req 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+select concat(employees.FIRST_NAME, ' ', employees.LAST_NAME) as `Nome completo`,
+jobs.JOB_TITLE as Cargos, history.START_DATE as `Data de início do cargo`,
+departments.DEPARTMENT_NAME as Departamento from hr.employees employees 
+inner join hr.jobs jobs on jobs.JOB_ID = employees.JOB_ID 
+inner join hr.job_history as `history` on jobs.JOB_ID = history.JOB_ID
+inner join hr.departments departments on history.DEPARTMENT_ID = departments.DEPARTMENT_ID
+order by `Nome completo` desc, Cargos asc;
