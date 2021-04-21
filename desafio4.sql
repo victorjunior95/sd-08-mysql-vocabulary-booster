ba45709d862ac58f0a6815bb18e000bd57ec1adc
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-04-20 21:02:46 -0300

    fix: desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -4,11 +4,12 @@ SELECT
   CASE
     WHEN AVG(e.SALARY) >= 2000 AND AVG(e.SALARY) <= 5800 THEN 'Júnior'
     WHEN AVG(e.SALARY) >= 5801 AND AVG(e.SALARY) <= 7500 THEN 'Pleno'
-	  WHEN AVG(e.salary) >= 7501 AND AVG(e.salary) <= 10500 THEN 'Sênior'
+	WHEN AVG(e.salary) >= 7501 AND AVG(e.salary) <= 10500 THEN 'Sênior'
     WHEN AVG(e.salary) > 10500 THEN 'CEO'
     ELSE 'default'
   END AS 'Senioridade'
 FROM hr.jobs AS j
 INNER JOIN hr.employees AS e
 ON j.JOB_ID = e.JOB_ID
-GROUP BY e.JOB_ID;
+GROUP BY e.JOB_ID
+ORDER BY `Média salarial`, `Cargo`;
