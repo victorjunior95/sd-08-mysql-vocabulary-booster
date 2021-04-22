e516e367cabbe4592e373c2865f2b74e74e03623
amandaos <amanda.775.souza@gmail.com>
2021-04-22 12:11:43 -0300

    fix: refatora desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,9 @@
 SELECT PRO.productname AS 'Produto',
-ORDD.Mínima,
-ORDD.Máxima,
-ROUND(`Média`, 2) AS `Média`
-FROM (SELECT ProductID,
-MIN(Quantity) AS 'Mínima',
-MAX(Quantity) AS 'Máxima',
-AVG(Quantity) AS `Média`
-FROM w3schools.order_details GROUP BY ProductID) AS ORDD
+MIN(ORDD.quantity) AS 'Mínima',
+MAX(ORDD.quantity) AS 'Máxima',
+ROUND(AVG(ORDD.quantity),2) AS 'Média'
+FROM w3schools.order_details AS ORDD
 INNER JOIN w3schools.products AS PRO ON ORDD.ProductID = PRO.ProductID
-WHERE `Média` > 20
-ORDER BY `Média`, `Produto`;
+group by PRO.productname
+having AVG(ORDD.quantity) > 20
+ORDER BY `Média` ASC, `Produto` ASC;
