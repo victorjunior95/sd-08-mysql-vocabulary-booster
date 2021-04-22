83947c7e0ec47bb7e2425934e77f4612bc7daa4b
JRGCast <joao.ralph1@gmail.com>
2021-04-22 10:42:11 -0300

    verificar se AS está causando o dito erro de sintaxe, após, verificar se seria a crase

--- desafio3.sql
+++ desafio3.sql
@@ -3,6 +3,11 @@ SELECT
     JOB_TITLE AS `Cargo`,
     (MAX_SALARY - MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
 FROM
-    hr.jobs AS job
-ORDER BY `Diferença entre salários máximo e mínimo`, job.JOB_TITLE
-;
+    hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE;
+
+-- SELECT 
+--   JOB_TITLE AS 'Cargo',
+--   MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+-- FROM hr.jobs
+-- ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE;
