e8c6bc673ccb5bfb9272c5c5e02397d057089f6d
JRGCast <joao.ralph1@gmail.com>
2021-04-22 10:28:11 -0300

    apesar da merda do resultado igual, invertida a ordem de regions e country, pra imitar o projeto alheio, já que a merda do teste github fala em 'erro de sintaxe'

--- desafio1.sql
+++ desafio1.sql
@@ -5,8 +5,8 @@ SELECT
         'incluído',
         'não incluído') AS `Status Inclusão`
 FROM
-    hr.regions AS rs
+    hr.countries AS co
         INNER JOIN
-    hr.countries AS co ON co.REGION_ID = rs.REGION_ID
+    hr.regions AS rs ON co.REGION_ID = rs.REGION_ID
 ORDER BY co.COUNTRY_NAME
 ;
