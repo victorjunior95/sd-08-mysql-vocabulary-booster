c1fab1f3b486dd362156ebebb52b69d194a3f753
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-22 14:33:20 -0300

    Desafio 15 - Tentativa-3

--- desafio15.sql
+++ desafio15.sql
@@ -1,5 +1,5 @@
 USE hr;
 
-DELIMITER $$ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(35)) BEGIN SELECT AVG(e.SALARY) AS 'Média salarial' FROM hr.employees AS e GROUP BY e.JOB_ID HAVING e.JOB_ID = (SELECT JOB_ID FROM hr.jobs AS j WHERE JOB_TITLE = cargo);
+DELIMITER $$ CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(35)) BEGIN SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial' FROM hr.employees AS e GROUP BY e.JOB_ID HAVING e.JOB_ID = (SELECT JOB_ID FROM hr.jobs AS j WHERE JOB_TITLE = cargo);
 
 END $$ DELIMITER ;
