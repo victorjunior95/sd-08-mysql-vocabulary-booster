f6b7528dcd9c0efdad63c97f1d6ce06cfc9a5662
Breenolf <blf@ic.ufal.br>
2021-04-22 16:48:43 -0300

    :package: Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT jobs.JOB_TITLE AS Cargo,
+ROUND(AVG(func.SALARY), 2) AS `Média Salarial`,
+CASE
+  WHEN ROUND(AVG(func.SALARY), 2) <= 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(func.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(func.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(func.SALARY), 2) > 10500 THEN 'CEO'
+END AS Senioridade
+FROM hr.employees AS func
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = func.JOB_ID
+GROUP BY Cargo
+ORDER BY `Média Salarial`, Cargo;
