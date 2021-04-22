09394c12f281c28e7368d734645fa1642b4d049a
JRGCast <joao.ralph1@gmail.com>
2021-04-22 11:40:37 -0300

    testando req 8 pra ver o curioso erro de sintaxe e colocar no fórum

--- desafio8.sql
+++ desafio8.sql
@@ -1,28 +1,28 @@
--- USE w3schools;
--- SELECT 
---     c.ContactName AS `Nome de Contato`,
---     sh.ShipperName AS `Empresa que fez o envio`,
---     o.OrderDate AS `Data do pedido`
--- FROM
---     `customers` AS c
---         INNER JOIN
---     `orders` AS o ON c.CustomerID = o.CustomerID
---         INNER JOIN
---     shippers AS sh ON o.ShipperID = sh.ShipperID
--- WHERE
---     sh.ShipperName = 'Speedy Express'
---         OR 'United Package'
--- ORDER BY `Nome de Contato` , `Empresa que fez o envio` , `Data do pedido`
--- ;
+USE w3schools;
+SELECT 
+    c.ContactName AS `Nome de Contato`,
+    sh.ShipperName AS `Empresa que fez o envio`,
+    o.OrderDate AS `Data do pedido`
+FROM
+    `customers` AS c
+        INNER JOIN
+    `orders` AS o ON c.CustomerID = o.CustomerID
+        INNER JOIN
+    shippers AS sh ON o.ShipperID = sh.ShipperID
+WHERE
+    sh.ShipperName = 'Speedy Express'
+        OR 'United Package'
+ORDER BY `Nome de Contato` , `Empresa que fez o envio` , `Data do pedido`
+;
 
-SELECT
-  C.ContactName AS 'Nome de contato',
-  S.ShipperName AS 'Empresa que fez o envio',
-  O.OrderDate AS 'Data do pedido'
-FROM w3schools.orders AS O
-INNER JOIN w3schools.customers AS C
-ON O.CustomerID = C.CustomerID
-INNER JOIN w3schools.shippers AS S
-ON O.ShipperID = S.ShipperID
-WHERE S.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY `Nome de contato`, S.ShipperName, O.OrderDate;
+-- SELECT
+--   C.ContactName AS 'Nome de contato',
+--   S.ShipperName AS 'Empresa que fez o envio',
+--   O.OrderDate AS 'Data do pedido'
+-- FROM w3schools.orders AS O
+-- INNER JOIN w3schools.customers AS C
+-- ON O.CustomerID = C.CustomerID
+-- INNER JOIN w3schools.shippers AS S
+-- ON O.ShipperID = S.ShipperID
+-- WHERE S.ShipperName IN ('Speedy Express', 'United Package')
+-- ORDER BY `Nome de contato`, S.ShipperName, O.OrderDate;
