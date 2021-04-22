a2bf7e7d426919049378184391e958fd9b866aaf
José Bernardo Johann <josebernardojohann@gmail.com>
2021-04-22 20:27:33 -0300

    Fixing syntax errors

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,9 @@
 SELECT Products.ProductName AS Produto,
-MIN(Orders_Details.Quantity) AS 'Mínima',
-MAX(Orders_Details.Quantity) AS 'Máxima',
-ROUND(AVG(Orders_Details.Quantity), 2) AS 'Média'
+MIN(Order_Details.Quantity) AS 'Mínima',
+MAX(Order_Details.Quantity) AS 'Máxima',
+ROUND(AVG(Order_Details.Quantity), 2) AS 'Média'
 FROM w3schools.products AS Products
-INNER JOIN w3schools.order_details AS Order_Details ON Products.ProductID = Orders_Details.ProductID
+INNER JOIN w3schools.order_details AS Order_Details ON Products.ProductID = Order_Details.ProductID
 GROUP BY Products.ProductID
-HAVING 'Média' > 20
+HAVING `Média` > 20
 ORDER BY `Média`, Produto;
