4d00c2961e174733591b6a7fc00e2c836f274534
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-22 19:47:21 -0300

    Desafio 11 correçao2

--- desafio11.sql
+++ desafio11.sql
@@ -2,9 +2,10 @@ SELECT * FROM w3schools.customers;
 SELECT
 t1.ContactName AS "Nome",
 t1.Country AS "País",
-(COUNT(*) - 1) AS `Número de compatriotas` -- Não pode contar a pessoa!
+COUNT(*) AS `Número de compatriotas` -- Não pode contar a pessoa!
 FROM  w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
--- AND t1.ContactName <> t2.ContactName
-GROUP BY t1.ContactName, t1.Country
+AND t1.ContactName <> t2.ContactName
+GROUP BY t1.ContactName, t1.Country 
+-- (usar os dois pois dava problema de agregação)
 ORDER BY 1;
