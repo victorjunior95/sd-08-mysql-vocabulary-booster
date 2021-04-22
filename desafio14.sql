337fbdb9b0cbca222d67e0ad81e72fb90c2a7d60
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-04-22 17:44:12 -0300

    fix 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,4 @@
-SELECT Country FROM w3schools.suppliers
+SELECT Country AS 'País' FROM w3schools.suppliers
 UNION
 SELECT Country FROM w3schools.customers
-ORDER BY Country LIMIT 5;
+ORDER BY País LIMIT 5;
