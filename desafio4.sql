a1e8eb31b4cad0bf2ca42d768ec9db0c5bf5e56b
Uilton Rocha <itoclick@gmail.com>
2021-04-22 01:29:07 -0300

    REQ4 do projeto Vocabulary Booster

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-select jobs.JOB_TITLE as 'CARGO', round(avg(emp.SALARY), 2) as 'Média salarial',
+select jobs.JOB_TITLE as 'Cargo', round(avg(emp.SALARY), 2) as 'Média salarial',
 case
 when ROUND(AVG(emp.SALARY), 2) between 2000 and 5800 then 'Júnior'
 when ROUND(AVG(emp.SALARY), 2) between 5801 and 7500 then 'Pleno'
@@ -8,4 +8,4 @@ end as `Senioridade`
 from hr.jobs as jobs
 inner join hr.employees as emp 
 on jobs.JOB_ID = emp.JOB_ID
-group by CARGO order by `Média salarial`, jobs.JOB_TITLE;
+group by Cargo order by `Média salarial`, jobs.JOB_TITLE;
