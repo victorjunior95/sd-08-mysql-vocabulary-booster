c408e23f0c3d3dd991ab0784209f9e8dc166c081
Uilton Rocha <itoclick@gmail.com>
2021-04-22 14:05:18 -0300

    REQ6 do projeto Vocabulary Booster

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
-select CONCAT(e.FIRST_NAME ,' ', e.LAST_NAME) as 'Nome completo', j.JOB_TITLE as 'Cargo',
- h.START_DATE as 'Data de início do cargo', dep.DEPARTMENT_NAME AS 'Departamento'
+select  CONCAT(e.FIRST_NAME ,' ', e.LAST_NAME) as 'Nome completo', j.JOB_TITLE as 'Cargo',
+h.START_DATE as 'Data de início do cargo', dep.DEPARTMENT_NAME AS 'Departamento'
 from hr.employees as e
-inner join hr.jobs as j on e.JOB_ID = j.JOB_ID
-inner join hr.job_history as h on h.JOB_ID = e.JOB_ID
+inner join hr.jobs as j on j.JOB_ID = e.JOB_ID 
+inner join hr.job_history as h on h.DEPARTMENT_ID = e.DEPARTMENT_ID
 inner join hr.departments as dep on dep.DEPARTMENT_ID = h.DEPARTMENT_ID
-order by `Nome completo`, `Cargo` asc;
+order by `Nome completo` desc, `Cargo` asc;
  
