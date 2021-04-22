b96b9cb194316671a780e1ec75a90350c3b02f0e
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-04-22 14:51:44 -0300

    done second

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,9 @@
-SELECT * FROM sakila.actor;
+SELECT 
+    COUNTRY_NAME AS País,
+    CASE
+        WHEN REGION_id = 1 THEN 'incluído'
+        ELSE 'não incluído'
+    END AS 'Status Inclusão'
+FROM
+    hr.countries
+ORDER BY COUNTRY_NAME;
