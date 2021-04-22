9a5a6d8320f71e0f6cb65643635d5165fed9fe05
Daniel <marcio.daniel@msn.com>
2021-04-22 03:17:44 -0300

    Testing Format

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,4 @@
-SELECT 
-    JOB_TITLE AS 'Cargo',
+SELECT JOB_TITLE AS 'Cargo',
     ROUND(AVG(SALARY), 2) AS 'Média salarial',
     CASE
         WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
@@ -7,9 +6,8 @@ SELECT
         WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
         ELSE 'CEO'
     END AS 'Senioridade'
-FROM
-    hr.jobs AS jobs
-        INNER JOIN
-    hr.employees AS empl ON empl.JOB_ID = jobs.JOB_ID
+FROM hr.jobs AS jobs
+    INNER JOIN hr.employees AS emp ON emp.JOB_ID = jobs.JOB_ID
 GROUP BY Cargo
-ORDER BY `Média Salarial` , Cargo;
+ORDER BY `Média Salarial`,
+    Cargo;
