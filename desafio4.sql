a1a61308456006be2f198970c0572b4793f05692
Daniel Leon <daniel.radialista@gmail.com>
2021-04-22 20:44:54 -0300

    requisito 4, reenviando, passando normal nos testes locais

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT j.job_title AS 'Cargo', ROUND(AVG(e.salary), 2) AS 'Média salarial',
+SELECT j.job_title AS Cargo, ROUND(AVG(e.salary), 2) AS 'Média salarial',
 CASE
 WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -9,4 +9,4 @@ FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON e.job_id = j.job_id
 GROUP BY j.job_title
-ORDER BY  e.salary, j.job_title;
+ORDER BY  `Média salarial`, Cargo;
