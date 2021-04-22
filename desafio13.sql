ae8e856ab2564a1e280d6f38cf93e70061267757
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-04-22 17:17:12 -0300

    req 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT prod.ProductName AS 'Produto', prod.Price AS 'Preço'
+FROM w3schools.products AS prod
+INNER JOIN w3schools.order_details AS ord
+ON prod.ProductID = ord.ProductID
+WHERE ord.Quantity > 80
+ORDER BY prod.ProductName;
