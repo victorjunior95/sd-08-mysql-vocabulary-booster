89c96ed93d289f3ba77adb44c0de90c14ea74911
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-04-22 10:08:19 -0300

    adicionando desafio 16 CC

--- desafio16.sql
+++ desafio16.sql
@@ -3,16 +3,16 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE jobsQuantity INT;
-	SELECT 
-    COUNT(*)
+DECLARE jobsQuantity INT;
+SELECT 
+  COUNT(*)
 FROM
-    hr.job_history AS job
-        INNER JOIN
-    hr.employees AS emp ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
+  hr.job_history AS job
+      INNER JOIN
+  hr.employees AS emp ON job.EMPLOYEE_ID = emp.EMPLOYEE_ID
 WHERE
-    emp.EMAIL = email
-    INTO jobsQuantity;
-    RETURN jobsQuantity;
+  emp.EMAIL = email
+  INTO jobsQuantity;
+  RETURN jobsQuantity;
 END $$
 DELIMITER ;
