b700e239ac794a0746fbc9d2a45e6e172a5712fc
Cleberton <clebertonfgc@gmail.com>
2021-04-21 21:12:05 -0300

    Requisito 11 Finalizado

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,10 @@
 SELECT
-    t1.ContactName AS 'Nome',
-    t2.Country AS 'País',
-    (COUNT(t2.Country) - 1) AS 'Número de compatriotas'
+    t1.ContactName, t2.Country, COUNT(t1.Country)
 FROM
     w3schools.customers AS t1,
     w3schools.customers AS t2
 WHERE
     t1.Country = t2.Country
-GROUP BY t2.ContactName
-ORDER BY t2.ContactName;
+        AND t1.ContactName <> t2.ContactName
+GROUP BY t1.ContactName
+ORDER BY t1.ContactName;
