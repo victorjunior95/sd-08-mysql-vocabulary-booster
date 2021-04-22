cf598e79e9408a1f37d50b9f859746d6380c28f9
JRGCast <joao.ralph1@gmail.com>
2021-04-22 11:06:24 -0300

    entao eu desisto, copiar tudo dos projetos de quem passou já que a unica diferença é o USE e o alias, porque o resultado é o mesmo, o execution plan tem o mesmíssimo uso, enfim, atinge o objetivo, mas se a trybe quer que façamos tudo igual, porque não copiar logo certo?

--- desafio3.sql
+++ desafio3.sql
@@ -1,14 +1,14 @@
-USE hr;
-SELECT 
-    job.JOB_TITLE AS `Cargo`,
-    (job.MAX_SALARY - job.MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
-FROM
-    hr.jobs AS job
-ORDER BY `Diferença entre salários máximo e mínimo`, job.JOB_TITLE
-;
-
+-- USE hr;
 -- SELECT 
---   JOB_TITLE AS 'Cargo',
---   MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
--- FROM hr.jobs
--- ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE;
+--     job.JOB_TITLE AS `Cargo`,
+--     (job.MAX_SALARY - job.MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
+-- FROM
+--     hr.jobs AS job
+-- ORDER BY `Diferença entre salários máximo e mínimo`, job.JOB_TITLE
+-- ;
+
+SELECT 
+  JOB_TITLE AS 'Cargo',
+  MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE;
