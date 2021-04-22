cd7fed94c25a588fe8f94efe1d05325e012986ec
Aline Debastiani <debastianialine@gmail.com>
2021-04-22 15:21:10 -0300

    cumpre desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(40))
+BEGIN
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = cargo;
+END $$
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
