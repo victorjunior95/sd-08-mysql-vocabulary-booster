bd7375f2da9172a5489adc1fae706e88c72210d7
caiomzrocha <caiomzrocha@gmail.com>
2021-04-22 03:16:30 -0300

    [REQ-13] Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80.

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+  products.ProductName AS `Produto`,
+  products.Price AS `Preço`
+FROM w3schools.products AS `products`
+INNER JOIN w3schools.order_details AS `order_details`
+  ON products.ProductID = order_details.ProductID
+WHERE order_details.Quantity > 80
+ORDER BY `Produto`;
