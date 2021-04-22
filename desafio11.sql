df1dacea2383e2ba90ebeea0609f612bdad15838
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-22 19:36:10 -0300

    Desafio 11 correçao - faltava nao contar a nacionalidade da pessoa que estava sendo analizada

--- desafio11.sql
+++ desafio11.sql
@@ -2,8 +2,9 @@ SELECT * FROM w3schools.customers;
 SELECT
 t1.ContactName AS "Nome",
 t1.Country AS "País",
-COUNT(*) AS `Número de compatriotas`
+(COUNT(*) - 1) AS `Número de compatriotas` -- Não pode contar a pessoa!
 FROM  w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
+-- AND t1.ContactName <> t2.ContactName
 GROUP BY t1.ContactName, t1.Country
 ORDER BY 1;
