b74d4a344a36b1f6e70366b399484ed7e4fb72cd
JRGCast <joao.ralph1@gmail.com>
2021-04-22 10:31:19 -0300

    literalmente copiado o desafio1 do projeto alheio

--- desafio1.sql
+++ desafio1.sql
@@ -1,12 +1,24 @@
-USE hr;
-SELECT 
-    co.COUNTRY_NAME AS `País`,
-    IF(rs.REGION_NAME = 'Europe',
-        'incluído',
-        'não incluído') AS `Status Inclusão`
-FROM
-    hr.countries AS co
-        INNER JOIN
-    hr.regions AS rs ON co.REGION_ID = rs.REGION_ID
-ORDER BY co.COUNTRY_NAME
-;
+-- USE hr;
+-- SELECT 
+--     co.COUNTRY_NAME AS `País`,
+--     IF(rs.REGION_NAME = 'Europe',
+--         'incluído',
+--         'não incluído') AS `Status Inclusão`
+-- FROM
+--     hr.countries AS co
+--         INNER JOIN
+--     hr.regions AS rs ON co.REGION_ID = rs.REGION_ID
+-- ORDER BY co.COUNTRY_NAME
+-- ;
+
+SELECT
+  C.COUNTRY_NAME AS "País",
+  IF(
+    R.REGION_NAME = 'Europe',
+    "incluído",
+    "não incluído"
+  ) AS "Status Inclusão"
+FROM hr.countries AS C
+INNER JOIN hr.regions AS R
+ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME;
