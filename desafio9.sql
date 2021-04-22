b765d73c4038839c639cf2f634636245298dbbbe
Leandro Lachini <leandrolachini@gmail.com>
2021-04-22 20:11:36 -0300

    task 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,17 +1,8 @@
 SELECT
-*
--- CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
--- COUNT(od.Quantity) AS `Total de pedidos`
+CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS e
 ON o.EmployeeID = e.EmployeeID
-INNER JOIN w3schools.order_details AS od
-ON o.OrderID = od.OrderID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
-
--- SELECT * FROM w3schools.customers;
--- SELECT * FROM w3schools.orders;
--- SELECT * FROM w3schools.shippers;
--- SELECT * FROM w3schools.employees;
--- SELECT COUNT(od.Quantity) FROM w3schools.order_details AS od;
