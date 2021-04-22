0111b99c9913c358699e572f4c59540288372707
caiomzrocha <caiomzrocha@gmail.com>
2021-04-22 02:25:14 -0300

    [REQ-11] Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente.

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT
+  ContactName AS `Nome`,
+  Country AS `País`,
+  (SELECT
+    Count(Country)
+  FROM w3schools.customers AS `compatriots`
+  WHERE customers.Country = compatriots.Country)
+  AS `Número de compatriotas`
+FROM w3schools.customers AS `customers`
+ORDER BY `Nome`;
