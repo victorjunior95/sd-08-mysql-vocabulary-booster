449ad46df030463c7dc31a571573c1fea75de41e
Uilton Rocha <itoclick@gmail.com>
2021-04-22 19:36:36 -0300

    REQ6 do projeto Vocabulary Booster

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
 select jobs.JOB_TITLE as 'Cargo', (jobs.MAX_SALARY - jobs.MIN_SALARY) as 'Variação Salarial',
 round((jobs.MIN_SALARY / 12) , 2) as 'Média mínima mensal',
-round((jobs.MAX_SALARY / 12 ), 2) as 'Média maxima mensal'
+round((jobs.MAX_SALARY / 12 ), 2) as 'Média máxima mensal'
 from hr.jobs as jobs  order by`Variação Salarial`, Cargo;
