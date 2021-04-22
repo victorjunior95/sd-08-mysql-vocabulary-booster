f86f7526acc9792de7345e497d87ab46aded521d
José Bernardo Johann <josebernardojohann@gmail.com>
2021-04-22 20:16:59 -0300

    Fixing syntax errors

--- desafio19.sql
+++ desafio19.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
 
-CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano ('mês' INT, ano INT)
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano (`mês` INT, ano INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantidadeContratada INT;
 SELECT COUNT(*) FROM hr.employees
-WHERE YEAR(hire_date) = ano AND MONTH(hire_date) = 'mês'
+WHERE YEAR(hire_date) = ano AND MONTH(hire_date) = `mês`
 INTO quantidadeContratada;
 RETURN quantidadeContratada;
 END $$
