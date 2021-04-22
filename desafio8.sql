35ad7b363ae05a2250073130de3f9e8f6cfc449e
caiomzrocha <caiomzrocha@gmail.com>
2021-04-22 01:20:15 -0300

    [REQ-08] Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas "Speedy Express" ou "United Package".

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+  customers.ContactName AS `Nome do contato`,
+  shippers.ShipperName AS `Empresa que fez o envio`,
+  orders.OrderDate 	AS `Data do pedido`
+FROM w3schools.orders AS `orders`
+INNER JOIN w3schools.customers AS `customers`
+ON orders.CustomerID = customers.CustomerID
+INNER JOIN w3schools.shippers AS `shippers`
+ON orders.ShipperID = shippers.ShipperID
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome do contato`, `Empresa que fez o envio`, DATE(`Data do pedido`);
