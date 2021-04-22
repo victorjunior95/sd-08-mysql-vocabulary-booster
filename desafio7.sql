05cfb60e953e411aaacb0a76fba816bc3e0e4ef9
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-04-21 23:14:23 -0300

    fixing req7

--- desafio7.sql
+++ desafio7.sql
@@ -9,5 +9,5 @@ hr.employees AS e
 ON
 e.EMPLOYEE_ID = h.EMPLOYEE_ID
 AND
-MONTH(h.START_DATE) = 1 OR MONTH(h.START_DATE) = 2 OR MONTH(h.START_DATE) = 3
+MONTH(h.START_DATE) IN (1, 2, 3)
 ORDER BY `Nome completo`, `Data de início`;
