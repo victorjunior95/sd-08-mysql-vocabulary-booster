abbb265ba492eb830f1d7bd9bc03f8bae12fba5a
bidfca <bruninhakjurugat11@gmail.com>
2021-04-22 02:43:38 -0300

    Desafio 11 Test

--- desafio11.sql
+++ desafio11.sql
@@ -4,5 +4,5 @@ COUNT(customer2.ContactName) AS 'Número de compatriotas'
 FROM w3schools.customers AS customer1, w3schools.customers AS customer2
 WHERE customer1.ContactName <> customer2.ContactName
 AND customer1.Country = customer2.Country
-GROUP BY customer1.ContactName
+GROUP BY customer1.ContactName, customer1.Country
 ORDER BY customer1.ContactName;
