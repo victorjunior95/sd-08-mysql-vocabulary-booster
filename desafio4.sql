a61daf52ce2fcf7f2549d4bd4e67a8b896b047fc
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-04-21 23:54:48 -0300

    req4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT jobs.JOB_TITLE AS 'Cargo',
+ROUND(AVG(emp.SALARY), 2) AS 'Média salarial',
+  CASE
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    ELSE 'CEO'
+  END AS 'Senioridade'
+FROM hr.employees AS emp
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = emp.JOB_ID
+GROUP BY jobs.JOB_TITLE
+ORDER BY ROUND(AVG(emp.SALARY), 2), jobs.JOB_TITLE;
