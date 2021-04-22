608de8d1fcd5c6a30ed664ce30dbafbdfda15a85
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-04-22 00:16:26 -0300

    C3 - FIX - 14th req fixed

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,4 @@
-SELECT Country AS 'País' FROM w3schools.customers 
+SELECT Country AS `País`FROM w3schools.customers 
 UNION
-SELECT Country AS 'País' FROM w3schools.suppliers
-ORDER BY Country LIMIT 5;
+SELECT Country AS `País` FROM w3schools.suppliers
+ORDER BY `País` LIMIT 5;
