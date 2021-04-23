46546ec4f2b06f16ca3812f233f34abc93310a87
antonioarieiro <sfgfffs@gmail.com>
2021-04-23 14:53:46 -0300

    order by req 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,4 @@
 SELECT country_name AS `País`, 
-IF(region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão` FROM hr.countries; 
+IF(region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão` FROM hr.countries
+ORDER BY COUNTRY_NAME ASC;
+ 
