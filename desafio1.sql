268a20806a017e470caccb331b59454de9e8f7ea
Gabriel Donnantuoni Lima <gabrieldonnantuoni@gmail.com>
2021-04-23 01:00:17 -0300

    Fix req 01, 05 and 06

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT
-  c.COUNTRY_ID AS `País`,
+  c.COUNTRY_NAME AS `País`,
   IF(r.REGION_NAME = 'europe', 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r ON c.REGION_ID = r.REGION_ID
--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,16 @@
 SELECT
-  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+  (
+    SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e
+    WHERE e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+  ) AS `Nome completo`,
   (
     SELECT j.JOB_TITLE FROM hr.jobs AS j
-    WHERE j.JOB_ID = e.JOB_ID
+    WHERE j.JOB_ID = jh.JOB_ID
   ) AS `Cargo`,
-  e.HIRE_DATE AS `Data de início do cargo`,
+  jh.START_DATE AS `Data de início do cargo`,
   (
     SELECT d.DEPARTMENT_NAME FROM hr.departments AS d
-    WHERE d.DEPARTMENT_ID = e.DEPARTMENT_ID
+    WHERE d.DEPARTMENT_ID = jh.DEPARTMENT_ID
   ) AS `Departamento`
-FROM hr.employees AS e
+FROM hr.job_history AS jh
 ORDER BY `Nome completo` DESC, `Cargo`;
