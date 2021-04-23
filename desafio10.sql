3d09acb5c619564121d5d9307c252a8bec2dd7ae
Gabriel Donnantuoni Lima <gabrieldonnantuoni@gmail.com>
2021-04-23 11:27:39 -0300

    Reqs 08 to 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.ProductName AS `Produto`,
+  MIN(od.Quantity) AS `Mínima`,
+  MAX(od.Quantity) AS `Máxima`,
+  AVG(od.Quantity) AS `Média`
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT
+  (
+    SELECT c.ContactName FROM w3schools.customers AS c
+    WHERE c.CustomerID = o.CustomerID
+  ) AS `Nome de contato`,
+  (
+    SELECT s.ShipperName FROM w3schools.shippers AS s
+    WHERE s.ShipperID = o.ShipperID
+  ) AS `Empresa que fez o envio`,
+  o.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS o
+WHERE o.ShipperID IN(1,2)
+ORDER BY `Nome de contato`, `Empresa que fez o envio`,`Data do pedido`;
