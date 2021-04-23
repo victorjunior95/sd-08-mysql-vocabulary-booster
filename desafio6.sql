8dd018b17830c0b5bf669429853282c6b2c2cac3
Gabriel Donnantuoni Lima <gabrieldonnantuoni@gmail.com>
2021-04-23 01:34:14 -0300

    Fix Req 06

--- desafio6.sql
+++ desafio6.sql
@@ -4,8 +4,8 @@ SELECT
     WHERE e.EMPLOYEE_ID = jh.EMPLOYEE_ID
   ) AS `Nome completo`,
   (
-    SELECT jo.JOB_TITLE FROM hr.jobs AS jo
-    WHERE jo.JOB_ID = jh.JOB_ID
+    SELECT j.JOB_TITLE FROM hr.jobs AS j
+    WHERE j.JOB_ID = jh.JOB_ID
   ) AS `Cargo`,
   jh.START_DATE AS `Data de início do cargo`,
   (
@@ -13,4 +13,5 @@ SELECT
     WHERE d.DEPARTMENT_ID = jh.DEPARTMENT_ID
   ) AS `Departamento`
 FROM hr.job_history AS jh
+WHERE jh.EMPLOYEE_ID <> 0
 ORDER BY `Nome completo` DESC, `Cargo`;
