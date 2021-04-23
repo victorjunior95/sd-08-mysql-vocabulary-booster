30ebc00f78ee31b68ee216aa2d52ceeef679bf37
MEianelli <filipelouzano@gmail.com>
2021-04-23 11:47:15 -0300

    Requisitos 1 e 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,1 +1,10 @@
-a
+SELECT
+CONCAT( f.first_name, ' ', f.last_name) AS 'Nome completo',
+h.job_title AS 'Cargo',
+c.start_date AS 'Data de início do cargo',
+g.department_name AS 'Departamento'
+FROM hr.job_history c
+INNER JOIN hr.employees f ON c.employee_id = f.employee_id
+INNER JOIN hr.departments g ON c.department_id = g.department_id
+INNER JOIN hr.jobs h ON c.job_id = h.job_id
+ORDER BY `Nome completo` DESC, `Cargo`;
