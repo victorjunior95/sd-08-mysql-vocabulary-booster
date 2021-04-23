f52000f759213cc7f51079319d7be36aada1f8fd
albertassi88 <albertassi88@gmail.com>
2021-04-23 09:57:14 -0300

    Alteracoes no desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,8 +1,9 @@
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
 jh.Start_Date AS 'Data de início',
 jh.End_Date AS 'Data de rescisão',
-YEAR(jh.End_Date) - YEAR(jh.Start_Date) AS 'Anos trabalhados'
+ROUND((DATEDIFF(jh.End_Date, jh.Start_Date)/365),2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON jh.Employee_ID = e.Employee_ID
+GROUP BY `Nome completo`, `Data de início`, `Data de rescisão`
 ORDER BY CONCAT(e.first_name, ' ', e.last_name), YEAR(jh.End_Date) - YEAR(jh.Start_Date);
