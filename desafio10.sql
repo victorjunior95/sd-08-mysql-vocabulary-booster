509b93f466305d929686fbbd644e3a7381e6c187
patriciavsilva <silva.patricia.v@gmail.com>
2021-04-22 21:24:01 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+
+SELECT 
+    p.ProductName AS 'Produto',
+    MIN(d.Quantity) AS 'Mínima',
+    MAX(d.Quantity) AS 'Máxima',
+    ROUND(AVG(d.Quantity), 2) AS 'Média'
+FROM
+    w3schools.products AS p
+        INNER JOIN
+    w3schools.order_details AS d ON p.ProductID = d.ProductID
+GROUP BY d.ProductID
+HAVING `Média` > 20
+ORDER BY Média , Produto;
