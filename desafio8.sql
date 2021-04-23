23445d3b01248c8257456275deee4b442754e7d6
Uilton Rocha <itoclick@gmail.com>
2021-04-23 03:54:29 -0300

    REQ8 do projeto Vocabulary Booster

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,6 @@
-select c.CustomerName as 'Nome de contato', s.ShipperName as 'Empresa que fez o envio',
+select c.ContactName as 'Nome de contato', s.ShipperName as 'Empresa que fez o envio',
 o.OrderDate as 'Data do pedido' from w3schools.customers as c
 inner join w3schools.orders as o on o.CustomerID = c.CustomerID 
-inner join w3schools.shippers as s on o.ShipperID = s.ShipperID
-;
+inner join w3schools.shippers as s on o.ShipperID = s.ShipperID 
+where s.ShipperName in ('Speedy Express', 'United Package')
+order by `Nome de contato`,`Empresa que fez o envio`, `Data do pedido`;
