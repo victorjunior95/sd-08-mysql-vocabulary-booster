e0531a7e66a7a01361b24876839a997812977a96
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-04-23 17:24:57 -0300

    Desafio 18 - Tentativa 2

--- desafio18.sql
+++ desafio18.sql
@@ -1,1 +1,4 @@
-SELECT (SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e WHERE e.EMPLOYEE_ID = j_h.EMPLOYEE_ID) AS 'Nome completo', CONCAT(DAY(j_h.START_DATE),'/',MONTH(j_h.START_DATE),'/',YEAR(j_h.START_DATE)) AS 'Data de início do cargo', CONCAT(DAY(j_h.END_DATE),'/',MONTH(j_h.END_DATE),'/',YEAR(j_h.END_DATE)) AS 'Data de rescisão', ROUND(DATEDIFF(j_h.END_DATE, j_h.START_DATE ) / 365, 2) AS 'Anos trabalhados' FROM hr.job_history AS j_h WHERE EMPLOYEE_ID <> 0 ORDER BY `Nome completo` , `Anos trabalhados`;
+SELECT (SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e WHERE e.EMPLOYEE_ID = j_h.EMPLOYEE_ID) AS 'Nome completo', CONCAT(LPAD(DAY(j_h.START_DATE), 2, 0),'/',LPAD(MONTH(j_h.START_DATE), 2, 0),'/',YEAR(j_h.START_DATE)) AS 'Data de início do cargo', CONCAT(LPAD(DAY(j_h.END_DATE), 2, 0),'/',LPAD(MONTH(j_h.END_DATE), 2, 0),'/',YEAR(j_h.END_DATE)) AS 'Data de rescisão', ROUND(DATEDIFF(j_h.END_DATE, j_h.START_DATE ) / 365, 2) AS 'Anos trabalhados' FROM hr.job_history AS j_h WHERE EMPLOYEE_ID <> 0 ORDER BY `Nome completo` , `Anos trabalhados`;
+
+-- Referência
+-- https://database.guide/how-to-add-leading-zeros-to-a-number-in-mysql/
