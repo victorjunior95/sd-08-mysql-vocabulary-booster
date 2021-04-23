764c3782f49ef2a8a26a5e1583905c4126079d5a
Uilton Rocha <itoclick@gmail.com>
2021-04-23 04:51:26 -0300

    REQ9 do projeto Vocabulary Booster

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+select concat(e.FirstName,' ',e.LastName) as 'Nome completo', count(o.EmployeeID )as'Total de pedidos'
+from w3schools.orders as o 
+inner join w3schools.employees as e on e.EmployeeID = o.EmployeeID
+group by `Nome completo` order by  `Total de pedidos`;
+
+
+
