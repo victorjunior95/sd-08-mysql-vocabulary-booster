1bf3b3348ae6e9da25a1ec12ea8f5e8cf8d4f9d5
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-23 00:12:25 -0300

    Desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo (IN jobTitle VARCHAR(50))
+BEGIN
+  SELECT ROUND(AVG(employee.SALARY), 2) AS `Média salarial`
+  FROM hr.employees AS employee
+  INNER JOIN hr.jobs AS job
+  ON employee.JOB_ID = job.JOB_ID
+  WHERE job.JOB_TITLE = jobTitle
+  GROUP BY job.JOB_TITLE;
+END
+
+$$ DELIMITER ;
