84ea56df8aa806a40cb20aa6489862197073c22f
albertassi88 <albertassi88@gmail.com>
2021-04-23 09:23:02 -0300

    Alteracoes no desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -4,6 +4,5 @@ BEFORE INSERT ON w3schools.orders
 FOR EACH ROW
 BEGIN
 SET NEW.OrderDate = NOW();
-INSERT INTO w3schools.orders VALUES(NEW.CustomerID, NEW.EmployeeID, NEW.ShipperID);
 END $$
 DELIMITER ;
