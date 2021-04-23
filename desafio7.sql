a978bf3d1ef5468c1f3f154b7d48669744619a31
Uilton Rocha <itoclick@gmail.com>
2021-04-22 23:29:35 -0300

    REQ7 do projeto Vocabulary Booster

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,8 @@
-select  CONCAT(e.FIRST_NAME ,' ', e.LAST_NAME) as 'Nome completo', j.JOB_TITLE as 'Cargo',
-(select START_DATE from hr.job_history where START_DATE = month(START_DATE)=01 and month(START_DATE)=02 and month(START_DATE)=03) as 'Data de início', e.SALARY AS 'Salário'
+select  CONCAT(e.FIRST_NAME ,' ', e.LAST_NAME) as 'Nome completo',
+j.JOB_TITLE as 'Cargo',
+h.START_DATE as 'Data de início',
+e.SALARY AS 'Salário'
 from hr.employees as e
 inner join hr.jobs as j on j.JOB_ID = e.JOB_ID 
-inner join hr.job_history as h on h.EMPLOYEE_ID = e.EMPLOYEE_ID
+inner join hr.job_history  as h on h.EMPLOYEE_ID = e.EMPLOYEE_ID where month(START_DATE) in (01, 02, 03)
 order by `Nome completo`, `Cargo`;
