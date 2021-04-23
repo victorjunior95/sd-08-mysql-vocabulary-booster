b3abb45fd82e8519b0057524a4e99395b61f3c29
Gabriel Donnantuoni Lima <gabrieldonnantuoni@gmail.com>
2021-04-23 01:42:26 -0300

    Fix Req 07

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,4 @@ SELECT
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 WHERE MONTH(jh.START_DATE) IN(1,2,3)
-ORDER BY `Nome completo` DESC, `Data de início`;
+ORDER BY `Nome completo`, `Data de início`;
