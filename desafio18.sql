12fbc1bba9c22d9dc7cc841798d0ba099a26ca20
albertassi88 <albertassi88@gmail.com>
2021-04-23 10:26:15 -0300

    Alteracoes no desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -5,5 +5,4 @@ ROUND((DATEDIFF(jh.End_Date, jh.Start_Date)/365),2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON jh.Employee_ID = e.Employee_ID
-GROUP BY `Nome completo`, `Data de início`, `Data de rescisão`, `Anos trabalhados`
 ORDER BY `Nome completo`, `Anos trabalhados`;
