5e774ffdab7239e20ba74d81fcd2393f1d895703
MEianelli <filipelouzano@gmail.com>
2021-04-23 20:18:47 -0300

    Requisito 11 corrigido denovo

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT 
-a.CustomerName AS 'Nome',
+a.ContactName AS 'Nome',
 a.Country AS 'País',
 b.`total` - 1 AS 'Número de compatriotas'
 FROM w3schools.customers a
@@ -8,4 +8,4 @@ INNER JOIN
 FROM w3schools.customers 
 GROUP BY Country) b ON a.Country = b.Country
 WHERE (b.`total` - 1) != 0
-ORDER BY a.CustomerName;
+ORDER BY a.ContactName;
