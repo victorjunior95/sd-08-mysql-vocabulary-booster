96d085f0f66f60c10fe9eec49481b0e6adfec3fb
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-04-23 11:37:59 -0300

    Req 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    c1.ContactName AS `Nome`,
+    c1.Country AS `País`,
+    COUNT(c2.Country) AS `Número de compatriotas`
+FROM
+    w3schools.customers AS c1,
+    w3schools.customers AS c2
+WHERE
+    c1.Country = c2.Country
+GROUP BY c1.CustomerID
+ORDER BY `Nome`;
