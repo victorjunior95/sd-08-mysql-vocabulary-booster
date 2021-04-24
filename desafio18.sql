5456ce3fe3a614d36b97d8fdf56a78e72e8dfa62
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-23 21:38:25 -0300

    Desafio 18 - correçao do item job.START_DATE > job.END_START

--- desafio18.sql
+++ desafio18.sql
@@ -1,8 +1,9 @@
 SELECT
 CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS `Nome completo`,
--- https://www.w3schools.com/sql/func_mysql_date_format.asp --
+/* https://www.w3schools.com/sql/func_mysql_date_format.asp 
+A função DATE_FORMAT () formata uma data conforme especificado.  */
 DATE_FORMAT(job.START_DATE, '%d/%m/%Y') AS `Data de início`,
-DATE_FORMAT(job.START_DATE, '%d/%m/%Y') AS `Data de rescisão`,
+DATE_FORMAT(job.END_DATE, '%d/%m/%Y') AS `Data de rescisão`, -- Coloquei as datas iguais
 ROUND(DATEDIFF(job.END_DATE, job.START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM hr.employees AS employee
 INNER JOIN hr.job_history AS job
