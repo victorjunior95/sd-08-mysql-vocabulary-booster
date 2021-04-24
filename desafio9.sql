30bc299da1c7c8a7c1a5324c05ca8ca88cdbba8c
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-04-24 07:47:58 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+COUNT(o.EmployeeID) as `Total de pedidos`
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
+GROUP BY o.EmployeeID
+ORDER BY `Total de pedidos`;
