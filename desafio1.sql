7e13db6fbfbe24ed0f85608c01c82b71ff5f386f
Rita Jeveaux <ritajeveaux@gmail.com>
2021-04-23 22:27:58 -0300

    Requisito 8

--- desafio1.sql
+++ desafio1.sql
@@ -1,11 +1,7 @@
 SELECT countries.COUNTRY_NAME AS 'País',
-SELECT
-    countries.COUNTRY_NAME AS 'País',
-    IF(regions.REGION_NAME = 'Europe',
-        'incluído',
-        'não incluído') AS 'Status Inclusão'
-FROM
-    hr.countries AS countries
-        INNER JOIN
-    hr.regions AS regions ON countries.REGION_ID = regions.REGION_ID
-ORDER BY `País`;
+IF (regions.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS countries
+INNER JOIN hr.regions AS regions
+ON countries.REGION_ID = regions.REGION_ID
+ORDER BY countries.COUNTRY_NAME;
+
