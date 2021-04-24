7489a791955803ac97635777b17f8a11a78d3126
Breenolf <blf@ic.ufal.br>
2021-04-24 04:22:02 -0300

    :package: Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
+COUNt(*) AS `Total de pedidos`
+FROM w3schools.orders AS O
+INNER JOIN w3schools.employees AS E
+ON O.EmployeeID = E.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
