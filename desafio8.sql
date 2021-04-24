e6f47dafa884ba3c42a47c8d196b3a19fad4b8d8
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-04-24 07:36:15 -0300

    corrige or no desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,5 +4,5 @@ O.OrderDate as `Data do pedido`
 FROM w3schools.customers as C
 INNER JOIN w3schools.orders AS O ON C.CustomerID = O.CustomerID
 INNER JOIN w3schools.shippers AS S ON O.ShipperID = S.ShipperID
-WHERE ShipperName = 'Speedy Express' OR 'United Package'
+WHERE ShipperName = 'Speedy Express' OR ShipperName = 'United Package'
 ORDER BY C.ContactName, S.ShipperName, O.OrderDate;
