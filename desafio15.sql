721b5ddf09645185e5980276125e377bbf8d8fc0
Cleberton <clebertonfgc@gmail.com>
2021-04-24 14:57:22 -0300

    Requisito 14 e 15 Ajustando codeClimate

--- desafio15.sql
+++ desafio15.sql
@@ -4,10 +4,10 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo( IN nome_cargo VARCHAR(20))
 BEGIN
- SELECT ROUND(AVG(tb_empl.SALARY), 2) AS 'Média salarial'
- FROM hr.employees AS tb_empl
- INNER JOIN hr.jobs AS tb_jb
- ON tb_empl.JOB_ID = tb_jb.JOB_ID WHERE tb_jb.JOB_TITLE = nome_cargo;
+  SELECT ROUND(AVG(tb_empl.SALARY), 2) AS 'Média salarial'
+  FROM hr.employees AS tb_empl
+  INNER JOIN hr.jobs AS tb_jb
+  ON tb_empl.JOB_ID = tb_jb.JOB_ID WHERE tb_jb.JOB_TITLE = nome_cargo;
 END $$
 
 DELIMITER ;
