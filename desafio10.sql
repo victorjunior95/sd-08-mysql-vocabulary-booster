7081dd1adaf179f141ce190fd25e338ffad7cef5
Breenolf <blf@ic.ufal.br>
2021-04-24 19:26:51 -0300

    :package: Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT P.ProductName AS Produto,
+MIN(OD.Quantity) AS Mínima,
+MAX(OD.Quantity) AS Máxima,
+ROUND(AVG(OD.Quantity), 2) AS Média
+FROM w3schools.order_details AS OD
+INNER JOIN w3schools.products AS P
+ON OD.ProductID = P.ProductID
+GROUP BY Produto
+HAVING Média > 20.00
+ORDER BY Média, Produto;
