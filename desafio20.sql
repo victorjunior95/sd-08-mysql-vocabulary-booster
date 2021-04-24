d578bb7716ad4a68349eb35d56d52d7f0cc0f3dc
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-24 03:37:19 -0300

    Challenge 20 done

---
+++ desafio20.sql
@@ -0,0 +1,23 @@
+DELIMITER $$
+
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(45))
+
+BEGIN
+
+  SELECT 
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+    D.DEPARTMENT_NAME AS Departamento,
+    J.JOB_TITLE AS Cargo
+  FROM hr.employees AS E
+  INNER JOIN hr.job_history AS JH
+    ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+  INNER JOIN hr.departments AS D
+    ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
+  INNER JOIN hr.jobs AS J
+    ON JH.JOB_ID = J.JOB_ID
+  WHERE E.EMAIL = email
+  ORDER BY 2, 3;
+
+END $$
+
+DELIMITER;
