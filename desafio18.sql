87d0e5d5fbbc6727a678d8125109cbd07f7d8323
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-04-24 02:49:24 -0300

    Challenge 18 Code fixed

--- desafio18.sql
+++ desafio18.sql
@@ -1,8 +1,24 @@
 SELECT
-  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
-  CONCAT(DAY(JH.START_DATE), '/', MONTH(JH.START_DATE), '/', YEAR(JH.START_DATE)) AS `Data de início`,
-  CONCAT(DAY(JH.END_DATE), '/', MONTH(JH.END_DATE), '/', YEAR(JH.END_DATE)) AS `Data de rescisão`,
-  ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365, 2) AS `Anos trabalhados`
+ CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+ 
+ CONCAT(
+   IF(DAY(JH.START_DATE) > 9, DAY(JH.START_DATE), CONCAT(0, DAY(JH.START_DATE))),
+   '/',
+   IF(MONTH(JH.START_DATE) > 9, MONTH(JH.START_DATE), CONCAT(0, MONTH(JH.START_DATE))),
+   '/',
+   YEAR(JH.START_DATE)
+ ) AS `Data de início`,
+ 
+ CONCAT(
+   IF(DAY(JH.END_DATE) > 9, DAY(JH.END_DATE), CONCAT(0, DAY(JH.END_DATE))),
+   '/',
+   IF(MONTH(JH.END_DATE) > 9, MONTH(JH.END_DATE), CONCAT(0, MONTH(JH.END_DATE))),
+   '/',
+   YEAR(JH.END_DATE)
+  ) AS `Data de rescisão`,
+ 
+ ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365, 2) AS `Anos trabalhados`
+ 
 FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH
   ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
