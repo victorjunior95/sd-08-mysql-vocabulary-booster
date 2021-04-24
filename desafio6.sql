dd6fa042d932718b184788f053cb1f3f25ad9e38
Breenolf <blf@ic.ufal.br>
2021-04-24 01:57:11 -0300

    :package: Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+J.JOB_TITLE AS Cargo,
+H.START_DATE AS `Data de início do cargo`,
+D.DEPARTMENT_NAME AS `Departamento`
+FROM hr.job_history AS H
+INNER JOIN hr.employees AS E
+ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J
+ON J.JOB_ID = H.JOB_ID
+INNER JOIN hr.departments AS D
+ON H.DEPARTMENT_ID = D.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
