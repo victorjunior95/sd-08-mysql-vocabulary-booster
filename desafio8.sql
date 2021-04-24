fec95799df0c92cb9e3b081666d12b1d73bae8ee
Douglas Ferreira <dodofp.df@gmail.com>
2021-04-24 17:25:57 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT C.ContactName AS "Nome de contato",
+S.ShipperName "Empresa que fez o envio",
+O.OrderDate AS "Data do pedido"
+FROM w3schools.customers AS C
+INNER JOIN w3schools.orders AS O
+ON C.CustomerID = O.CustomerID
+INNER JOIN w3schools.shippers AS S
+ON O.ShipperID = S.ShipperID
+WHERE S.ShipperID = 1 OR S.ShipperID = 2
+ORDER BY `Nome de contato`, `Data do pedido`;
