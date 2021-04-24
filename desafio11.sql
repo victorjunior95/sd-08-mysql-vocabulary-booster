07fdf2f46e3f036456533a15a4f771574aa8076a
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-04-24 08:23:05 -0300

    corrige condcional desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,6 +2,6 @@ SELECT t1.ContactName AS `Nome`,
 t1.Country AS `País`,
 COUNT(t2.CustomerName) AS `Número de compatriotas`
 FROM w3schools.customers AS t1, w3schools.customers AS t2
-WHERE t1.country = t2.country
+WHERE t1.country = t2.country AND t1.ContactName <> t2.ContactName
 GROUP BY t1.ContactName, t1.Country
 ORDER BY t1.ContactName;
