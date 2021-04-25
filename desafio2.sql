6cd7762220af2941e9c213559ddfa6858244983c
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-04-25 09:56:17 -0400

    requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 select JOB_TITLE as Cargo, 
 case
-	when MAX_SALARY >= 5000 and MAX_SALARY <= 10000 then 'Baixo'
-	when MAX_SALARY >= 10001 and MAX_SALARY <= 20000 then 'Medio'
-	when MAX_SALARY >= 20001 and MAX_SALARY <= 30000 then 'Alto'
-	when MAX_SALARY > 30000 then 'Altíssimo'
-    else 'Nao registrado'
+when MAX_SALARY >= 5000 and MAX_SALARY <= 10000 then 'Baixo'
+when MAX_SALARY >= 10001 and MAX_SALARY <= 20000 then 'Medio'
+when MAX_SALARY >= 20001 and MAX_SALARY <= 30000 then 'Alto'
+when MAX_SALARY > 30000 then 'Altíssimo'
+else 'Nao registrado'
 end as Nível
 from hr.jobs
 order by JOB_TITLE;
