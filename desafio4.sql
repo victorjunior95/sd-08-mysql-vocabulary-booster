26b876e967cfd5104533931d7c1af789c0ccad5e
w-cypriano <profwellington17@gmail.com>
2021-04-25 19:51:50 -0300

    5 e fix 2 e 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT j.JOB_TITLE AS Cargo, ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
 CASE
-	WHEN ROUND(AVG(e.SALARY), 2) < 5800  THEN 'Júnior'
-	WHEN ROUND(AVG(e.SALARY), 2) < 7500  THEN 'Pleno'
-    WHEN ROUND(AVG(e.SALARY), 2) < 10500 THEN 'Sênior'
-    WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
-    END AS Senioridade
+WHEN ROUND(AVG(e.SALARY), 2) < 5800  THEN 'Júnior'
+WHEN ROUND(AVG(e.SALARY), 2) < 7500  THEN 'Pleno'
+WHEN ROUND(AVG(e.SALARY), 2) < 10500 THEN 'Sênior'
+WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
+END AS Senioridade
 FROM jobs as j
 INNER JOIN hr.employees as e
 ON j.JOB_ID = e.JOB_ID
