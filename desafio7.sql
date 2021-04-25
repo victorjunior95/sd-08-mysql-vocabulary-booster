21add254810f2e29f4defad4076342e93b85722c
Joao <joaocarlitoneto@gmail.com>
2021-04-25 18:25:26 -0300

    Req 07

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+/* Desafio7.SQL */
+
+SELECT
+UCASE(CONCAT(tbE.FIRST_NAME, ' ', tbE.LAST_NAME)) 'Nome completo',
+tbJH.START_DATE 'Data de início',
+tbE.SALARY 'Salário'
+FROM hr.employees tbE
+INNER JOIN hr.job_history tbJH
+ON tbE.EMPLOYEE_ID = tbJH.EMPLOYEE_ID
+WHERE MONTH(tbJH.START_DATE)
+IN (1, 2, 3)
+ORDER BY UCASE(CONCAT(tbE.FIRST_NAME, ' ', tbE.LAST_NAME)), tbJH.START_DATE;
