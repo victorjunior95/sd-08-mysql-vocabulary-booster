daa17112e4de308d82788b99fb3fe961e2a95742
Blucilha <blucilha.nascimento@gmail.com>
2021-04-25 16:22:23 -0300

    desafio quatro

---
+++ desafio4.sql
@@ -0,0 +1,17 @@
+SELECT 
+(SELECT j.JOB_TITLE
+  FROM jobs j
+  WHERE
+  j.JOB_ID = e.JOB_ID
+) AS Cargo,
+ROUND(AVG(SALARY), 2) AS `Média salarial`,
+(CASE
+  WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+  ELSE 'CEO'
+  END
+) AS Senioridade
+FROM employees e
+GROUP BY Cargo
+ORDER BY `Média salarial`, Cargo;
