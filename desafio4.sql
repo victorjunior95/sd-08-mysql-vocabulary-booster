75229214aeb8a4cee80f868e4b79f02e8c4303f4
Fabio <fabiohigoralm2@gmail.com>
2021-04-25 00:44:03 -0300

    Desafio 1 ao 5-fix

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,18 @@
 SELECT 
-(select JOB_TITLE from hr.jobs e where e.JOB_ID=j.JOB_ID) as 'Cargo',
-round(avg(j.salary),2) as 'Média salarial',
-case
-when avg(j.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
-when avg(j.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
-when avg(j.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
-when avg(j.salary) > 10500 THEN 'CEO'
-end as 'Senioridade'
+    (SELECT 
+            JOB_TITLE
+        FROM
+            hr.jobs e
+        WHERE
+            e.JOB_ID = j.JOB_ID) AS 'Cargo',
+    ROUND(AVG(j.salary), 2) AS 'Média salarial',
+    CASE
+        WHEN AVG(j.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(j.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(j.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN AVG(j.salary) > 10500 THEN 'CEO'
+    END AS 'Senioridade'
 FROM
-hr.employees j 
-GROUP BY j.JOB_ID ORDER BY avg(j.salary) ,`Cargo`;
-     
+    hr.employees j
+GROUP BY j.JOB_ID
+ORDER BY AVG(j.salary) , `Cargo`;
