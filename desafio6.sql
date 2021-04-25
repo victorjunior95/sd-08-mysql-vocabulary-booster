8d020fd8065134a5b863f7616254b14c5f5fc2ed
Giovanna Betti <giovannabetti@gmail.com>
2021-04-25 13:38:03 -0300

    Revisando desafios 6 e 8 mais uma vez

--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,16 @@
 SELECT 
-    CONCAT(E.first_name, ' ', E.last_name) AS 'Nome completo',
-    J.job_title AS 'Cargo',
+    CONCAT(E.first_name,
+            ' ',
+            E.last_name) AS 'Nome completo',
+    J.job_title AS Cargo,
     JH.start_date AS 'Data de início do cargo',
     D.department_name AS 'Departamento'
 FROM
-    hr.employees E
+    hr.employees AS E
         INNER JOIN
-    hr.jobs J ON E.job_id = J.job_id
+    hr.job_history AS JH ON E.employee_id = JH.employee_id
         INNER JOIN
-    hr.job_history JH ON E.employee_id = JH.employee_id
+    hr.jobs AS Jobs ON JH.job_id = J.job_id
         INNER JOIN
-    hr.departments D ON E.department_id = D.department_id
-ORDER BY `Nome completo` DESC , `Cargo`;
+    hr.departments AS D ON JH.department_id = D.department_id
+ORDER BY `Nome completo` DESC , Cargo;
--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,13 @@
 SELECT 
-    C.ContactName AS 'Nome de Contato',
+    C.ContactName AS 'Nome de contato',
     S.ShipperName AS 'Empresa que fez o envio',
     O.OrderDate AS 'Data do pedido'
 FROM
-    w3schools.orders O
+    w3schools.orders AS O
         INNER JOIN
-    w3schools.customers C ON O.CustomerID = C.CustomerID
+    w3schools.customers AS C ON O.CustomerID = C.CustomerID
         INNER JOIN
-    w3schools.shippers S ON O.ShipperID = S.ShipperID
+    w3schools.shippers AS S ON O.ShipperID = S.ShipperID
 WHERE
-    S.ShipperName IN ('Speedy Express' , 'United Package')
-ORDER BY 1, 2, 3;
+    S.ShipperID IN (1 , 2)
+ORDER BY `Nome de contato` , S.ShipperName , O.OrderDate;
