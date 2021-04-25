22cf1b360808e26a268785610ea1761156248373
Lucas Neiva <lucassneiva.code@gmail.com>
2021-04-25 10:39:30 -0300

    desafio4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT jobs.JOB_TITLE AS 'Cargo', ROUND(AVG(employees.SALARY), 2) AS `Média salarial`,
+CASE
+	WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN  2000 AND 5800 THEN 'Júnior'
+	WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN  5801 AND 7500 THEN 'Pleno'
+	WHEN ROUND(AVG(employees.SALARY), 2) BETWEEN  7501 AND 10500 THEN 'Sênior'
+	WHEN ROUND(AVG(employees.SALARY), 2) > 10500 THEN 'CEO'
+END AS 'Senioridade'
+FROM hr.employees AS employees
+INNER JOIN hr.jobs AS jobs
+ON employees.JOB_ID = jobs.JOB_ID
+GROUP BY Cargo
+ORDER BY ROUND(AVG(employees.salary), 2), Cargo;
+
