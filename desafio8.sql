70d062e0874d5728dacf3297b663a6ac7028098b
hugobrr <hugobrr@gmail.com>
2021-04-25 14:01:09 -0300

    corrigindo esrros do cc no arquivo 8  agora

--- desafio8.sql
+++ desafio8.sql
@@ -4,12 +4,11 @@ SELECT
 sub.`ContatName` AS 'Nome de contato',
 sub.`CompanyName` AS 'Empresa que fez o envio',
 sub.`OrderDate` AS 'Data do pedido'
- FROM ( 
- SELECT c.ContactName AS 'ContatName', s.ShipperName AS 'CompanyName', o.OrderDate FROM w3schools.orders AS o
- INNER JOIN w3schools.customers AS c
- ON o.CustomerID = c.CustomerID
- INNER JOIN  w3schools.shippers AS s
- ON o.ShipperID = s.ShipperID
- ) AS sub
- ORDER BY 1,2,3;
- 
+  FROM ( 
+  SELECT c.ContactName AS 'ContatName', s.ShipperName AS 'CompanyName', o.OrderDate FROM w3schools.orders AS o
+  INNER JOIN w3schools.customers AS c
+  ON o.CustomerID = c.CustomerID
+  INNER JOIN  w3schools.shippers AS s
+  ON o.ShipperID = s.ShipperID
+  ) AS sub
+  ORDER BY 1,2,3;
