8274e7522627c6ed4f22164bdacb197e6c44fb27
w-cypriano <profwellington17@gmail.com>
2021-04-25 16:54:34 -0300

    fix desafio 2 e 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT JOB_TITLE AS Cargo, ROUND(AVG(MAX_SALARY), 2) AS 'Média Salarial',
-(CASE
+CASE
 	WHEN MAX_SALARY <= 5800 THEN 'Júnior'
 	WHEN MAX_SALARY <= 7500 THEN 'Pleno'
     WHEN MAX_SALARY <= 10500 THEN 'Sênior'
     ELSE 'CEO'
     END 
-)AS Senioridade
+AS Senioridade
 FROM jobs
 ORDER BY `Média Salarial`;
