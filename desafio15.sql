3b253d1078293ec3909fa1c1e3358e9146e8bf5c
Uilton Rocha <itoclick@gmail.com>
2021-04-25 00:38:55 -0300

    REQ15 do projeto Vocabulary Booster

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+create procedure buscar_media_por_cargo(in cargo varchar(20))
+BEGIN
+select round(AVG(e.SALARY),2) as 'Salário' from hr.employees as e
+inner join (SELECT * FROM hr.jobs) as j on j.JOB_ID = e.JOB_ID where j.JOB_TITLE = `cargo`
+group by j.JOB_TITLE ;
+END
+$$ DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
+
