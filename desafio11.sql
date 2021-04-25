3ca5a67e212fea5872f30521b167b768576afa33
Breenolf <blf@ic.ufal.br>
2021-04-25 04:27:29 -0300

    :package: Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT C1.ContactName AS Nome,
+C2.Country AS País,
+COUNT(*) AS `Número de compatriotas`
+FROM w3schools.customers C1, w3schools.customers C2
+WHERE C1.Country = C2.Country AND C1.CustomerID <> C2.CustomerID
+GROUP BY País, Nome
+ORDER BY Nome;
