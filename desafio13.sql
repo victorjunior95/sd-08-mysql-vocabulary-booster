62c723b0de2800008867190863bc4e8b80444957
Uilton Rocha <itoclick@gmail.com>
2021-04-24 21:05:56 -0300

    REQ14 do projeto Vocabulary Booster

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,4 @@
-select * from w3schools.products
+select p.ProductName as 'Produto', p.Price as 'Preço'  from w3schools.products as p
+inner join (SELECT * FROM w3schools.order_details
+where Quantity >80) as l on p.ProductID = l.ProductID
+order by Produto;
