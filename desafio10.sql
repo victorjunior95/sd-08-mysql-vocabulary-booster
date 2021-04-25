4c4dd27bc1ef4963bb237c26545cebfe8d191daa
Douglas Ferreira <dodofp.df@gmail.com>
2021-04-25 10:21:34 -0300

    Teste codeclimate

--- desafio10.sql
+++ desafio10.sql
@@ -14,7 +14,6 @@ ORDER BY Média, Produto;
 -- SELECT P.ProductName AS Produto,
 -- MIN(O.Quantity) AS Mínima,
 -- MAX(O.Quantity) AS Máxima,
-
 -- ROUND(AVG(O.Quantity),2) AS "Média"
 -- FROM w3schools.products AS P
 -- INNER JOIN w3schools.order_details AS O
