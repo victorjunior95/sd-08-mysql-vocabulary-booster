2fdad81ae9d9172b6b1f2d26c21fd151f02553ea
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-04-25 10:07:11 -0400

    requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -4,7 +4,7 @@ when MAX_SALARY >= 5000 and MAX_SALARY <= 10000 then 'Baixo'
 when MAX_SALARY >= 10001 and MAX_SALARY <= 20000 then 'Medio'
 when MAX_SALARY >= 20001 and MAX_SALARY <= 30000 then 'Alto'
 when MAX_SALARY > 30000 then 'Altíssimo'
-else 'Nao registrado'
+else 'Salário não categorizado'
 end as Nível
 from hr.jobs
 order by JOB_TITLE;
