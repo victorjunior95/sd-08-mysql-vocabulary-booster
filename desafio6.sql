3095570bc5d82fbbad58661681be1aa8663ae3b2
Giovanna Betti <giovannabetti@gmail.com>
2021-04-25 10:55:57 -0300

    Desafios 6 e 7 concluidos

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(E.first_name, ' ', E.last_name) AS 'Nome completo',
+    J.job_title AS 'Cargo',
+    DATE(JH.start_date) AS 'Data de início do cargo',
+    D.department_name AS 'Departamento'
+FROM
+    hr.employees E
+        INNER JOIN
+    hr.jobs J ON E.job_id = J.job_id
+        INNER JOIN
+    hr.job_history JH ON E.job_id = JH.job_id
+        INNER JOIN
+    hr.departments D ON E.department_id = D.department_id
+ORDER BY `Nome completo` DESC , `Cargo`;
