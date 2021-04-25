72bd3d8609e5dae9c65b5069a0790a63e7400273
Mario Pardo <mariio.pardojr@gmail.com>
2021-04-25 17:39:08 -0300

    Codclimate

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT
 	j.JOB_TITLE AS 'Cargo',
-    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+	ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
 	CASE
 		WHEN (AVG(e.SALARY) BETWEEN 2000 AND 5800) THEN 'Júnior'
 		WHEN (AVG(e.SALARY) BETWEEN 5801 AND 7500) THEN 'Pleno'
