4309302097d2b95e794834c478acb3d965072361
w-cypriano <profwellington17@gmail.com>
2021-04-25 15:52:07 -0300

    fix desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 SELECT
 	JOB_TITLE AS Cargo,
 	CASE
-        WHEN MAX_SALARY >= 5000 && MAX_SALARY  <=10000  THEN 'Baixo'
-        WHEN MAX_SALARY >= 10001 && MAX_SALARY <=20000 THEN 'Médio'
-        WHEN MAX_SALARY >= 20001 && MAX_SALARY <= 30000 THEN 'Alto'
+        WHEN MAX_SALARY <=10000  THEN 'Baixo'
+        WHEN MAX_SALARY <=20000 THEN 'Médio'
+        WHEN MAX_SALARY <=30000 THEN 'Alto'
         ELSE 'Altíssimo'
     END AS Nível
 FROM jobs
