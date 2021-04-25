49049b7c5776b19d2d8508629f4553abc2db4b04
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-04-25 12:48:41 -0300

    desafio 17 - codeclimate fix

--- desafio17.sql
+++ desafio17.sql
@@ -2,10 +2,10 @@ USE w3schools;
 
 DELIMITER $$
 CREATE TRIGGER incluir_data_do_pedido
-	BEFORE INSERT ON orders
-    FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN
-    SET NEW.OrderDate = NOW();
+  SET NEW.OrderDate = NOW();
 END $$
 
 DELIMITER ;
