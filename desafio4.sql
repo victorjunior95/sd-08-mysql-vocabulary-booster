ccc000f1696045aec9a5b623312263d318f3b997
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-25 15:22:12 -0300

    req 4 mais uma vez

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,15 @@
-SELECT jobs.JOB_TITLE AS 'Cargo',
-  ROUND(AVG(employees.SALARY), 2) AS `Média Salarial`,
-    CASE
-      WHEN AVG(employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
-      WHEN AVG(employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
-      WHEN AVG(employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
-      WHEN AVG(employees.SALARY) > 10500 THEN 'CEO'
-    END AS 'Senioridade'
-FROM hr.employees AS employees
-INNER JOIN hr.jobs AS jobs
-ON employees.JOB_ID = jobs.JOB_ID
-GROUP BY employees.JOB_ID
-ORDER BY `Média Salarial`, Cargo;
+SELECT 
+  Jobs.JOB_TITLE AS 'Cargo',
+  ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
+  CASE
+    WHEN AVG(Employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN AVG(Employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN AVG(Employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN AVG(Employees.SALARY) > 10500 THEN 'CEO'
+  END AS 'Senioridade'
+FROM
+  hr.employees AS Employees
+    INNER JOIN
+  hr.jobs AS Jobs ON Employees.JOB_ID = Jobs.JOB_ID
+GROUP BY Employees.JOB_ID
+ORDER BY `Média salarial`, Cargo;
