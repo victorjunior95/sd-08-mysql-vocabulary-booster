191373ea825a037c6c3293ff90885395c0a230ef
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-25 15:15:18 -0300

    req4 mais uma vez

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,11 @@
-SELECT jobs.JOB_TITLE AS 'Cargo', ROUND(AVG(employees.SALARY), 2) AS `Média Salarial`,
-  CASE
-    WHEN AVG(employees.SALARY) >= 2000 AND AVG(employees.SALARY) <= 5800 THEN 'Júnior'
-    WHEN AVG(employees.SALARY) >= 5801 AND AVG(employees.SALARY) <= 7500 THEN 'Pleno'
-    WHEN AVG(employees.SALARY) >= 7501 AND AVG(employees.SALARY) <= 10500 THEN 'Sênior'
-    ELSE 'CEO'
-  END AS 'Senioridade'
+SELECT jobs.JOB_TITLE AS 'Cargo',
+  ROUND(AVG(employees.SALARY), 2) AS `Média Salarial`,
+    CASE
+      WHEN AVG(employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+      WHEN AVG(employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+      WHEN AVG(employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+      WHEN AVG(employees.SALARY) > 10500 THEN 'CEO'
+    END AS 'Senioridade'
 FROM hr.employees AS employees
 INNER JOIN hr.jobs AS jobs
 ON employees.JOB_ID = jobs.JOB_ID
