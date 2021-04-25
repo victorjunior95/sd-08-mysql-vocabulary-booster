630d1a257bc1e840030e0d9c6f0ecea2491e31c6
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-25 14:28:10 -0300

    corrige req4

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 SELECT jobs.JOB_TITLE AS 'Cargo', ROUND(AVG(employees.SALARY),2) AS 'Média Salarial',
   CASE
-    WHEN ROUND(AVG(SALARY),2) >= 2000 AND ROUND(AVG(SALARY),2) <= 5800 THEN 'Júnior'
-    WHEN ROUND(AVG(SALARY),2) >= 5801 AND ROUND(AVG(SALARY),2) <= 7500 THEN 'Pleno'
-    WHEN ROUND(AVG(SALARY),2) >= 7501 AND ROUND(AVG(SALARY),2) <= 10500 THEN 'Sênior'
+    WHEN ROUND(AVG(employees.SALARY),2) >= 2000 AND ROUND(AVG(employees.SALARY),2) <= 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(employees.SALARY),2) >= 5801 AND ROUND(AVG(employees.SALARY),2) <= 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(employees.SALARY),2) >= 7501 AND ROUND(AVG(employees.SALARY),2) <= 10500 THEN 'Sênior'
     ELSE 'CEO'
   END AS 'Senioridade'
 FROM hr.jobs AS jobs
