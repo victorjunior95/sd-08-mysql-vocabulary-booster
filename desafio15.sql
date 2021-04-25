84132906eebfc247e4e2233d212b575b0cd7f482
lucasbgama <lucasbg@protonmail.com>
2021-04-25 10:40:52 -0300

    correçao nome da coluna

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-SELECT AVG(salary)
+SELECT ROUND(AVG(salary), 2) AS `Média salarial`
 FROM employees e
 INNER JOIN jobs j ON j.job_id = e.job_id
 WHERE j.job_title = cargo;
