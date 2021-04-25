c6fb4241c0161d2a0dc08864aa6d1d7c46bf5cea
Joao <joaocarlitoneto@gmail.com>
2021-04-25 16:44:02 -0300

    Req 06

---
+++ desafio6.sql
@@ -0,0 +1,15 @@
+/* Desafio6.SQL */
+
+SELECT 
+CONCAT(tbE.FIRST_NAME, ' ', tbE.LAST_NAME) 'Nome completo',
+tbJ.JOB_TITLE 'Cargo',
+tbJH.START_DATE 'Data de início do cargo',
+tbD.DEPARTMENT_NAME 'Departamento'
+FROM hr.employees tbE
+INNER JOIN hr.job_history tbJH
+ON tbE.EMPLOYEE_ID = tbJH.EMPLOYEE_ID
+INNER JOIN hr.jobs tbJ
+ON tbJH.JOB_ID = tbJ.JOB_ID
+INNER JOIN hr.departments tbD
+ON tbJH.DEPARTMENT_ID = tbD.DEPARTMENT_ID
+ORDER BY CONCAT(tbE.FIRST_NAME, ' ', tbE.LAST_NAME) DESC, tbJ.JOB_TITLE;
