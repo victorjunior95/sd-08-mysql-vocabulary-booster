8738ab403a27cef8c1425ae20fa12cafab47daae
hugobrr <hugobrr@gmail.com>
2021-04-25 19:38:33 -0300

    10 passando de verdade

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 -- 10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
 -- SELECT * FROM w3schools.products;
-SELECT p.ProductName , MIN(Quantity) as 'Mínima', MAX(Quantity) AS 'Máxima', AVG(Quantity) AS 'Média' FROM w3schools.order_details AS o
+SELECT p.ProductName AS 'Produto' , MIN(Quantity) as 'Mínima', MAX(Quantity) AS 'Máxima', ROUND(AVG(Quantity),2 ) AS 'Média' FROM w3schools.order_details AS o
 INNER JOIN w3schools.products as p
 ON p.ProductID = o.ProductID
 GROUP BY o.ProductID
