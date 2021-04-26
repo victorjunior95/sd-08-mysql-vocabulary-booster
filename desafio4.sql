38e971ef991b664e211ea0395aeef6b3d2b4071f
Victor Moraes <moraesvictorgk@gmail.com>
2021-04-26 15:27:43 -0300

    arrumar ordem alfabética

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT JOB_TITLE AS Cargo, 
-((MAX_SALARY + MIN_SALARY)) / 2 AS 'Média salarial',
+ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) AS 'Média salarial',
 CASE 
-WHEN ROUND('Média salarial',2) >= 2000 AND ROUND('Média salarial',2) <= 5800 THEN 'Júnior'
-WHEN ROUND('Média salarial',2) >= 5801 AND ROUND('Média salarial',2) <= 7500 THEN 'Pleno'
-WHEN ROUND('Média salarial',2) >= 7501 AND ROUND('Média salarial',2) <= 10500 THEN 'Sênior'
-WHEN ROUND('Média salarial',2) >= 10501 THEN 'CEO'
-ELSE 'NÃO CLASSIFICADO'
+WHEN ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) >= 2000 AND ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) <= 5800 THEN 'Júnior'
+WHEN ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) >= 5801 AND ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) <= 7500 THEN 'Pleno'
+WHEN ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) >= 7501 AND ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) <= 10500 THEN 'Sênior'
+WHEN ROUND(((MAX_SALARY + MIN_SALARY)/2), 2) >= 10501 THEN 'CEO'
+ELSE 'Não classificado'
 END AS Senioridade
-FROM hr.jobs ORDER BY 'Média salarial' ASC;
+FROM hr.jobs ORDER BY ((MAX_SALARY + MIN_SALARY) /2), Cargo;
