3b32a8692eca312182ae7d0663778a09ae961929
bidfca <bruninhakjurugat11@gmail.com>
2021-04-26 09:38:54 -0300

    SQL is case insensitive

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,7 @@
 USE hr;
 DELIMITER $$
 
-CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emailDoFuncionario VARCHAR(50))
 RETURNS INT READS SQL DATA
 
 BEGIN
@@ -9,7 +9,7 @@ DECLARE quantidadeDeEmpregos INT;
 SELECT COUNT(*) FROM hr.job_history jhistory
 INNER JOIN hr.employees employee
 ON jhistory.EMPLOYEE_ID = employee.EMPLOYEE_ID
-WHERE employee.EMAIL = email 
+WHERE employee.EMAIL = emailDoFuncionario
 INTO quantidadeDeEmpregos
 RETURN quantidadeDeEmpregos
 END $$
