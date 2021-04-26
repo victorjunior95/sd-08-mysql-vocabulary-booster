3975c10e9841f1f8269dd76735c100b59780c06a
Blucilha <blucilha.nascimento@gmail.com>
2021-04-26 10:37:05 -0300

    desafio nove

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+  CONCAT(e.FirstName,' ', e.LastName) AS `Nome completo`,
+  COUNT(o.EmployeeID) AS `Total de Pedidos`
+FROM employees e
+INNER JOIN orders o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de Pedidos`;
