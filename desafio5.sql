79a85415ecb9fdb90908b4fe5cd7a7ecedb609b2
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-04-26 01:04:30 -0300

    requisito 5 - deferenca, maximo/mes e minimo/mes salario por cargo

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT J.job_title AS `Cargo`,
+(J.max_salary - J.min_salary) AS `Variação Salarial`,
+ROUND(J.min_salary / 12, 2) AS `Média mínima mensal`,
+ROUND(J.max_salary / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs AS J
+INNER JOIN hr.employees AS E
+ON J.job_id = E.job_id
+GROUP BY J.job_id
+ORDER BY `Variação Salarial`ASC, `Cargo` ASC;
