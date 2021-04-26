91c4f2cde09cd1000c06e5ee24a608276fa60238
Blucilha <blucilha.nascimento@gmail.com>
2021-04-26 20:18:16 -0300

    desafio doze correçao parte um

--- desafio12.sql
+++ desafio12.sql
@@ -2,7 +2,7 @@ SELECT
   CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo funcionário 1`,
   e.SALARY AS `Salário funcionário 1`,
   e.PHONE_NUMBER AS `Telefone funcionário 1`,
-  CONCAT(ee.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo funcionário 2`,
+  CONCAT(ee.FIRST_NAME, ' ', ee.LAST_NAME) AS `Nome completo funcionário 2`,
   ee.SALARY AS `Salário funcionário 2`,
   ee.PHONE_NUMBER AS `Telefone funcionário 2`
 FROM employees e
