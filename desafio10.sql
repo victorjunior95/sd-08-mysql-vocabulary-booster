ecb3f32cee95316911c09d8dc81981ad62400cde
w-cypriano <profwellington17@gmail.com>
2021-04-26 19:25:33 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+SELECT Sub.ProductName AS Produto, 
+Sub.Mínima,
+Sub.Máxima,
+Sub.Média
+FROM(
+SELECT p.ProductID, p.ProductName,
+MAX(od.Quantity) AS Máxima,
+MIN(od.Quantity) AS Mínima,
+ROUND(AVG(od.Quantity),2) AS Média
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+GROUP BY p.ProductID) AS Sub
+WHERE Média > 20
+ORDER BY 4,1;
+
+
