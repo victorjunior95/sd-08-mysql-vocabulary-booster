3632ca8546b775aa85f3fde89c34eb5fde0709d9
caiomzrocha <caiomzrocha@gmail.com>
2021-04-26 18:52:24 -0300

    [REQ-16] Cria a função buscar_quantidade_de_empregos_por_funcionario().

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(25))
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE quantidade_de_empregos INT;
+  SELECT COUNT(*) FROM hr.job_history AS j_history
+  INNER JOIN hr.employees AS emp
+  ON j_history.EMPLOYEE_ID = emp.EMPLOYEE_ID
+  WHERE emp.EMAIL = email
+  INTO quantidade_de_empregos;
+  RETURN quantidade_de_empregos;
+END $$
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
