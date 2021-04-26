a8208a0bb51d7a0545b8e4b6ee4b3a3a085c354c
Rafael Greco <rafaelmgreco@hotmail.com>
2021-04-26 11:17:06 -0300

    req 2 3 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT
+  j.JOB_TITLE AS `Cargo`,
+  ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
+  (
+    CASE
+      WHEN ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
+      WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+      WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+      WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
+    END
+  ) AS `Senioridade`
+FROM hr.jobs AS j
+INNER JOIN hr.employees AS e ON e.JOB_ID = j.JOB_ID
+GROUP BY `Cargo`
+ORDER BY `Média salarial`, `Cargo`;
