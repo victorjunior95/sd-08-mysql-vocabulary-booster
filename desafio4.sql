90c69c2173208f8bbefbe97a43d89a555d623b25
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-04-25 23:45:53 -0300

    requisito 4 - media de salario por cargo e senoridade

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT J.job_title AS `Cargo`,
+ROUND(AVG(E.salary), 2) AS `Média salarial`,
+CASE
+WHEN AVG(E.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN AVG(E.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN AVG(E.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN AVG(E.salary) > 10500 THEN 'CEO'
+END AS `Senioridade`
+FROM hr.jobs AS J
+INNER JOIN hr.employees AS E
+ON J.job_id = E.job_id
+GROUP BY E.job_id
+ORDER BY `Média salarial` ASC, `Cargo` ASC;
