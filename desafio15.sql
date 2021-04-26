c79a9e6572972747b5d80ae7d77ac67e5d547e99
caiomzrocha <caiomzrocha@gmail.com>
2021-04-26 12:21:40 -0300

    [REQ-15][FIX] Adiciona um alias para a coluna na média salarial.

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,8 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(35))
 BEGIN
-  SELECT ROUND(AVG(SALARY), 2) FROM hr.employees AS employees
+  SELECT ROUND(AVG(SALARY), 2) AS `Média salarial`
+  FROM hr.employees AS employees
   INNER JOIN hr.jobs AS jobs
     ON employees.JOB_ID = jobs.JOB_ID
   WHERE jobs.JOB_TITLE = cargo;
