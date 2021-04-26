8909830f8dd22a0d1c6da2912a90a8c70543124e
bidfca <bruninhakjurugat11@gmail.com>
2021-04-26 09:20:20 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+
+BEGIN
+DECLARE quantidadeDeEmpregos INT;
+SELECT COUNT(*) FROM hr.job_history jhistory
+INNER JOIN hr.employees employee
+ON jhistory.EMPLOYEE_ID = employee.EMPLOYEE_ID
+WHERE EMAIL = email 
+INTO quantidadeDeEmpregos
+RETURN quantidadeDeEmpregos
+END $$
+
+DELIMITER ;
