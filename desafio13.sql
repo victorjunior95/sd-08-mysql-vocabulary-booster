6e1418f21de9e69ac16c85434c427f62426993ab
marcos <marcosd3wow@gmail.com>
2021-04-26 19:29:33 -0300

    req12 revisado

--- desafio13.sql
+++ desafio13.sql
@@ -1,10 +1,10 @@
-SELECT products.ProductName as 'Produto', products.Price as 'Preço' FROM w3schools.order_details AS order INNER JOIN w3schools.products AS products ON order.ProductID = products.ProductID WHERE Quantity > 80 ORDER BY `Produto`; 
+SELECT products.ProductName as 'Produto', products.Price as 'Preço' FROM w3schools.order_details AS orderd INNER JOIN w3schools.products AS products ON orderd.ProductID = products.ProductID WHERE Quantity > 80 ORDER BY `Produto`;
 
--- SELECT
---   P.ProductName as 'Produto',
---   P.Price as 'Preço'
--- FROM w3schools.order_details AS OD
--- INNER JOIN w3schools.products AS P
--- ON OD.ProductID = P.ProductID
--- WHERE Quantity > 80
--- ORDER BY `Produto`;
+SELECT
+  products.ProductName as 'Produto',
+  products.Price as 'Preço'
+FROM w3schools.order_details AS orderd
+INNER JOIN w3schools.products AS products
+ON orderd.ProductID = products.ProductID
+WHERE Quantity > 80
+ORDER BY `Produto`;
