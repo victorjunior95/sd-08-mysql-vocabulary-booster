256c84e029b490cb467c91028c9c1f80870ee2d4
patriciavsilva <silva.patricia.v@gmail.com>
2021-04-25 21:28:14 -0300

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT
+    p.ProductName AS `Produto`, p.Price AS `Preço`
+FROM
+    w3schools.products AS p
+        INNER JOIN
+    w3schools.order_details AS o ON p.ProductID = o.ProductID AND o.Quantity > 80
+    ORDER BY `Produto`;
