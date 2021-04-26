c58697a4872c7f3a421396b23e42c1e5fd91e937
Uilton Rocha <itoclick@gmail.com>
2021-04-26 09:33:37 -0300

    REQ15 do projeto Vocabulary Booster

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 create procedure buscar_media_por_cargo(in cargo varchar(20))
 BEGIN
-select round(AVG(e.SALARY),2) as 'Salário' from hr.employees as e
+select round(AVG(e.SALARY),2) as 'Média salarial' from hr.employees as e
 inner join (SELECT * FROM hr.jobs) as j on j.JOB_ID = e.JOB_ID where j.JOB_TITLE = `cargo`
 group by j.JOB_TITLE ;
 END
