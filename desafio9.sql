e6d171dfa0662d41d057de9f67ef8cba206db549
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-04-26 04:08:05 -0400

    requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,4 @@
+SELECT CONCAT(e.FirstName, ' ',LastName) AS `Nome completo`, COUNT(*) AS `Total de pedidos` FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o ON o.EmployeeID = e.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
