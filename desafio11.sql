0097b930530948c72b61bae92b438d7b1a73b375
Blucilha <blucilha.nascimento@gmail.com>
2021-04-26 12:23:32 -0300

    desafio onze

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT
+  c.ContactName AS Nome,
+  c.Country AS País,
+  count(c.Country) As `Número de compatriotas`
+FROM customers c
+LEFT JOIN customers cc
+ON c.CustomerID <> cc.CustomerID
+AND c.Country = cc.Country
+GROUP BY c.CustomerID
+ORDER BY Nome;
