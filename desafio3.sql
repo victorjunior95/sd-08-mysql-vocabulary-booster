466125c90b3f362839cbaa6414045b0b094672d0
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-04-25 22:26:17 -0300

    requisito 3 - diferença de salario por cargo

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT job_title AS `Cargo`,
+MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo` ASC,
+`Cargo` ASC;
