9c9c5dc95e789cc31190596a2fc7e8895b035f60
marcos <marcosd3wow@gmail.com>
2021-04-26 19:34:36 -0300

    req15 tentando acertar

--- desafio15.sql
+++ desafio15.sql
@@ -1,21 +1,14 @@
 USE hr;
 DELIMITER @!
 
-CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(35)) BEGIN SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial' FROM hr.employees AS employees INNER JOIN hr.jobs AS jobs ON employees.job_id = jobs.job_id WHERE jobs.job_title = jobTitle GROUP BY jobs.job_title; END
+CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(35))
+BEGIN
+  SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
+  FROM hr.employees AS employees
+  INNER JOIN hr.jobs AS jobs
+  ON employees.job_id = jobs.job_id
+  WHERE jobs.job_title = jobTitle
+  GROUP BY jobs.job_title;
+END
 
 @! DELIMITER ;
-
--- USE hr;
--- DELIMITER @!
-
--- CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(35))
--- BEGIN
---   SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
---   FROM hr.employees AS employees
---   INNER JOIN hr.jobs AS jobs
---   ON employees.job_id = jobs.job_id
---   WHERE jobs.job_title = jobTitle
---   GROUP BY jobs.job_title;
--- END
-
--- @! DELIMITER ;
