0144e1555a8c602b369dc6ee7e70f6df7ef539f4
Daniel <marcio.daniel@msn.com>
2021-04-26 13:45:22 -0300

    Fix 13

--- desafio13.sql
+++ desafio13.sql
@@ -3,4 +3,4 @@ SELECT prod.ProductName AS 'Produto',
 FROM w3schools.products AS prod
     INNER JOIN w3schools.order_details AS OD ON prod.ProductID = OD.ProductID
 WHERE OD.Quantity > 80
-ORDER BY `Produto`;
+ORDER BY Produto;
