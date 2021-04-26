778300b7fa1909472c456fd01791f53cdb98b00b
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-04-26 05:57:11 -0400

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,4 @@
+SELECT c1.ContactName AS `Nome`, c1.Country AS `País`, COUNT(c1.Country) AS `Número de compatriotas` FROM w3schools.customers AS c1
+INNER JOIN w3schools.customers AS c2 ON c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID
+GROUP BY c1.Country, c1.CustomerID
+ORDER BY `Nome`;
