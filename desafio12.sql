4f45e138bec4d90d015d8438e2de56ce02c39d5e
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-27 10:32:45 -0300

    corrige req12 novamente

--- desafio12.sql
+++ desafio12.sql
@@ -6,6 +6,6 @@ E2.SALARY AS 'Salário funcionário 2',
 E2.PHONE_NUMBER AS 'Telefone funcionário 2',
 FROM hr.employees AS E1,
 hr.employees AS E2
-ON E1.JOB_ID = E2.JOB_ID
+WHERE E1.JOB_ID = E2.JOB_ID
 AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
