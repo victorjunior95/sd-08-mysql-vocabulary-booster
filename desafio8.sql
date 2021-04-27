456a1635fd368a001970c8460a21d11156ddb796
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-26 22:17:31 -0300

    req8 ContactName

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
-select c.CustomerName as `Nome de contato`, s.ShipperName as `Empresa que fez o envio`, o.OrderDate as `Data do pedido` 
+select c.ContactName as `Nome de contato`, s.ShipperName as `Empresa que fez o envio`, o.OrderDate as `Data do pedido` 
 from w3schools.customers c
 inner join w3schools.orders o on c.CustomerID = o.CustomerID
 inner join w3schools.shippers s on s.ShipperID = o.ShipperID
-where s.ShipperID in (1, 2) order by c.CustomerName, s.ShipperName, o.OrderDate;
+where s.ShipperID in (1, 2) order by c.ContactName, s.ShipperName, o.OrderDate;
