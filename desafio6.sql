9a9c31ea899e19aa36b8dbfca0980a0794ffb71e
adrianWarrior <adrianoforcellini>
2021-04-27 19:57:41 -0300

    requisito6 - cc doidovsky

--- desafio6.sql
+++ desafio6.sql
@@ -10,9 +10,4 @@ FROM
   INNER JOIN hr.employees ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID 
   INNER JOIN hr.jobs ON job_history.JOB_ID = jobs.JOB_ID 
   INNER JOIN hr.departments ON job_history.DEPARTMENT_ID = departments.DEPARTMENT_ID 
-ORDER BY 
-  CONCAT(
-    employees.FIRST_NAME, ' ', employees.LAST_NAME
-  ) DESC, 
-  jobs.JOB_TITLE;
-  
+ORDER BY CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) DESC , jobs.JOB_TITLE;
