d70ba252567a18874b5b75d1d84ed038d4ed350f
Isabella Paz <bellapaz.iorp@gmail.com>
2021-04-27 14:23:54 -0300

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT 
+  P.ProductName AS 'Produto',
+  P.Price AS 'Preço'
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS OD
+  ON OD.ProductID = P.ProductID
+WHERE OD.Quantity > 80
+ORDER BY `Produto`;
