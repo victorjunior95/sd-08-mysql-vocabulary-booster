20f7e91fc695ecd08a197895ea6c581f59f35943
Isabella Paz <bellapaz.iorp@gmail.com>
2021-04-27 14:26:14 -0300

    desafio 19

---
+++ desafio19.sql
@@ -0,0 +1,11 @@
+DELIMITER &&
+  CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes VARCHAR(2), ano VARCHAR(4))
+    RETURNS INT READS SQL DATA
+    BEGIN
+      DECLARE quantidade_funcionarios_contratados INT;
+      SELECT  COUNT(hire_date)
+      FROM hr.employees
+      WHERE MONTH(hire_date) = mes AND YEAR(hire_date) = ano INTO quantidade_funcionarios_contratados;
+      RETURN quantidade_funcionarios_contratados;
+    END
+&& DELIMITER;
