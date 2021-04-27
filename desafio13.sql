c940a38a27a7c37c6f30580b37ab3f64706ec1cb
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 02:29:28 -0300

    Muda o nome das colunas

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,4 @@
-SELECT PRODUCTS.ProductName, PRODUCTS.Price
+SELECT PRODUCTS.ProductName as "Produto", PRODUCTS.Price AS "Price"
 FROM w3schools.products AS PRODUCTS
 INNER JOIN w3schools.order_details AS ORDER_DETAILS
 ON PRODUCTS.ProductID = ORDER_DETAILS.ProductID
