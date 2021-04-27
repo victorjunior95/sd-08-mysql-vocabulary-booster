dbd0393394f4316d636128c1bdf1ea271bbe594a
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-27 13:23:19 -0300

    [fix] - Corrige req 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,8 +1,8 @@
 SELECT CONCAT(Emp1.FIRST_NAME, ' ', Emp1.LAST_NAME) AS `Nome completo funcionário 1`,
-Emp1.SALARY AS `Salário funcionario 1`,
+Emp1.SALARY AS `Salário funcionário 1`,
 Emp1.PHONE_NUMBER AS `Telefone funcionário 1`,
 CONCAT(Emp2.FIRST_NAME, ' ', Emp2.LAST_NAME) AS `Nome completo funcionário 2`,
-Emp2.SALARY AS `Salário funcionario 2`,
+Emp2.SALARY AS `Salário funcionário 2`,
 Emp2.PHONE_NUMBER AS `Telefone funcionário 2`
 FROM hr.employees AS `Emp1`,
 hr.employees AS `Emp2`
