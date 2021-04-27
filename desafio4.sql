83e3baab32db5b419b167609016069c8e15e0aae
adrianWarrior <adrianoforcellini>
2021-04-27 18:53:43 -0300

    requisito4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT jobs.JOB_TITLE AS 'Cargo',
+ROUND(SUM(employees.SALARY)/COUNT(employees.JOB_ID), 2) AS MÉDIA,
+CASE
+WHEN (ROUND(SUM(employees.SALARY)/COUNT(employees.JOB_ID), 2) <= 5800) THEN 'Júnior'
+WHEN (ROUND(SUM(employees.SALARY)/COUNT(employees.JOB_ID), 2) > 5800 AND
+ROUND(SUM(employees.SALARY)/COUNT(employees.JOB_ID), 2) <= 7500 ) THEN 'Pleno'
+WHEN (ROUND(SUM(employees.SALARY)/COUNT(employees.JOB_ID), 2) > 7500 AND
+ROUND(SUM(employees.SALARY)/COUNT(employees.JOB_ID), 2) <= 10500 ) THEN 'Sênior'
+ELSE 'CEO'
+END AS TIPO
+ FROM hr.employees
+ INNER JOIN jobs AS jobs 
+ ON employees.JOB_ID = jobs.JOB_ID
+ GROUP BY (employees.JOB_ID);
+ 
