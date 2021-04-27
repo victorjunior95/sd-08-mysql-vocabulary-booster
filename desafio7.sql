418760accd0c7ef03357fd9ce3196c477ee71a3e
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-04-27 00:22:38 -0300

    requisito 7 - historico de cargos de janeiro, fevereiro e marco

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+UCASE(CONCAT(e.first_name, ' ', e.last_name)) AS `Nome completo`,
+jh.start_date AS `Data de início`,
+e.salary AS `Salário`
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh ON jh.employee_id = e.employee_id
+INNER JOIN hr.jobs AS j ON j.job_id = jh.job_id
+WHERE MONTH(jh.start_date) IN (1, 2, 3)
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
