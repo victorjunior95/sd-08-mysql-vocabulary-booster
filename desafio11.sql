aca1bdaf1e49ea4c1ca1eb198617b8d7044781fd
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 02:17:32 -0300

    Remove os que tem quantidade menor ou igual a zero

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,7 @@
 SELECT CUSTOMERS.ContactName AS "Nome",
 CUSTOMERS.Country as "País",
 -- https://stackoverflow.com/questions/16707780/count-how-many-rows-have-the-same-value
-(SELECT COUNT(*) - 1 AS `Número de compatriotas` FROM w3schools.customers WHERE Country = CUSTOMERS.Country)
+(SELECT COUNT(*) - 1 FROM w3schools.customers WHERE Country = CUSTOMERS.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS CUSTOMERS
+HAVING `Número de compatriotas` > 0
 ORDER BY Nome;
