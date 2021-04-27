1e7abc7c178014e185272b800efd1faf93d33d06
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-27 10:04:28 -0300

    corrige req11 novamente

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
-SELECT ContactName AS 'Nome',
-Country AS 'País',
-COUNT(Country) AS 'Número de compatriotas'
+SELECT C1.ContactName AS 'Nome',
+C1.Country AS 'País',
+COUNT(C1.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS C1
 INNER JOIN w3schools.customers AS C2
 ON C1.Country = C2.Country
