de3cb06918e220854fd7535ee588673d45dcb4e9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 01:23:48 -0300

    Requisito 8

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT * FROM w3schools.orders;
+
+SELECT CONCAT(EMPLOYEES.FirstName, " ", EMPLOYEES.LastName) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
+FROM w3schools.orders AS ORDERS
+INNER JOIN w3schools.employees AS EMPLOYEES
+ON ORDERS.EmployeeID = EMPLOYEES.EmployeeID
+GROUP BY EMPLOYEES.EmployeeID
+ORDER BY `Total de pedidos`;
