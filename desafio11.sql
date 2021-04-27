d7c95c343fbc92b63e4b3d9f87b30fbc6fa17a37
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-27 00:18:28 -0300

    req11 fix1

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
 select c1.ContactName as Nome, c1.Country as País, count(c2.Country) - 1 as `Número de compatriotas` 
 from w3schools.customers c1, w3schools.customers c2
 where c1.Country = c2.Country
-group by c1.CustomerID order by c1.ContactName;
+group by c1.CustomerID having `Número de compatriotas` > 0 order by c1.ContactName;
