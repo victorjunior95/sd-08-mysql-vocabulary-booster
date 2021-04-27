f7006edb9e41ba72cad722765c61125df86c52cd
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-27 09:58:25 -0300

    corrige req11

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,5 @@ FROM w3schools.customers AS C1
 INNER JOIN w3schools.customers AS C2
 ON C1.Country = C2.Country
 AND C1.CustomerID <> C2.CustomerID
-GROUP BY `País`, C1.CustomerID
+GROUP BY C1.Country, C1.CustomerID
 ORDER BY `Nome`;
