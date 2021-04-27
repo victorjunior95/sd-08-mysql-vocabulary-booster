958e9c4a3387c9ec04bdaf26a470a095c1d92d7e
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-27 10:27:57 -0300

    corrige req13

--- desafio13.sql
+++ desafio13.sql
@@ -2,5 +2,6 @@ SELECT P.ProductName AS 'Produto',
 P.Price AS 'Preço'
 FROM w3schools.products AS P
 INNER JOIN w3schools.order_details AS Od
+ON P.ProductID = Od.ProductID
 WHERE Od.Quantity > 80
 ORDER BY `Produto`;
