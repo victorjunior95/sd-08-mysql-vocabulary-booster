6761193b97c91e14a0475fcecf2b52376441218b
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-04-27 09:54:56 -0300

    req11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT ContactName AS 'Nome',
+Country AS 'País',
+COUNT(Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS C1
+INNER JOIN w3schools.customers AS C2
+ON C1.Country = C2.Country
+AND C1.CustomerID <> C2.CustomerID
+GROUP BY `País`, C1.CustomerID
+ORDER BY `Nome`;
