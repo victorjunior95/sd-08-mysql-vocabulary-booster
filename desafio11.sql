fdd6ea713355e9ae7dcb0717cd726572e22d6dbe
Fabio <fabiohigoralm2@gmail.com>
2021-04-27 01:08:53 -0300

    Desafio 7 ao 14-fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,11 @@
 SELECT 
     c.ContactName AS `Nome`,
     c.Country AS `País`,
-    COUNT(d.Country)  AS `Número de compatriotas`
+    COUNT(d.Country) AS `Número de compatriotas`
 FROM
     w3schools.customers c
-    inner join
-    w3schools.customers d
-    on c.Country = d.Country
-	and c.CustomerID<>d.CustomerID
+        INNER JOIN
+    w3schools.customers d ON c.Country = d.Country
+        AND c.CustomerID <> d.CustomerID
 GROUP BY c.Country , c.ContactName
 ORDER BY c.ContactName;
-
