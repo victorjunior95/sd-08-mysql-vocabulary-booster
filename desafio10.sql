bce1acc7e57ae7bec6483bffd9261835b56d38bb
jnpefo <jnpefo@gmail.com>
2021-04-27 16:06:01 -0300

    Req 10, fix

--- desafio10.sql
+++ desafio10.sql
@@ -17,7 +17,7 @@ SELECT P.ProductName AS 'Produto',
   ROUND(AVG(O.Quantity), 2) AS 'Média'
 FROM w3schools.products AS P
   INNER JOIN w3schools.order_details AS O ON P.ProductID = O.ProductID
-GROUP BY `Produto`
+GROUP BY Produto
 HAVING Média > 20
 ORDER BY `Média`,
   `Produto`;
