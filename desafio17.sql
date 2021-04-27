be87e3fc909cc8d7a89d8ea2a464f22ded59f7cf
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 04:02:30 -0300

    Corrige requisito 17

--- desafio17.sql
+++ desafio17.sql
@@ -5,7 +5,11 @@ CREATE TRIGGER trigger_update_time
 BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
-SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = curdate();
 END $$
 
 DELIMITER ;
+
+INSERT INTO w3schools.orders(CustomerID, EmployeeID, ShipperID) VALUES (4, 2, 2);
+
+SELECT * FROM w3schools.orders WHERE CustomerID = 4;
