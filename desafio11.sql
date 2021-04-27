fbdf9410ae542ec9cb118c39c40e7e0a377c6733
hugobrr <hugobrr@gmail.com>
2021-04-27 11:21:12 -0300

    14 passando 11 com defeito

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 -- 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
 SELECT 
 t1.ContactName AS 'Nome',
-t2.Country AS 'País',
+t1.Country AS 'País',
 COUNT(t1.Country = t2.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE  t1.ContactName <> t2.ContactName AND t1.Country = t2.Country
