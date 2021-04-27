fa123f57c8ec1ad77d9ae0854c0b197bb6197f0a
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-27 11:32:32 -0300

    [fix] - Corrige req 7

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ Emp.salary AS `Salário`
 FROM hr.employees AS `Emp`
 INNER JOIN hr.job_history AS `Jh`
 ON Emp.employee_id = Jh.employee_id
-ORDER BY `Nome completo` ASC, Jh.start_date;
+WHERE MONTH(Jh.start_date) IN (1 , 2, 3)
+ORDER BY `Nome completo` ASC, `Data de início do cargo`;
