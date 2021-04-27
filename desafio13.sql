f2f8ceac1c519c07da973fd1cd8b9c5b13efe72c
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-27 13:02:07 -0300

    [add] - Req 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT pd.ProductName AS `Produto`,
+pd.Price AS `Preço` FROM w3schools.products AS `pd`
+INNER JOIN w3schools.order_details AS `Od`
+ON Pd.productId = Od.productId
+WHERE Od.quantity > 80
+ORDER BY `Produto`;
