9954fa3c34b25bcc70e16ddef2ddc6cc10b3fd3f
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-27 14:10:07 -0300

    [fix] - Corrige req 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,6 +1,6 @@
 SELECT pd.ProductName AS `Produto`,
 pd.Price AS `Preço` FROM w3schools.products AS `pd`
 INNER JOIN w3schools.order_details AS `Od`
-ON Pd.productId = Od.productId
+ON Pd.productId = Od.productid
 WHERE Od.quantity > 80
 ORDER BY `Produto`;
