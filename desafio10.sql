1272432250f9fbc9651f5c9204057267a5977a48
rodolfo_oliveira <rod.vgo@gmail.com>
2021-04-26 23:44:32 -0300

    desafio-10

---
+++ desafio10.sql
@@ -0,0 +1,7 @@
+SELECT P.ProductName AS 'Produto', MIN(O.Quantity) AS 'Mínima', MAX(O.Quantity) AS 'Máxima',
+ROUND(AVG(O.Quantity), 2) AS 'Média'
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS O ON P.ProductID = O.ProductID
+GROUP BY P.ProductID
+HAVING Média > 20
+ORDER BY Média , Produto;
