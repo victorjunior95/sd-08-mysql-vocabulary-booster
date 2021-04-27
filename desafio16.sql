e200807694b7e1b670601d2248038ba794205499
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-27 13:35:33 -0300

    Desafio 19

---
+++ desafio16.sql
@@ -0,0 +1,10 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario( VARCHAR(50)
+RETURNS INT READS SQL DATA
+BEGIN
+
+END
+
+$$ DELIMITER;
---
+++ desafio19.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes VARCHAR(5), ano VARCHAR(5)
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE quantidade_de_contratados INT
+  SELECT COUNT(HIRE_DATE)
+  FROM hr.employees
+  WHERE MONTH(HIRE_DATE) = mes
+  AND YEAR(HIRE_DATE) = ano
+  INTO quantidade_de_contratados;
+  RETURN quantidade_de_contratados;
+
+END
+
+$$ DELIMITER;
