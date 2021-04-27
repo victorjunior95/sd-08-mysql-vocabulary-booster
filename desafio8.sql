616ac708387cfa7cd129a67896975b24d78ec5f5
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 01:04:46 -0300

    Requisito 08

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT * FROM w3schools.orders;
+
+SELECT CUSTOMERS.CustomerName AS `Nome de contato`,
+SHIPPERS.ShipperName AS `Empresa que fez o envio`,
+OrderDate AS `Data do pedido`
+from w3schools.orders AS ORDERS
+INNER JOIN w3schools.customers AS CUSTOMERS
+ON ORDERS.CustomerID = CUSTOMERS.CustomerID
+INNER JOIN w3schools.shippers AS SHIPPERS
+ON ORDERS.ShipperID = SHIPPERS.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
