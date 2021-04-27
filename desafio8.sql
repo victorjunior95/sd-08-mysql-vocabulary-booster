702ac253ea9c4937b0d697d61afb143142ce3793
rodolfo_oliveira <rod.vgo@gmail.com>
2021-04-26 22:32:39 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 SELECT customers.ContactName AS 'Nome de contato', 
 shippers.ShipperName AS 'Empresa que fez o envio', 
 orders.OrderDate AS 'Data do pedido'
-FROM w3schools.customers AS customers
+FROM w3schools.customers  customers
 INNER JOIN w3schools.orders AS orders ON customers.CustomerID = orders.CustomerID
 INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
 WHERE shippers.ShipperName <> 'Federal Shipping'
