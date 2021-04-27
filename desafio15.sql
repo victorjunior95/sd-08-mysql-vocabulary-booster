286651e5a0c4df8533f72f7d773e65d745f144a3
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-04-27 18:18:07 -0300

    Codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN JobTitle VARCHAR(200))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-    FROM hr.employees AS e
-    INNER JOIN 
-    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-    WHERE JOB_TITLE = JobTitle;
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+FROM hr.employees AS e
+INNER JOIN 
+hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+WHERE JOB_TITLE = JobTitle;
 END $$
 
 DELIMITER ;
