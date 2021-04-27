007a8081caea6a605c821b400a1010c4dc9075d3
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-27 14:19:26 -0300

    Desafio 20 > aumentando o VARCHAR

--- desafio20.sql
+++ desafio20.sql
@@ -1,6 +1,6 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN employeeEmail VARCHAR(50))
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN employeeEmail VARCHAR(100))
 BEGIN
 SELECT
   CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS `Nome completo`,
@@ -13,7 +13,7 @@ SELECT
   ON job.JOB_ID = jhistory.JOB_ID
   INNER JOIN hr.departments AS dep
   ON dep.DEPARTMENT_ID = jhistory.DEPARTMENT_ID
-  WHERE EMAIL = employeeEmail
+  WHERE Email = employeeEmail
   ORDER BY 3, 2  
 END
 $$ DELIMITER ;
