deafde1ca252f74bc7305869dd4678ba6bc0ba1f
Victor Moraes <moraesvictorgk@gmail.com>
2021-04-27 15:54:03 -0300

    desafios 4,5,6

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT JOB_TITLE AS Cargo,
-(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-ROUND(MIN_SALARY, 2) AS 'Média mínima mensal',
-ROUND(MAX_SALARY, 2) AS 'Média máxima mensal'
+format(MAX_SALARY - MIN_SALARY, 2) AS 'Variação Salarial',
+format(MIN_SALARY, 2) AS 'Média mínima mensal',
+format(MAX_SALARY, 2) AS 'Média máxima mensal'
 FROM hr.jobs ORDER BY (MIN_SALARY - MAX_SALARY), Cargo;
---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT concat(FIRST_NAME, ' ', LAST_NAME) AS 'Nome Completo',
+JOB_TITLE AS Cargo,
+HIRE_DATE AS 'Data de início do cargo',
+DEPARTMENT_ID AS 'Departamento'
+FROM hr.jobs
+INNER JOIN hr.employees
+ON hr.jobs.JOB_ID = hr.employees.JOB_ID 
+ORDER BY concat(FIRST_NAME, ' ', LAST_NAME), Cargo;
