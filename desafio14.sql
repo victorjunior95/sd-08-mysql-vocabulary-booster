fb67e3b4f4818a9935fbf4ef1c098c22a5c2fd6b
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 02:38:53 -0300

    Adiciona nome a coluna País

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-(SELECT Country FROM w3schools.suppliers)
+(SELECT Country AS "País" FROM w3schools.suppliers)
 UNION
 (SELECT Country FROM w3schools.customers)
-ORDER BY Country
+ORDER BY País
 LIMIT 5;
