52cb17b5f590011dbe2fdfe5a112a7cf943c889e
hugobrr <hugobrr@gmail.com>
2021-04-27 12:32:48 -0300

    acho que o 15 vai

--- desafio11.sql
+++ desafio11.sql
@@ -5,7 +5,7 @@ t1.Country AS 'País',
 COUNT(t1.Country = t2.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE  t1.ContactName <> t2.ContactName AND t1.Country = t2.Country
-GROUP BY `Nome`
+GROUP BY t1.ContactName
 ORDER BY 1;
 
 
