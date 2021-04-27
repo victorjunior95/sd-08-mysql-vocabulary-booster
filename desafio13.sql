b1725c9b269e4838cc109cc2db6dabfc813dace7
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 02:23:48 -0300

    Requisito 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT PRODUCTS.ProductName, PRODUCTS.Price
+FROM w3schools.products AS PRODUCTS
+INNER JOIN w3schools.order_details AS ORDER_DETAILS
+ON PRODUCTS.ProductID = ORDER_DETAILS.ProductID
+WHERE ORDER_DETAILS.Quantity > 80
+ORDER BY PRODUCTS.ProductName;
