68d6b21ce0dd68f9f5f7a58b3a5ece4298800ea4
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 01:15:27 -0300

    Adiciona filtro por shipper

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 SELECT * FROM w3schools.orders;
 
-SELECT CUSTOMERS.CustomerName AS `Nome de contato`,
+SELECT CUSTOMERS.ContactName AS `Nome de contato`,
 SHIPPERS.ShipperName AS `Empresa que fez o envio`,
 OrderDate AS `Data do pedido`
 from w3schools.orders AS ORDERS
@@ -8,4 +8,5 @@ INNER JOIN w3schools.customers AS CUSTOMERS
 ON ORDERS.CustomerID = CUSTOMERS.CustomerID
 INNER JOIN w3schools.shippers AS SHIPPERS
 ON ORDERS.ShipperID = SHIPPERS.ShipperID
+WHERE ORDERS.ShipperID IN (1, 2)
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
