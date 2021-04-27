f07223dae6381a63321662352604f9209b60265e
rodolfo_oliveira <rod.vgo@gmail.com>
2021-04-27 01:52:03 -0300

    desafio-13 lint

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,4 @@
 SELECT P.ProductName AS 'Produto', P.Price AS 'Preço' FROM w3schools.products AS P
-INNER JOIN w3schools.order_details AS OD ON P.ProductID = OD.ProductID
-WHERE OD.Quantity > 80
+INNER JOIN w3schools.order_details AS d ON P.ProductID = d.ProductID
+WHERE d.Quantity > 80
 ORDER BY Produto;
