3fd9a1f7c64516e957d07c347f7b67f87861ecad
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 02:38:09 -0300

    Troca Price por Preço

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,4 @@
-SELECT PRODUCTS.ProductName as "Produto", PRODUCTS.Price AS "Price"
+SELECT PRODUCTS.ProductName as "Produto", PRODUCTS.Price AS "Preço"
 FROM w3schools.products AS PRODUCTS
 INNER JOIN w3schools.order_details AS ORDER_DETAILS
 ON PRODUCTS.ProductID = ORDER_DETAILS.ProductID
