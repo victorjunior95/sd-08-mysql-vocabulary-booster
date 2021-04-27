dd601bdf7b67d4676189040befb379b8ab82d942
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-27 11:25:19 -0300

    [fix] - Corrige req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,7 @@
-SELECT CONCAT(UCASE(Emp.first_name), ' ', UCASE(Emp.last_name)) AS `Nome Completo`,
-Jb.job_title AS `Cargo`,
+SELECT UCASE(CONCAT(Emp.first_name, ' ', Emp.last_name)) AS `Nome completo`,
 Jh.start_date AS `Data de início do cargo`,
 Emp.salary AS `Salário`
 FROM hr.employees AS `Emp`
-INNER JOIN hr.jobs AS `Jb`
-ON Jb.job_id = Emp.job_id
 INNER JOIN hr.job_history AS `Jh`
 ON Emp.employee_id = Jh.employee_id
-ORDER BY `Nome Completo` ASC, `Cargo` ASC;
+ORDER BY `Nome completo` ASC, Jh.start_date;
