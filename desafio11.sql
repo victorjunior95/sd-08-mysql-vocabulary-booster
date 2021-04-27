e118f7d264ee67bf1533c6ebe28aab23fccdb708
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-27 00:12:21 -0300

    req11

---
+++ desafio11.sql
@@ -0,0 +1,4 @@
+select c1.ContactName as Nome, c1.Country as País, count(c2.Country) - 1 as `Número de compatriotas` 
+from w3schools.customers c1, w3schools.customers c2
+where c1.Country = c2.Country
+group by c1.CustomerID order by c1.ContactName;
