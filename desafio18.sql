139cf6b98aac467c0f3be6f00b1e7d8b44e2231f
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-27 18:59:08 -0300

    req18 fix

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,11 @@
-/* usei este link para me ajudar a calcular a diferença entre as datas: https://zerobugs.com.br/ver-post/mysql-calcular-quantidade-de-dias-meses-ou-anos-entre-duas-datas/ */
+/* usei este link para me ajudar a calcular a diferença entre as datas: https://www.w3schools.com/sql/func_mysql_datediff.asp */
 select concat(e.FIRST_NAME, ' ', e.LAST_NAME) as `Nome completo`, 
-concat(day(jb.START_DATE), '/', month(jb.START_DATE), '/', year(jb.START_DATE)) as `Data de início`,
-concat(day(jb.END_DATE), '/', month(jb.END_DATE), '/', year(jb.END_DATE)) as `Data de rescisão`,
-timestampdiff(year, jb.START_DATE, jb.END_DATE) as `Anos trabalhados`
-from hr.employees e inner join hr.job_history jb on e.EMPLOYEE_ID = jb.EMPLOYEE_ID
+concat(if(day(jh.START_DATE) <= 9, concat('0', day(jh.START_DATE)), day(jh.START_DATE)), '/',
+if(month(jh.START_DATE) <= 9, concat('0', month(jh.START_DATE)), month(jh.START_DATE)), '/',
+year(jh.START_DATE)) as `Data de início`,
+concat(if(day(jh.END_DATE) <= 9, concat('0', day(jh.END_DATE)), day(jh.END_DATE)), '/',
+if(month(jh.END_DATE) <= 9, concat('0', month(jh.END_DATE)), month(jh.END_DATE)), '/',
+year(jh.END_DATE)) as `Data de rescisão`,
+round(datediff(jh.END_DATE, jh.START_DATE) / 365, 2) as `Anos trabalhados`
+from hr.employees e inner join hr.job_history jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 order by `Nome completo`, `Anos trabalhados`;
