aad8d0b6dda8fd4f8e17e9a41fdee86cbaa76f09
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-04-27 18:14:39 -0300

    Req 15

---
+++ desafio15.sql
@@ -0,0 +1,13 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN JobTitle VARCHAR(200))
+BEGIN
+	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+    FROM hr.employees AS e
+    INNER JOIN 
+    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+    WHERE JOB_TITLE = JobTitle;
+END $$
+
+DELIMITER ;
