6701b46a04a88fd2b7b50e00093e65ca1528e165
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-04-27 00:13:44 -0300

    Adiciona ordenação por JOB_TITLE

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,4 @@ MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
 ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
 ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
 FROM hr.jobs
-ORDER BY `Variação Salarial`;
+ORDER BY `Variação Salarial`, JOB_TITLE;
