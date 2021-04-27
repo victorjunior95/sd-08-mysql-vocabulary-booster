7e448fdad7b8c6d747764bbc0bc4eac3fb73fb06
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-27 13:48:04 -0300

    Desafio 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,9 +1,17 @@
 USE hr;
 DELIMITER $$
 
-CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario( VARCHAR(50)
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(employeeEmail VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
+  DECLARE quantidade_empregos INT;
+  SELECT COUNT(jhistory.DEPARTMENT_ID)
+  FROM hr.employees AS employee
+  INNER JOIN hr.job_history AS jhistory
+  ON jhistory.EMPLOYEE_ID = employee.EMPLOYEE_ID
+  WHERE Email = employeeEmail
+  INTO quantidade_empregos;
+  RETURN quantidade_empregos;
 
 END
 
--- desafio19.sql
+++ desafio19.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes VARCHAR(5), ano VARCHAR(5)
 RETURNS INT READS SQL DATA
 BEGIN
-  DECLARE quantidade_de_contratados INT
+  DECLARE quantidade_de_contratados INT;
   SELECT COUNT(HIRE_DATE)
   FROM hr.employees
   WHERE MONTH(HIRE_DATE) = mes
