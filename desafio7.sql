ab10ec27e0854a5222e723ff4015c5a072e93988
jnpefo <jnpefo@gmail.com>
2021-04-27 13:14:24 -0300

    Req 7, fix

--- desafio7.sql
+++ desafio7.sql
@@ -14,3 +14,17 @@
 -- Os resultados devem estar ordenados pelo nome completo das pessoas empregadas em ordem alfabética.
 
 -- Em caso de empate no nome completo, ordene os resultados pela data de início que a pessoa iniciou seu cargo, em ordem crescente.
+-- SELECT * FROM hr.employees;
+-- SELECT * FROM hr.job_history;
+
+SELECT 
+    UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
+    JH.START_DATE AS 'Data de início',
+    E.SALARY AS 'Salário'
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE
+    MONTH(JH.START_DATE) IN (01, 02, 03)
+ORDER BY `Nome completo` , `Data de início`;
