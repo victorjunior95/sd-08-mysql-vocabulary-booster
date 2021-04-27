5c9d819ddf96531a8b545b6a0eddef9bbf679232
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-04-27 12:08:13 -0300

    Desafio 20 esqueci de salvar

--- desafio20.sql
+++ desafio20.sql
@@ -1,12 +1,18 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE exibir_historico_completo_por_funcionario (IN  VARCHAR(50))
+CREATE PROCEDURE exibir_historico_completo_por_funcionario (IN employeeEmail VARCHAR(50))
 BEGIN
-  CONCAT()
+  CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS `Nome completo`,
+  dep.DEPARTMENT_NAME AS "Departamento",
+  job.JOB_TITLE AS "Cargo"
   FROM hr.employees AS employee
+  INNER JOIN hr.jobs AS jhistory
+  ON employee.EMPLOYEE_ID = jhistory.EMPLOYEE_ID
   INNER JOIN hr.jobs AS job
-  ON employee.JOB_ID = job.JOB_ID
-  WHERE job.JOB_TITLE = jobTitle
-  GROUP BY job.JOB_TITLE;
+  ON job.JOB_ID = jhistory.JOB_ID
+  INNER JOIN hr.departments AS dep
+  ON dep.DEPARTMENT_ID = jhistory.DEPARTMENT_ID
+  WHERE EMAIL = employeeEmail
+  ORDER BY 3, 2  
 END
 $$ DELIMITER ;
