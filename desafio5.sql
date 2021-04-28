c7cf842ae3b923dd08c51806a53a19e556d3bda8
Marcela <marcela9908@gmail.com>
2021-04-27 23:28:57 -0300

    Desafio 5 - Correçao 2

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
 SELECT JOB_TITLE AS 'Cargo', 
 MAX_SALARY - MIN_SALARY AS 'Variação Salarial', 
-ROUND(AVG(MIN_SALARY)/12,2) AS 'Média mínima mensal',
-ROUND(AVG(MAX_SALARY)/12,2) AS 'Média máxima mensal'
+ROUND(MIN_SALARY/12,2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY/12,2) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY JOB_TITLE
 ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
