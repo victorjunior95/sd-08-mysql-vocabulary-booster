12c3078736d32f732167b1c97d5fed78039bc573
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-04-27 23:48:52 -0300

    Req 18

---
+++ desafio18.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+    DATE_FORMAT(j.START_DATE, '%d/%m/%Y') AS `Data de início`,
+    DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
+    ROUND(DATEDIFF(j.END_DATE, j.START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.job_history AS j ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
