e719406c08248a8633b3c9ba6b56bfcfea56de9c
adrianWarrior <adrianoforcellini>
2021-04-28 00:37:11 -0300

    requisito 11 - CCempre Sacaneando

--- desafio11.sql
+++ desafio11.sql
@@ -9,7 +9,7 @@ WHERE
     A.Country = B.Country
         AND A.CustomerID <> B.CustomerID
 GROUP BY A.ContactName , A.Country
-ORDER BY `Nome`;
     /* Adaptado do PR de Arnaelcio Gomes
   https://github.com/tryber/sd-08-mysql-vocabulary-booster/pull/54/files */
+ORDER BY `Nome`;
   
