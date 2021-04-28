8372bb80a47e8324344adc25ca46f7b30d699fb5
adrianWarrior <adrianoforcellini>
2021-04-27 23:50:47 -0300

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,16 @@
+SELECT
+    A.ContactName AS 'Nome',
+    A.Country AS 'País',
+    COUNT(*) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS A,
+    w3schools.customers AS B
+WHERE
+    A.Country = B.Country
+        AND A.CustomerID <> B.CustomerID
+GROUP BY A.ContactName , A.Country
+ORDER BY `Nome`;
+
+    /* Adaptado do PR de Arnaelcio Gomes
+  https://github.com/tryber/sd-08-mysql-vocabulary-booster/pull/54/files */
+  
