40693ed679a8acc23d6a04043c7d8c978eddcc0d
adrianWarrior <adrianoforcellini>
2021-04-27 23:09:04 -0300

    requisito 14

---
+++ desafio14.sql
@@ -0,0 +1,18 @@
+SELECT 
+    Country AS 'País'
+FROM
+    w3schools.customers 
+UNION 
+SELECT 
+    Country
+FROM
+    w3schools.suppliers
+ORDER BY País
+LIMIT 5;
+/* O operador UNION, por default, executa o equivalente a um SELECT DISTINCT no result set final.
+ Em outras palavras, ele combina o resultado de execução das duas queries e então executa um SELECT DISTINCT 
+ a fim de eliminar as linhas duplicadas. Este processo é executado mesmo que não hajam registros duplicados. 
+ https://www.devmedia.com.br/sql-utilizando-o-operador-union-e-union-all/37457 */
+     /* Adaptado do PR de Arnaelcio Gomes
+  https://github.com/tryber/sd-08-mysql-vocabulary-booster/pull/54/files */
+  
