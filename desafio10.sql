63b9cfb3166d0c314c2c7b28c2671404acfb3e13
UmberBn <humberto-bandeira@hotmail.com>
2021-04-27 23:56:29 -0400

    Corrigindo desafio 10, enviando desafio 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 SELECT p.ProductName AS `Produto`,
 MIN(od.Quantity) AS `Mínima`,
 MAX(od.Quantity) AS `Máxima`,
-ROUND(AVG(od.Quantity)) AS `Média`
+ROUND(AVG(od.Quantity), 2) AS `Média`
 From w3schools.products p
 INNER JOIN w3schools.order_details od
 ON p.ProductID = od.ProductID
 GROUP BY `Produto`
 HAVING `Média` > 20
-ORDER BY `Produto`;
+ORDER BY `Média`, `Produto`;
