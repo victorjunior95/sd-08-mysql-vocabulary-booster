1e41e42f2ff77374c3095eb405b6aac48c33a785
UmberBn <humberto-bandeira@hotmail.com>
2021-04-28 20:28:59 -0400

    Requsito 18 concluido

---
+++ requisito18.sql
@@ -0,0 +1,11 @@
+USE hr;
+-- https://www.w3schools.com/sql/func_mysql_datediff.asp
+-- https://www.w3schools.com/sql/func_mysql_date_format.asp
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+DATE_FORMAT(jh.START_DATE, "%d/%m/%y") AS `Data de início`,
+DATE_FORMAT(jh.END_DATE, "%d/%m/%y") AS `Data de rescisão`,
+ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM employees AS e
+INNER JOIN job_history AS jh
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
