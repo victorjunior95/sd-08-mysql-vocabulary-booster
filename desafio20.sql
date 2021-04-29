c9da017768ffba01165a54c11df39e98a4a1c47c
UmberBn <humberto-bandeira@hotmail.com>
2021-04-28 21:34:58 -0400

    Corrigindo ordem de resultados no desafio 20

--- desafio20.sql
+++ desafio20.sql
@@ -1,1 +1,21 @@
-USE HR;
+USE hr;
+
+DELIMITER $$
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(40))
+BEGIN
+    SELECT CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) AS `Nome completo`,
+    d.DEPARTMENT_NAME AS `Departamento`,
+    j.JOB_TITLE AS `Cargo`
+    FROM job_history AS jh
+    INNER JOIN employees AS e
+    ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+    INNER JOIN departments AS d
+    ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+    INNER JOIN jobs AS j
+    ON j.JOB_ID = jh.JOB_ID
+    WHERE e.email = email
+    ORDER BY `Departamento`, `Cargo`;
+END $$
+DELIMITER ;
+
+CALL exibir_historico_completo_por_funcionario('NKOCHHAR');
