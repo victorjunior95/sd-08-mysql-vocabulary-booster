d5d3a4485ea4f4d425d8009c533df4d39240f983
Bruno Martins <bruno.martins217@gmail.com>
2021-04-30 19:31:40 -0300

    Desafio 6 fix

--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS VARCHAR(50) READS SQL DATA
 BEGIN
     DECLARE result INT;
-    SELECT COUNT(jh.EMPLOYEE_ID) AS total
+    SELECT COUNT(jh.EMPLOYEE_ID) AS total_empregos
     FROM hr.job_history as jh
     INNER JOIN hr.employees as e
     ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
