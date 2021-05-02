54015fe1739f697bb506dda77bba58ceb2cababb
Marcela <marcela9908@gmail.com>
2021-05-02 17:58:52 -0300

    Desafio 9 - Refatoraçao

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
-COUNT(*) AS 'Total de pedidos'
+COUNT(OrderID) AS 'Total de pedidos'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS e
 ON e.EmployeeID = o.EmployeeID
