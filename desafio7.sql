d365d44d59d4f5f001251467f4e8cbe6ee535135
Marcela <marcela9908@gmail.com>
2021-05-02 01:51:19 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+  START_DATE AS 'Data de início',
+  e.SALARY AS 'Salário'
+FROM hr.employees AS e
+  INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE MONTH(h.start_date) IN(1, 2, 3)
+ORDER BY `Nome completo`,
+  `Data de início`;
