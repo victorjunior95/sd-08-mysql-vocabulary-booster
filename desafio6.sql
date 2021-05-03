821ff3daaf8e3db8886256938e9ce2b26a15bee3
Alana Monteiro <lanaah.15@gmail.com>
2021-05-03 19:42:14 -0300

    CORRECAO DESAFIO 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT 
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     j.JOB_TITLE AS 'Cargo',
     e.HIRE_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
@@ -8,4 +8,5 @@ FROM
         INNER JOIN
     hr.jobs AS j ON e.JOB_ID = j.JOB_ID
         INNER JOIN
-    hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+    hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , j.JOB_TITLE;
