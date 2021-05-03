b18dddf4ef764291083ea494db35c068ca5a1fd2
Marcela <marcela9908@gmail.com>
2021-05-02 22:56:13 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT p.ProductName AS 'Produto',
+MIN(d.Quantity) AS 'Mínima',
+MAX(d.Quantity) AS 	'Máxima',
+ROUND(AVG(d.Quantity),2) As 'Média'
+ FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS d
+ON p.ProductID = d.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20.00
+ORDER BY `Média`,`Produto`;
