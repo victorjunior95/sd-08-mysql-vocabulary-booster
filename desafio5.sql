9aa0c361bc429d633136ff6e8965fe529b57eefe
Alana Monteiro <lanaah.15@gmail.com>
2021-05-03 19:24:36 -0300

    DESAFIO5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT 
+    JOB_TITLE AS Cargo,
+    (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+    ROUND((MIN(MIN_SALARY) / 12),2) AS 'Média mínima mensal',
+    ROUND((MAX(MAX_SALARY) / 12),2) AS 'Média máxima mensal'
+FROM hr.jobs
+GROUP BY JOB_TITLE
+ORDER BY (MAX_SALARY - MIN_SALARY),
+JOB_TITLE;
