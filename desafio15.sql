57a045af850c8e717d3c06244397069232ae6c19
Victor Breno <victor.brns2@gmail.com>
2021-05-03 22:54:02 -0300

    correçoes lint

--- desafio15.sql
+++ desafio15.sql
@@ -1,10 +1,10 @@
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR (50))
 
 BEGIN
-	SELECT ROUND(AVG(emp.SALARY), 2) AS `Média salarial` FROM hr.employees AS emp 
-	INNER JOIN hr.jobs AS job 
-	ON emp.JOB_ID = job.JOB_ID
-	WHERE job.JOB_TITLE = cargo;
+SELECT ROUND(AVG(emp.SALARY), 2) AS `Média salarial` FROM hr.employees AS emp 
+INNER JOIN hr.jobs AS job 
+ON emp.JOB_ID = job.JOB_ID
+WHERE job.JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
