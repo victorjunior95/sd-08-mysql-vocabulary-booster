dc197e1e58f533421b73a21fada143afc93fc0bb
Marcela <marcela9908@gmail.com>
2021-05-03 21:07:06 -0300

    Correçao code Climate

--- desafio11.sql
+++ desafio11.sql
@@ -5,7 +5,7 @@ COUNT(t1.Country ) AS 'Número de compatriotas'
   FROM 
 w3schools.customers AS t1, 
 w3schools.customers AS t2
- WHERE t1.Country = t2.Country 
- AND t1.ContactName <> t2.ContactName
- GROUP BY `Nome`, `País`
- ORDER BY `Nome`;
+  WHERE t1.Country = t2.Country 
+  AND t1.ContactName <> t2.ContactName
+  GROUP BY `Nome`, `País`
+  ORDER BY `Nome`;
