47467719421c8e76b8610b8e89d495f8cd24e924
Alana Monteiro <lanaah.15@gmail.com>
2021-05-04 14:32:40 -0300

    correcao final requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,11 +4,11 @@ SELECT
     h.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
-    hr.employees AS e
+    hr.job_history AS h
         INNER JOIN
-    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-        INNER JOIN
-    hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
-        INNER JOIN
-    hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
-ORDER BY `Nome completo` DESC , j.JOB_TITLE;
+    hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+		INNER JOIN
+	hr.jobs AS j ON h.JOB_ID = j.JOB_ID
+		INNER JOIN
+	hr.departments AS d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, j.JOB_TITLE;
