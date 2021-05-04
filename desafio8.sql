ff31306a5a8b2f412b08bce083c5fa1a6c98d88f
Alana Monteiro <lanaah.15@gmail.com>
2021-05-04 15:03:42 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+    c.CustomerName AS 'Nome de contato',
+    s.ShipperName AS 'Empresa que fez o envio',
+    o.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS c
+        INNER JOIN
+    w3schools.orders AS o ON c.CustomerID = o.CustomerID
+        INNER JOIN
+    w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE
+    s.ShipperID IN (1 , 2)
+ORDER BY c.CustomerName , s.ShipperName , o.OrderDate;
