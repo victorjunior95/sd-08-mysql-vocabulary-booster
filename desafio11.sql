67c79a6a02d713f7bf5fc53291d3f480d3b6867c
Alana Monteiro <lanaah.15@gmail.com>
2021-05-04 16:04:23 -0300

    desafio 11 correcao e desafio 13

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,12 @@
 SELECT 
     c1.ContactName AS 'Nome',
     c1.Country AS 'País',
-    COUNT(c2.Country) - 1 AS 'Número de compatriotas'
+    c2.Country AS 'Número de compatriotas'
 FROM
     w3schools.customers AS c1,
     w3schools.customers AS c2
-WHERE c1.Country = c2.Country
+WHERE
+    c1.Country = c2.Country
+        AND c1.CustomerID <> c2.CustomerID
 GROUP BY c1.CustomerID
-ORDER BY c1.ContactName;
+ORDER BY `Nome`;
---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.ProductName AS 'Produto', p.Price AS 'Preço'
+FROM
+    w3schools.order_details AS od
+        INNER JOIN
+    w3schools.products AS p ON p.ProductID = od.ProductID
+WHERE
+    od.Quantity > 80
+ORDER BY `Produto`;
