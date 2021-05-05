b4c0d07e0999a2ba3d1ceefbbe074cc586a21cb7
Alana Monteiro <lanaah.15@gmail.com>
2021-05-05 15:29:56 -0300

    desafio18

---
+++ desafio18.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    DATE_FORMAT(j.START_DATE, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND(YEAR(j.END_DATE - j.START_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM
+    hr.job_history AS j
+        INNER JOIN
+    hr.employees AS e ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
+    
