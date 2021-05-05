880395df5cdc1d44b664701e51610f3df6ababfb
Alana Monteiro <lanaah.15@gmail.com>
2021-05-05 15:37:39 -0300

    desafio18 correcao DATEDIFF

--- desafio18.sql
+++ desafio18.sql
@@ -2,7 +2,7 @@ SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     DATE_FORMAT(j.START_DATE, '%d/%m/%Y') AS 'Data de início',
     DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
-    ROUND(YEAR(j.END_DATE - j.START_DATE) / 365, 2) AS 'Anos trabalhados'
+    ROUND(DATEDIFF(j.END_DATE, j.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM
     hr.job_history AS j
         INNER JOIN
