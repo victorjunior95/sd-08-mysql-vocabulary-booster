29b92a3194e07c198e00ff3d765bcb8a4c7facfe
Rafael <rafaeljavaro750@gmail.com>
2021-05-06 00:48:38 -0300

    Desafio 11 - Troca count c1.Country para c2.CustomerName

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,9 @@
 SELECT
-  c1.CustomerName AS Nome,
-  c1.Country AS País,
-  COUNT(c1.Country) AS `Número de compatriotas`
-FROM w3schools.customers AS c1
-INNER JOIN w3schools.customers AS c2
-ON c1.CustomerName <> c2.CustomerName
-AND c1.Country = c2.Country 
-GROUP BY  c1.CustomerName, c1.Country
+  c1.CustomerName AS `Nome`,
+  c1.Country AS `País`,
+  COUNT(c2.ContactName) AS `Número de compatriotas`
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+AND c1.ContactName <> c2.ContactName
+GROUP BY c1.CustomerID
 ORDER BY `Nome`;
