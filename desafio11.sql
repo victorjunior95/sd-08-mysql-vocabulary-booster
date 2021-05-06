6bfc869411c552c0052b7610c2828973c3f767db
Rafael <rafaeljavaro750@gmail.com>
2021-05-06 00:59:55 -0300

    Desafio 11 - Agrupa por nome do Consumidor e País

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
-SELECT
-  c1.CustomerName AS `Nome`,
-  c1.Country AS `País`,
+SELECT 
+  c1.ContactName AS Nome,
+  c1.Country AS País,
   COUNT(c2.ContactName) AS `Número de compatriotas`
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country
 AND c1.ContactName <> c2.ContactName
-GROUP BY c1.CustomerID
-ORDER BY `Nome`;
+GROUP BY c1.ContactName, c1.Country
+ORDER BY c1.ContactName;
