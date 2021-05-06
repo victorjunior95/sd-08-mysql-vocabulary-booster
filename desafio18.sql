13c1d1e8230f9d73c9a8fcdcfbc163312a5c80c4
Rafael <rafaeljavaro750@gmail.com>
2021-05-06 01:58:58 -0300

    Desafio 18 - Refatora

--- desafio18.sql
+++ desafio18.sql
@@ -5,8 +5,8 @@ SELECT
   DATE_FORMAT(h.START_DATE, '%d/%m/%Y') AS `Data de início`,
   DATE_FORMAT(h.END_DATE, '%d/%m/%Y') AS `Data de recisão`,
   ROUND((DATEDIFF(h.END_DATE, h.START_DATE)) / 365, 2) AS `Anos trabalhados`
-  FROM hr.job_history AS h
-  INNER JOIN hr.employees AS e
-  WHERE h.EMPLOYEE_ID = e.EMPLOYEE_ID
-  ORDER BY `Nome Completo`, `Anos trabalhados`;
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e
+WHERE h.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY `Nome Completo`, `Anos trabalhados`;
   
