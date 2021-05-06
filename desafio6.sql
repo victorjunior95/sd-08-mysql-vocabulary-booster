aa28d32d88343c7cf58524ab78e6b05032630b80
Rafael <rafaeljavaro750@gmail.com>
2021-05-06 00:08:19 -0300

    Desafio 6 - Refatora - Utiliza Tabela job_history como base para comparação

--- desafio6.sql
+++ desafio6.sql
@@ -3,11 +3,11 @@ SELECT
   j.JOB_TITLE AS Cargo,
   h.START_DATE AS `Data de início do cargo`,
   d.DEPARTMENT_NAME AS Departamento
-FROM hr.employees AS e
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e
+ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.job_history AS h
-ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+ON h.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d
-WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID
+WHERE h.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
