7951aaf22b1e4b92c702c331b8c5c86e126840cb
Rafael <rafaeljavaro750@gmail.com>
2021-05-05 23:15:30 -0300

    Refatora 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
-  c1.CustomerName AS "Nome",
-  c1.Country AS "Páis",
-  COUNT(c1.Country) AS "Número de compatriotas"
+  c1.CustomerName AS Nome,
+  c1.Country AS País,
+  COUNT(c1.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS c1
 INNER JOIN w3schools.customers AS c2
 ON c1.Country = c2.Country
