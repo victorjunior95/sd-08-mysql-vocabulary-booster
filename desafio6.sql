b7feef44a2337a5d3f4dda9246f7b7c817ea05ca
Rafael <rafaeljavaro750@gmail.com>
2021-05-05 23:13:42 -0300

    Refatora Des 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS "Nome completo",
-  j.JOB_TITLE AS "Cargo",
-  h.START_DATE AS "Data de início do cargo",
-  d.DEPARTMENT_NAME AS "Departamento"
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+  j.JOB_TITLE AS Cargo,
+  h.START_DATE AS `Data de início do cargo`,
+  d.DEPARTMENT_NAME AS Departamento
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID
