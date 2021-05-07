74662c17a01b4d037b281a59435865390f76ccd4
Marcela <marcela9908@gmail.com>
2021-05-07 16:29:59 -0300

    Desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+	SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
+	FROM hr.employees AS e
+	INNER jOIN hr.jobs AS j
+	ON e.job_id = j.job_id AND j.job_title = cargo
+	GROUP BY j.JOB_TITLE;
+END$$ 
+DELIMITER;
+
+CALL buscar_media_por_cargo('Programmer');
