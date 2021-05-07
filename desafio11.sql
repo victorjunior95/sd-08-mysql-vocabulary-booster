ac5ef8cf4c1cf78587a5a184dabc75ababf88835
enicio <jessandro42@gmail.com>
2021-05-07 20:34:53 -0300

    Ajuste no requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ t1.Country AS 'País',
 COUNT(t2.Country) -1 AS 'Número de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 where t1.Country = t2.Country
-group by t1.CustomerName
+group by t1.ContactName, t1.Country
 having COUNT( t1.Country) > 1
 ORDER BY 1;
