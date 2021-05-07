30d7fd487a9ea1a4ec081c38243107c7ccc317fb
Marcela <marcela9908@gmail.com>
2021-05-07 16:31:44 -0300

    Correçao code Climate

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
-	FROM hr.employees AS e
-	INNER jOIN hr.jobs AS j
-	ON e.job_id = j.job_id AND j.job_title = cargo
-	GROUP BY j.JOB_TITLE;
+SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
+FROM hr.employees AS e
+INNER jOIN hr.jobs AS j
+ON e.job_id = j.job_id AND j.job_title = cargo
+GROUP BY j.JOB_TITLE;
 END$$ 
 DELIMITER;
 
