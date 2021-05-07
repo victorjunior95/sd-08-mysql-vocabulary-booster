53121ebe4d95d3f8782c92dad0c48ddafe6e22fb
enicio <jessandro42@gmail.com>
2021-05-07 16:08:56 -0300

    desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT 
-hrjobs.JOB_TITLE AS Cargo, 
-ROUND(hrjobs.MAX_SALARY - hrjobs.MIN_SALARY, 2) AS 'Variação Salarial',
+hrjobs.JOB_TITLE AS Cargo,
+ROUND(hrjobs.MAX_SALARY - hrjobs.MIN_SALARY) AS 'Variação Salarial',
 ROUND(MIN(hrjobs.MIN_SALARY)/12,2) AS 'Média mínima mensal',
 ROUND(MAX(hrjobs.MAX_SALARY)/12,2) AS 'Média máxima mensal'
 FROM hr.jobs AS hrjobs
