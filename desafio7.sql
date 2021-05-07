54b3d319cc9247d280577e69e31f4530b7684315
enicio <jessandro42@gmail.com>
2021-05-07 15:05:21 -0300

    Ajuste de identação no desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,6 @@
-SELECT
- UCASE(CONCAT(e.FIRST_NAME,' ',e.LAST_NAME)) AS `Nome completo`,
-  h.START_DATE AS `Data de início`,
-  e.salary AS 'Salário'
+SELECT UCASE(CONCAT(e.FIRST_NAME,' ',e.LAST_NAME)) AS `Nome completo`,
+h.START_DATE AS `Data de início`,
+e.salary AS 'Salário'
 FROM job_history h
 INNER JOIN employees e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN jobs j ON j.JOB_ID = h.JOB_ID
