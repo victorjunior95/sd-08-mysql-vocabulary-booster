1ba2acd45e18cafeb18367d3d49b7654a42b48d8
caiomzrocha <caiomzrocha@gmail.com>
2021-05-07 11:44:05 -0300

    [REQ-18] Mostra o histórico de cargos das pessoas empregadas, mostrando as datas de início e de saída, assim como os anos que ela ficou nesse cargo.

---
+++ desafio18.sql
@@ -0,0 +1,27 @@
+SELECT
+  CONCAT(
+    employees.FIRST_NAME,
+    ' ',
+    employees.LAST_NAME
+  ) AS `Nome completo`,
+  CONCAT(
+    DAY(job_history.START_DATE),
+    '/',
+    MONTH(job_history.START_DATE),
+    '/',
+    YEAR(job_history.START_DATE)
+  ) AS `Data de início`,
+  CONCAT(
+    DAY(job_history.END_DATE),
+    '/',
+    MONTH(job_history.END_DATE),
+    '/',
+    YEAR(job_history.END_DATE)
+  ) AS `Data de recisão`,
+  YEAR(job_history.END_DATE) - YEAR(job_history.START_DATE)
+    AS `Anos trabalhados`
+FROM hr.employees AS employees
+INNER JOIN hr.job_history AS job_history
+  ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
+  
