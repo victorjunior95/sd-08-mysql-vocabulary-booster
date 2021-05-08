75cc40df912d35566e88502fa7c75304578bfa9c
enicio <jessandro42@gmail.com>
2021-05-08 16:37:57 -0300

    Desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,15 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+	SELECT ROUND(AVG(SALARY),2) AS 'Média salarial'
+	FROM employees
+	WHERE JOB_ID IN (
+	SELECT JOB_ID FROM hr.jobs
+	where JOB_TITLE = cargo
+);
+
+END $$
+
+DELIMITER ;
