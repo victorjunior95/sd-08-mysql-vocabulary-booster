e5d3dd1a1045fdfa8d6a0aa840df7a4fa01a577b
enicio <jessandro42@gmail.com>
2021-05-08 16:39:52 -0300

    Desafio 15 - Requirado tabs

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(SALARY),2) AS 'Média salarial'
-	FROM employees
-	WHERE JOB_ID IN (
-	SELECT JOB_ID FROM hr.jobs
-	where JOB_TITLE = cargo
+SELECT ROUND(AVG(SALARY),2) AS 'Média salarial'
+FROM employees
+WHERE JOB_ID IN (
+SELECT JOB_ID FROM hr.jobs
+where JOB_TITLE = cargo
 );
 
 END $$
