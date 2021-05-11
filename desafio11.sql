d3f12564e8f76b99cd78b97a576b63db11504d8c
Victor Moraes <moraesvictorgk@gmail.com>
2021-05-10 21:30:05 -0300

    desafio6,11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,12 @@
-SELECT ContactName AS Nome,
-Country AS País,
-COUNT(Country) -1 AS 'Número de compatriotas'
-FROM w3schools.customers
-GROUP BY Country, ContactName
-ORDER BY Nome;
+SELECT 
+C1.ContactName AS `Nome`,
+C1.Country AS `País`,
+COUNT(*) AS `Número de compatriotas`
+FROM
+w3schools.customers AS C1,
+w3schools.customers AS C2
+WHERE
+C1.Country = C2.Country
+AND C1.ContactName <> C2.ContactName
+GROUP BY C1.ContactName, C2.Country
+ORDER BY C1.ContactName;
--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,13 @@ SELECT concat(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
 JOB_TITLE AS Cargo,
 START_DATE AS 'Data de início do cargo',
 DEPARTMENT_NAME AS 'Departamento'
-FROM hr.jobs AS J
-INNER JOIN hr.employees AS E
-ON J.JOB_ID = E.JOB_ID
-INNER JOIN hr.job_history AS JH
-ON JH.JOB_ID =  J.JOB_ID
-INNER JOIN hr.departments AS D
+FROM
+hr.job_history AS JH
+INNER JOIN hr.employees AS E 
+ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J 
+ON J.JOB_ID = JH.JOB_ID
+INNER JOIN hr.departments AS D 
 ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
-ORDER BY concat(FIRST_NAME, ' ', LAST_NAME), Cargo;
+ORDER BY `Nome completo` DESC, Cargo;
+
