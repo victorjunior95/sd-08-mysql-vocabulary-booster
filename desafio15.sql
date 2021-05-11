e2dbc99ba68a34db0f1e3201c4fda3815e5a82b0
Victor Moraes <moraesvictorgk@gmail.com>
2021-05-11 09:34:19 -0300

    desafio15

---
+++ desafio15.sql
@@ -0,0 +1,15 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN
+SELECT ROUND(AVG(E.SALARY), 2) AS `Média salarial`
+FROM employees AS E
+INNER JOIN jobs AS J 
+ON J.JOB_ID = E.JOB_ID
+WHERE J.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
