2535530d16ccbaf90682a417b7486e7c1db356b2
IzeldaValeriano <zel.valeriano@gmail.com>
2021-05-11 11:51:08 -0300

    requaitos 10

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+SELECT 
+    pdts.ProductName AS "Produto",
+    MIN(ordd.Quantity) AS "Mínima",
+    MAX(ordd.Quantity) AS "Máxíma",
+    ROUND(AVG(ordd.Quantity), 2) AS "Média"
+FROM
+    w3schools.products AS pdts
+        INNER JOIN
+    w3schools.order_details AS ordd
+WHERE
+    pdts.ProductID = ordd.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média` , `Produto`;
