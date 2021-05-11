e393301deff72f5ec233878189af9495a5a0bd1a
Victor Moraes <moraesvictorgk@gmail.com>
2021-05-11 08:15:34 -0300

    desafio 14

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
 C1.ContactName AS `Nome`,
 C1.Country AS `País`,
-COUNT(*) AS `Número de compatriotas`
+COUNT(C1.Country) AS `Número de compatriotas`
 FROM
 w3schools.customers AS C1,
 w3schools.customers AS C2
