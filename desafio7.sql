0884cd230f39d7e16848357f46131fab7cef225a
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-12 20:58:08 -0300

    REQ 07 - fix

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT
   UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
   START_DATE AS `Data de início`,
-  SALARY AS `Salary`
+  SALARY AS `Salário`
 FROM hr.job_history
 JOIN hr.employees
 ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
