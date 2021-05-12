bc90c31fcf44f40ee5d435212727f141d452e03b
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-12 20:48:53 -0300

    REQ 07 - fix

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,9 @@
 SELECT
-  UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
-  HIRE_DATE AS `Data de início`,
-  SALARY AS Salário
-FROM hr.employees AS employees
+	UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
+	START_DATE AS `Data de início`,
+  SALARY AS `Salary`
+FROM hr.job_history
+JOIN hr.employees
+ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+WHERE MONTH(START_DATE) BETWEEN 1 AND 3
 ORDER BY `Nome completo`, `Data de início`;
