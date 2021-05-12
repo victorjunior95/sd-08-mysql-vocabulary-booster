bf61178f21d98af69fd04519878ca9e57964c658
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-12 16:12:54 -0300

    REQ 08

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT
+  ContactName AS `Nome de contato`,
+  ShipperName AS `Empresa que fez o envio`,
+  OrderDate AS `Data do pedido`
+FROM w3schools.orders AS orders
+JOIN w3schools.customers AS customers
+ON orders.CustomerID = customers.customerID
+JOIN w3schools.shippers AS shippers
+ON orders.ShipperID = shippers.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
