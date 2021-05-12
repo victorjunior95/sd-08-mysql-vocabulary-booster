61b962207b850029fea7db4d697f33460d32ddc4
IzeldaValeriano <zel.valeriano@gmail.com>
2021-05-12 16:46:35 -0300

    requisito 7 e 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,11 @@
-SELECT 	
-    pdts.ProductName AS "Produto",
-    MIN(ordd.Quantity) AS "Mínima",
-    MAX(ordd.Quantity) AS "Máxíma",
-    ROUND(AVG(ordd.Quantity), 2) AS "Média"
-FROM
-    w3schools.products AS pdts
-        INNER JOIN
-    w3schools.order_details AS ordd
-WHERE
-    pdts.ProductID = ordd.ProductID
+SELECT
+  prod.ProductName AS "Produto",
+  MIN(odd.Quantity) AS "Mínima",
+  MAX(odd.Quantity) AS "Máxima",
+  ROUND(AVG(odd.Quantity), 2) AS "Média"
+FROM w3schools.products AS prod
+INNER JOIN w3schools.order_details AS odd
+WHERE prod.ProductID = odd.ProductID
 GROUP BY `Produto`
-HAVING `Média` > 20.00	
-ORDER BY `Média` , `Produto`;
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,9 @@
-SELECT 
-    UCASE(CONCAT(employ.FIRST_NAME, ' ', employ.LAST_NAME)) AS "Nome Completo",
-    hty.START_DATE AS "Data de início",
-    employ.SALARY AS "Salário"
-FROM
-    hr.employees AS employ
-        INNER JOIN
-    hr.job_history AS hty ON employ.EMPLOYEE_ID = hty.EMPLOYEE_ID
-    WHERE MONTH(hty.START_DATE) IN (1, 2, 3)
-ORDER BY `Nome completo` , `Data de início`;
+SELECT
+  UCASE(CONCAT(eploy.FIRST_NAME, ' ', eploy.LAST_NAME)) AS "Nome completo",
+  hyst.START_DATE AS "Data de início",
+  eploy.SALARY AS "Salário"
+FROM hr.employees AS eploy
+INNER JOIN hr.job_history AS hyst
+ON eploy.EMPLOYEE_ID = hyst.EMPLOYEE_ID
+WHERE MONTH(hyst.START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo`, `Data de início`;
