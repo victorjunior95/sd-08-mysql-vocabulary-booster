f94e4bf8749827c62decdbd6911394d915b96360
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-12 16:46:36 -0300

    REQ 10 - fix

--- desafio10.sql
+++ desafio10.sql
@@ -7,4 +7,5 @@ FROM w3schools.order_details AS order_details
 JOIN w3schools.products AS products
 ON order_details.ProductID = products.ProductID
 GROUP BY order_details.ProductID
+HAVING `Média` > 20.00
 ORDER BY `Média`, `Produto`;
