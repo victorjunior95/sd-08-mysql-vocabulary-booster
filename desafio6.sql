d5af047239dc88c88cf76ecb8979b2e4474884e0
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-12 21:03:32 -0300

    REQ 06 - fix

--- desafio6.sql
+++ desafio6.sql
@@ -1,18 +1,19 @@
 SELECT
   (
-    SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)
+    SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME)
     FROM hr.employees
     WHERE employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
   ) AS `Nome completo`,
   (
-    SELECT jobs.JOB_TITLE
+    SELECT JOB_TITLE
     FROM hr.jobs
     WHERE jobs.JOB_ID = job_history.JOB_ID
   ) AS Cargo,
   START_DATE AS `Data de início do cargo`,
   (
-    SELECT departments.DEPARTMENT_NAME
+    SELECT DEPARTMENT_NAME
     FROM hr.departments
     WHERE departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
   ) AS Departamento
-FROM hr.job_history;
+FROM hr.job_history
+ORDER BY `Nome completo` DESC, Cargo;
