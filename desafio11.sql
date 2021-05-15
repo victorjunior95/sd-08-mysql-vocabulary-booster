2fdfe16d074238da5ede99ea8bb7d0291c05fb7a
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-15 18:14:17 -0300

    REQ 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT
+  ContactName AS Nome,
+  Country AS País,
+  (
+    SELECT
+    COUNT(*)
+    FROM w3schools.customers AS current
+    WHERE customers.Country = current.Country
+  ) AS `Número de compatriotas`
+FROM w3schools.customers AS customers
+ORDER BY Nome;
