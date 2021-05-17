16b65be6d62c67aebc598a45b238d00f1d6f127f
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-17 02:03:44 -0300

    REQ 15 - fix

--- desafio15.sql
+++ desafio15.sql
@@ -1,15 +1,14 @@
 USE hr;
 DELIMITER $$;
 
-CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50), OUT average_salary DECIMAL(8, 2))
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 BEGIN
   SELECT
     ROUND(AVG(SALARY), 2) AS `Média salarial`
   FROM employees
   JOIN jobs
   ON jobs.JOB_ID = employees.JOB_ID
-  WHERE JOB_TITLE = job
-  INTO average_salary;
+  WHERE JOB_TITLE = job;
 END $$;
 
 DELIMITER ;
