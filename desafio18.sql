4ba51e6d60c1736136ec2517f042722daa3088e1
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-17 02:50:53 -0300

    REQ 18

---
+++ desafio18.sql
@@ -0,0 +1,10 @@
+USE hr;
+SELECT
+  CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+  DATE_FORMAT(START_DATE, '%d/%m/%Y') AS `Data de início`,
+  DATE_FORMAT(END_DATE, '%d/%m/%Y') AS `Data de recisão`,
+  ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM job_history
+JOIN employees
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
