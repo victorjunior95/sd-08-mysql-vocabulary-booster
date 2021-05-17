a5659cea5c95352489c7a0d6519bf025db3f9040
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-17 02:59:34 -0300

    REQ 18 - fix

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
   CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
   DATE_FORMAT(START_DATE, '%d/%m/%Y') AS `Data de início`,
-  DATE_FORMAT(END_DATE, '%d/%m/%Y') AS `Data de recisão`,
+  DATE_FORMAT(END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
   ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM hr.job_history
 JOIN hr.employees
