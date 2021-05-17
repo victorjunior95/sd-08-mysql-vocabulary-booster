34610ae12503169234d21271d54e8e97a1bfa4b4
victorvalim <victor.valim.c@gmail.com>
2021-05-17 12:05:56 -0300

    Requisito 5 e identação dos outros requisitos

--- Desafio3.sql
+++ Desafio3.sql
@@ -1,1 +1,8 @@
-SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY) AS "Diferença entre salários máximo e mínimo"  FROM hr.jobs WHERE MAX_SALARY <> MIN_SALARY ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
+SELECT 
+    JOB_TITLE AS Cargo,
+    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+FROM
+    hr.jobs
+WHERE
+    MAX_SALARY <> MIN_SALARY
+ORDER BY (MAX_SALARY - MIN_SALARY) , JOB_TITLE;
