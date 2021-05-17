a8c09ed67311e6a22f783b34e7f68140ce237475
victorvalim <victor.valim.c@gmail.com>
2021-05-17 14:07:40 -0300

    Requisito 7 fix

--- desafio7.sql
+++ desafio7.sql
@@ -8,8 +8,8 @@ FROM
     hr.job_history AS job_history
         INNER JOIN
     hr.employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
-    WHERE MONTH(job_history.START_DATE) BETWEEN 1 AND 3
-
+WHERE
+    MONTH(job_history.START_DATE) BETWEEN 1 AND 3
 ORDER BY CONCAT(employees.FIRST_NAME,
         ' ',
         employees.LAST_NAME) ASC;
