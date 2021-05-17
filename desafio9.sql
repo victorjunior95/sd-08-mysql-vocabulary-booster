3c15f20d21837859878106ed1ef7b45bd92f3fc7
victorvalim <victor.valim.c@gmail.com>
2021-05-17 17:27:14 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CONCAT(employees.FirstName,
+            ' ',
+            employees.LastName) AS 'Nome completo',
+    COUNT(*) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS employees
+        INNER JOIN
+    w3schools.orders AS orders ON orders.EmployeeID = employees.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos` ASC;
