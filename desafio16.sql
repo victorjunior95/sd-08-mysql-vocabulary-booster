99c3a2cbdc4ce664e0fef85a70360fe0c55b15db
victorvalim <victor.valim.c@gmail.com>
2021-05-17 22:56:55 -0300

    Requisito 16 fix

--- desafio16.sql
+++ desafio16.sql
@@ -4,15 +4,15 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS DOUBLE READS SQL DATA
 BEGIN
-	DECLARE numberOfJobs DOUBLE;
-	SELECT 
+DECLARE numberOfJobs DOUBLE;
+SELECT 
     COUNT(*)
 FROM
     hr.employees AS employees
         INNER JOIN
     hr.job_history AS job_history ON employees.EMAIL = email
         AND job_history.employee_id = employees.EMPLOYEE_ID INTO numberOfJobs;
-	RETURN numberOfJobs;
+RETURN numberOfJobs;
 
 END$$
 
