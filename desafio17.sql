5fef89641889fb6440cbcb876a513dc0f7d9d28c
victorvalim <victor.valim.c@gmail.com>
2021-05-18 00:08:17 -0300

    eslint >>>> Code climate

--- desafio17.sql
+++ desafio17.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER dateAtt
 BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END
 
 DELIMITER ;
--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,5 @@
 SELECT 
-    CONCAT(employees.FIRST_NAME,
-            ' ',
-            employees.LAST_NAME) AS 'Nome completo',
+    CONCAT(employees.FIRST_NAME,' ',employees.LAST_NAME) AS 'Nome completo',
     DATE_FORMAT(START_DATE, '%d/%m/%Y') AS 'Data de início',-- https://www.w3schools.com/mysql/func_mysql_date_format.asp
     DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
     ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
@@ -10,4 +8,3 @@ FROM
         INNER JOIN
     hr.job_history AS job_history ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
 ORDER BY `Nome completo` ASC , `Anos Trabalhados` ASC;
- 
